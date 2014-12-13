b91056eed3e8f6c3e6bc8d765f586ef7fb07f456
sandywater <sandywater@users.noreply.github.com>
2014-12-12 17:43:01 -0700

    Update Dockerfile and install_perl_mods.sh

--- deploy/ScotBaseImage/install_perl_mods.sh
+++ deploy/ScotBaseImage/install_perl_mods.sh
@@ -9,6 +9,9 @@ NC='\e[0m' # No Color
 
 echo -e "${yellow}Installing Perl Modules...${NC}"
 
+HOME=$(pwd)
+cpanm --local-lib=~/perl5 local::lib && eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
+
 for PACKAGE in "Config::Auto" "Config::Crontab" "Courriel" "Crypt::PBKDF2" "Data::Dumper" "Data::GUID" "Date::Parse" "DateTime" "Domain::PublicSuffix" "Encode" "File::Copy" "File::Slurp" "File::Temp" "File::Type" "FileHandle" "FindBin" "Geo::IP" "HTML::Entities" "HTML::FromText" "HTML::TreeBuilder" "HTTP::Request::Common" "JSON" "IPC::Run" "List::Uniq" "LWP::UserAgent" "Log::Log4perl" "MIME::Base64" "Mail::IMAPClient::BodyStructure" "Mail::IMAPClient" "Mojo::Asset::File" "Mojo::Cache" "Mojo::DOM" "Mojo::JSON" "Mojo::UserAgent" "Mojolicious::Static" "MongoDB" "Moose" "MooseX::MetaDescription::Meta::Attribute" "Net::LDAP" "Net::STOMP::Client" "Number::Bytes::Human" "Parallel::ForkManager" "Proc::PID::File" "Readonly" "Redis" "Switch" "TAP::Harness" "Test::Deep" "Test::Mojo" "Test::More" "XML::Smart" "namespace::autoclean" "utf8" "DateTime::Cron::Simple"
 
 do
