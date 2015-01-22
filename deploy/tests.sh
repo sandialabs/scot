1e3809e6958f860c5ecfa7f2ac2fa01f3b1eaf62
sandywater <sandywater@users.noreply.github.com>
2015-01-22 11:14:06 -0700

    Trying a smarter way to wait for mongo to start

--- deploy/tests.sh
+++ deploy/tests.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 /usr/bin/supervisord &
-sleep 40
+
+COUNTER=0
+grep -q 'waiting for connections on port' /var/log/mongodb/mongod.log
+while [[ $? -ne 0 && $COUNTER -lt 100 ]] ; do
+  sleep 1
+  let COUNTER+=1
+  echo "Waiting for mongo to initialize... ($COUNTER seconds so far)"
+  grep -q 'waiting for connections on port' /var/log/mongodb/mongod.log
+done
+
 cd /opt/sandia/webapps/scot3/t/
 perl /opt/sandia/webapps/scot3/t/all.t
 
