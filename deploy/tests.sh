958df64b25787c3f3f94413ecfaf98068a9e0138
sandywater <sandywater@users.noreply.github.com>
2015-01-22 11:08:18 -0700

    Adding more delay after container starts b4 tests

--- deploy/tests.sh
+++ deploy/tests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 /usr/bin/supervisord &
-sleep 20
+sleep 40
 cd /opt/sandia/webapps/scot3/t/
 perl /opt/sandia/webapps/scot3/t/all.t
 
-if [ "\$?" == "0" ]; then 
+if [ "\$?" == "0" ]; then
   exit 0;
 else
   exit 1;
