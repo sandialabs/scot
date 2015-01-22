16b7a08ebf04f81f328e9c4c43d830a80c83b8e5
sandywater <sandywater@users.noreply.github.com>
2015-01-22 15:49:49 -0700

    Let's just use the scot installer shall we?

--- deploy/tests.sh
+++ deploy/tests.sh
@@ -14,8 +14,8 @@ sleep 80;
 cd /opt/sandia/webapps/scot3/t/
 perl /opt/sandia/webapps/scot3/t/all.t
 
-if [ "\$?" == "0" ]; then
-  exit 0;
-else
-  exit 1;
-fi
+# if [ "\$?" == "0" ]; then
+#   exit 0;
+# else
+#   exit 1;
+# fi
