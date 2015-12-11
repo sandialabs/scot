3684b7db349e1bbe51de20e7051eb5531f31797c
angeor <angeor@sandia.gov>
2015-12-11 14:23:21 -0700

    entry development work - html working with nested entries

--- public/react_components/BottomPane.jsx
+++ public/react_components/BottomPane.jsx
@@ -2,16 +2,16 @@ var React     = require('react');
 var ReactDOM  = require('react-dom');
 
 //function getEvent(callback) {
-function getEvent() {
+function getEvent(id) {
         var jsonData = {};
         $.ajax({
             type: 'GET',
-            url: '/scot/api/v2/entry',
+            url: '/scot/api/v2/event/'+ id + '/entry',
             dataType: 'json',
             async: false,
             success: function(data, status) {
             jsonData = data;
-            console.log(jsonData);
+            //console.log(jsonData);
         },
         error: function(err) {
             console.error(err.toString());
@@ -38,28 +38,74 @@ var TableHeader = React.createClass({
     }
 });
 
+var TableSubData = React.createClass({
+    render: function() {
+        var rawMarkup = this.props.subitem.body_flair;
+        return (
+            <div className="row-fluid entry-outer todo_undefined_outer">
+                <div dangerouslySetInnerHTML={{__html: rawMarkup}} />
+            </div>
+        );
+    }
+});
+
+var TableSubRecursion = React.createClass({
+    render: function() {
+        var rows = [];
+        var count = 0;
+        console.log("in sub");
+        recursiveIter(this.props.subitem);
+        console.log("out of iteration");
+        count ++;
+        function recursiveIter(obj) {
+            for (var prop in obj) {
+                //console.log(obj[prop]);
+                if (prop == "children") {
+                    var childobj = obj[prop];
+                    obj[prop].forEach(function(childobj) {                        
+                        //console.log(childobj.body_flair);
+                        //var childobjbody = childobj.body_flair;
+                        rows.push(new Array(<TableSubData subitem = {childobj} />));
+                        //for (var key in childobj) {
+                            //rows.push(<TableData item = {childobj} />)
+                        //}
+                        recursiveIter(childobj);
+                    });
+                }
+            }
+        }
+        return (
+            <div>{rows}</div>
+        )
+    }
+});
+
 var TableData = React.createClass({
     render: function() {
+        var rawMarkup = this.props.item.body_flair;
         return (
             <div className="row-fluid entry-outer todo_undefined_outer" style={{marginLeft: 'auto', marginRight: 'auto',width:'99.3%'}}>
-                <div className="row-fluid entry-header todo_undefined">{this.props.item.id} {this.props.item.when} by {this.props.item.owner}</div>
-                <div className="entry-body">{this.props.item.body_plain}</div>
-            </div>
+                <div className="row-fluid entry-header todo_undefined">
+                    <div className="entry-header-inner">{this.props.item.id} {this.props.item.when} by {this.props.item.owner}</div>
+                </div>
+                <div className="row-fluid entry-body">                    
+                    <div dangerouslySetInnerHTML={{__html: rawMarkup}} />
+                    //{this.props.item.body_flair}
+                    <TableSubRecursion subitem = {this.props.item} />                    
+                </div>
+            </div>    
         );
     }
 });
 
 var Table = React.createClass({
     render: function() {
-        var events = [];
+        var header = [];
         var rows = [];
-        //myfunc = function(this.props.data) {    
-        //    events.push(<TableHeader item = {data} />)
-        //};
-        this.props.data.forEach(function(data) { 
-            rows.push(<TableData item = {data} />)
+        this.props.data.forEach(function(data) {  
+            rows.push(<TableData item = {data} />)   
         });
-        return (
+        return ( 
             <div>
                 <div width="100%" className="alerts events incidents tasks" style={{height: '800px', overflow: 'auto', display: 'block'}}>
                     <div>{header}</div>
@@ -70,13 +116,13 @@ var Table = React.createClass({
     }
 });
 
-var displaydata = getEvent();
-console.log(displaydata);
+var displaydata = getEvent(3306);
+/*console.log(displaydata);
 for (i = 0; i < displaydata.length; i++) {
     console.log("object each item of object: " + displaydata[i].id)
 }
 console.log("for loop ended");
-
+*/
 
 ReactDOM.render(<Table data={displaydata}/>, document.getElementById('NewBottomDataPane'));
  
