7c4997a356a870148dd387e521aae2e7439776ad
Todd Bruner <tbruner@sandia.gov>
2016-01-06 11:22:59 -0700

    installation i2 fixes
    Former-commit-id: 64f9abed739c56c1c8da1bed797fb0ff4c7a7ec8

--- bin/start_scot.sh
+++ bin/start_scot.sh
@@ -3,12 +3,13 @@
 
 PATH=/usr/local/bin:$PATH
 PLACKUP=`which plackup`
-BASE="/opt/sandia/webapps/scot3"
+BASE="/opt/scot"
 DIR="$BASE/script"
 APP="Scot"
 DAEMON="$DIR/$APP "
 NAME="scot3"
 PIDFILE="$BASE/scot.pid"
+LOG="/var/log/scot"
 OPTS="daemon -s Starman --daemonize --listen 127.0.0.1:5100 --user scot --group scot --max-requests 1 --workers 20 --pid $PIDFILE"
 
 # uncomment to allow profiling
@@ -29,5 +30,5 @@ fi
 echo "Starting the Scot Web Service..."
 echo "SCOTMODE is $SCOTMODE."
 
-eval $PLACKUP $DAEMON $OPTS 2>&1 >>$BASE/log/starman.log
+eval $PLACKUP $DAEMON $OPTS 2>&1 >>$LOG/starman.log
 
