df56594012882854dd72e2d37e2bf9430cc5fc03
angeor <angeor@sandia.gov>
2016-02-03 11:46:46 -0700

    Fully Async Events. Back button introduced. Color Added
    Former-commit-id: 704ab622c13e05c8b1a9eeb9f003ef971381c575

--- pubdev/jsdev/react_components/entry/entry_container.jsx
+++ pubdev/jsdev/react_components/entry/entry_container.jsx
@@ -3,49 +3,21 @@ var EntryHeader = require('./entry_header.jsx');
 var EntryWrapper = require('./entry_wrapper.jsx');
 
 var EntryContainer = React.createClass({
-    getEventData: function(id,type,datarows) {
-        var jsonData = {};
-        $.ajax({
-            type: 'GET',
-            url: '/scot/api/v2/' + type + '/'  + id,
-            dataType: 'json', 
-            async: false,
-            success: function(data, status) {
-                jsonData = data;
-                datarows.push(<EntryHeader id={id} data={jsonData} type={type}/>);
-            },
-            error: function(err) {
-                console.error(err.toString());
-            }
-        });
-    },
-    getEntryData: function(id,type,datarows) {
-        var jsonData = {};
-        $.ajax({
-            type: 'GET',
-            url: '/scot/api/v2/' + type + '/'+ id + '/entry',
-            dataType: 'json',
-            async: false,
-            success: function(data, status) {
-                jsonData = data.records;
-                datarows.push(<EntryWrapper id={id} entrydata={jsonData} />);
-            },
-            error: function(err) {
-                console.error(err.toString());
-            }
-        });
+    getInitialState: function() {
+        return {
+            type:this.props.type,
+            ids:this.props.ids,
+        }
     },
     render: function() {
         var datarows = [];
-        var ids = this.props.ids;
-        var type = this.props.type;
-        for (i=0; i < ids.length; i++) { 
-            datarows.push(<EntryHeader id={ids[i]} type={type}/>);
-            //this.getEventData(ids[i],type,datarows);
-            //this.getEntryData(ids[i],type,datarows);
+        for (i=0; i < this.state.ids.length; i++) { 
+            datarows.push(<EntryHeader id={this.state.ids[i]} type={this.state.type} toggleEventDisplay={this.props.viewEvent}/>);
+            datarows.push(<EntryWrapper id={this.state.ids[i]} type={this.state.type}/>);
         }
         return (
             <div className="entry-container">
+                <button onClick={this.props.viewEvent}>Toggle Display</button>
                 {datarows}
             </div>
         );
