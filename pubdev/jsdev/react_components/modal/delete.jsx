596bcf57bf2f76a5f73463219acf84999678b643
angeor <angeor@sandia.gov>
2016-02-15 11:28:09 -0700

    Delete Entry Functionality
    Former-commit-id: 99a6f1d3b16d6093b343697f191251d358e3c4a9

--- pubdev/jsdev/react_components/modal/delete.jsx
+++ pubdev/jsdev/react_components/modal/delete.jsx
@@ -12,7 +12,7 @@ const customStyles = {
     }
 }
 
-var DeleteEntry = React.createClass({
+var DeleteEvent = React.createClass({
     toggle: function() { 
         $.ajax({
             type: 'delete',
@@ -46,4 +46,41 @@ var DeleteEntry = React.createClass({
     }
 });
 
-module.exports = DeleteEntry;
+var DeleteEntry = React.createClass({
+     toggle: function() {
+        $.ajax({
+            type: 'delete',
+            url: 'scot/api/v2/' + this.props.type + '/'  + this.props.id + '/entry/' + this.props.entryid,
+            success: function(data) {
+                console.log('success: ' + data);
+                this.props.updated();
+                this.props.deleteToggle();
+            }.bind(this),
+            error: function() {
+                alert('Failed to delete - contact administrator');
+                this.props.deleteToggle();
+            }.bind(this)
+        });
+    },
+    render: function() {
+        return (
+            <div>
+                <Modal isOpen={true} onRequestClose={this.props.deleteToggle} style={customStyles}>
+                    <div className='modal-header'>
+                        <img src='images/close_toolbar.png' className='close_toolbar' onClick={this.props.deleteToggle} />
+                        <h3 id='myModalLabel'>Are you sure you want to delete Entry: {this.props.entryid}?</h3>
+                    </div>
+                    <div className='modal-footer'>
+                        <Button id='cancel-delete' onClick={this.props.deleteToggle}>Cancel</Button>
+                        <Button bsStyle='danger' id='delete' onClick={this.toggle}>Delete</Button>
+                    </div>
+                </Modal>
+            </div>
+        )
+    }  
+});
+
+module.exports = {
+    DeleteEvent:DeleteEvent,
+    DeleteEntry:DeleteEntry
+}
