99a6f1d3b16d6093b343697f191251d358e3c4a9
angeor <angeor@sandia.gov>
2016-02-15 11:28:09 -0700

    Delete Entry Functionality

--- pubdev/jsdev/react_components/entry/selected_entry.jsx
+++ pubdev/jsdev/react_components/entry/selected_entry.jsx
@@ -4,7 +4,8 @@ var SplitButton     = require('react-bootstrap/lib/SplitButton.js');
 var DropdownButton  = require('react-bootstrap/lib/DropdownButton.js');
 var MenuItem        = require('react-bootstrap/lib/MenuItem.js');
 var Button          = require('react-bootstrap/lib/Button.js');
-var AddEntry        = require('../modal/add_entry.jsx');
+var AddEntryModal   = require('../modal/add_entry.jsx');
+var DeleteEntry     = require('../modal/delete.jsx').DeleteEntry;
 
 var SelectedEntry = React.createClass({
     getInitialState: function() {
@@ -19,11 +20,20 @@ var SelectedEntry = React.createClass({
             this.setState({showEntryData:true, entryData:entryResult})
         }.bind(this));
     },
+    updated: function () {
+        this.headerRequest = $.get('scot/api/v2/' + this.props.type + '/' + this.props.id + '/entry', function(result) {
+            var entryResult = result.records;
+            this.setState({showEntryData:true, entryData:entryResult})
+        }.bind(this));
+        console.log('Ran update');
+    },
     render: function() { 
         var data = this.state.entryData; 
+        var type = this.props.type;
+        var id = this.props.id;
         return (
             <div className="row-fluid entry-wrapper"> 
-                {this.state.showEntryData ? <EntryIterator data={data}/> : null}
+                {this.state.showEntryData ? <EntryIterator data={data} type={type} id={id} updated={this.updated} /> : null}
             </div>       
         );
     }
@@ -32,9 +42,12 @@ var SelectedEntry = React.createClass({
 var EntryIterator = React.createClass({
     render: function() {
         var rows = [];
-        var data = this.props.data 
+        var data = this.props.data;
+        var type = this.props.type;
+        var id = this.props.id; 
+        var updated = this.props.updated;
         data.forEach(function(data) {
-            rows.push(new Array(<EntryParent items={data}/>));
+            rows.push(new Array(<EntryParent items={data} type={type} id={id} updated={updated} />));
         });
         return (
             <div>
@@ -45,10 +58,33 @@ var EntryIterator = React.createClass({
 });
 
 var EntryParent = React.createClass({
+    getInitialState: function() {
+        return {
+            entryToolbar:false,   
+            deleteToolbar:false
+        }
+    },
+    entryToggle: function() {
+        if (this.state.entryToolbar == false) {
+            this.setState({entryToolbar:true})
+        } else {
+            this.setState({entryToolbar:false})
+        } 
+    },
+    deleteToggle: function() {
+        if (this.state.deleteToolbar == false) {
+            this.setState({deleteToolbar:true})
+        } else {
+            this.setState({deleteToolbar:false})
+        }
+    }, 
     render: function() {
         var itemarr = [];
         var subitemarr = [];
         var items = this.props.items;
+        var type = this.props.type;
+        var id = this.props.id; 
+        var updated = this.props.updated;
         itemarr.push(<EntryData subitem = {items} />);
         for (var prop in items) {
             childfunc(prop);
@@ -69,19 +105,21 @@ var EntryParent = React.createClass({
                     <div className="row-fluid entry-header todo_undefined">
                         <div className="entry-header-inner">[<a style={{color:'black'}} href={"#"+items.id}>{items.id}</a>] <ReactTime value={items.created * 1000} format="MM/DD/YYYY hh:mm:ss a" /> by {items.owner} (updated on <ReactTime value={items.updated * 1000} format="MM/DD/YYYY hh:mm:ss a" />)
                             <span className='pull-right'>
-                                <SplitButton bsSize='xsmall' title="Reply" key={items.id} id={'Reply '+items.id} onClick={<AddEntry />}>
-                                    <MenuItem eventKey='1' onClick={this.props.entryToggle}>Move</MenuItem>
-                                    <MenuItem eventKey='2'>Delete</MenuItem>
+                                <SplitButton bsSize='xsmall' title="Reply" key={items.id} id={'Reply '+items.id} onClick={this.entryToggle}>
+                                    <MenuItem eventKey='1' onClick={this.entryToggle}>Move</MenuItem>
+                                    <MenuItem eventKey='2' onClick={this.deleteToggle}>Delete</MenuItem>
                                     <MenuItem eventKey='3'>Mark as Summary</MenuItem>
                                     <MenuItem eventKey='4'>Make Task</MenuItem>
                                     <MenuItem eventKey='5'>Permissions</MenuItem>
                                 </SplitButton>
-                                <Button bsSize='xsmall'>Edit</Button>
+                                <Button bsSize='xsmall' onClick={this.entryToggle}>Edit</Button>
                             </span>
                         </div>
                     </div>
                 {itemarr}
-            </div>
+                </div>
+                {this.state.entryToolbar ? <AddEntryModal type={type} id={id} entryToggle={this.entryToggle} /> : null}
+                {this.state.deleteToolbar ? <DeleteEntry type={type} id={id} deleteToggle={this.deleteToggle} entryid={items.id} updated={updated} /> : null}
             </div>
         );
     }
