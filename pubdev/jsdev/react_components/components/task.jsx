fcd01ac243da062f9e46f6e82d28274cce864a58
angeor <angeor@sandia.gov>
2016-02-19 13:46:08 -0700

    Fixed bug; nested state update on task/summary
    Former-commit-id: 725ca270fdfef1fd30de8039b61e057113a78912

--- pubdev/jsdev/react_components/components/task.jsx
+++ pubdev/jsdev/react_components/components/task.jsx
@@ -19,20 +19,34 @@ var Task = React.createClass({
             whoami:''
         }
     },
-    componentDidMount: function () {
-        this.whoamiRequest = $.get('scot/api/v2/whoami', function (result) {
+    componentWillMount: function () {
+        $.get('scot/api/v2/whoami', function (result) {
             var result = result.user;
             this.setState({whoami:result})
-        }.bind(this));  
+        }.bind(this));
         this.taskRequest = $.get('scot/api/v2/entry/' + this.props.entryid, function(result) {
             var taskOwner = result.task.who;
             var taskStatus = result.task.status;
             this.setState({taskOwner:taskOwner, taskStatus:taskStatus})
         }.bind(this)); 
     },
+    makeTask: function () {
+        var json = {'make_task':1}
+        $.ajax({
+            type: 'put',
+            url: 'scot/api/v2/entry/' + this.props.entryid,
+            data: JSON.stringify(json),
+            success: function(data) {
+                console.log('success: ' + data);
+                this.props.updated();
+            }.bind(this),
+            error: function() {
+                alert('Failed to close task - contact administrator');
+            }.bind(this)
+        }); 
+    },
     closeTask: function() {
-        var taskData = {'status':'closed','who':this.state.whoami}
-        var json = {'task':taskData}
+        var json = {'close_task':1}
         $.ajax({
             type: 'put',
             url: 'scot/api/v2/entry/' + this.props.entryid,
@@ -46,9 +60,8 @@ var Task = React.createClass({
             }.bind(this)
         });
     },
-    toggleOwner: function() {
-        var taskData = {'who':this.state.whoami,'status':'open'}
-        var json = {'task':taskData};
+    takeTask: function() {
+        var json = {'take_task':1} 
         $.ajax({
             type: 'put',
             url: 'scot/api/v2/entry/' + this.props.entryid,
@@ -64,16 +77,25 @@ var Task = React.createClass({
     },
     render: function () {
         var taskDisplay = 'Task Loading...';
-        var onClick;
-        if (this.state.whoami != this.state.taskOwner) {
+        var onClick; 
+        if (this.state.taskStatus === undefined || this.state.taskStatus === null) {
+            taskDisplay = 'Make Task';
+            onClick = this.makeTask;
+        } else if (this.state.whoami != this.state.taskOwner && this.state.taskStatus == 'open') {
             taskDisplay = 'Assign task to me';
-            onClick = this.toggleOwner;
-         } else if (this.state.whoami == this.state.taskOwner && this.state.taskStatus == 'open') {
+            onClick = this.takeTask;
+        } else if (this.state.whoami == this.state.taskOwner && this.state.taskStatus == 'open') {
             taskDisplay = 'Close Task';
             onClick = this.closeTask;
-        } else if (this.state.whoami == this.state.taskOwner && this.state.taskStatus == 'closed') {
+        } else if (this.state.taskStatus == 'closed') {
+            taskDisplay = 'Reopen Task';
+            onClick = this.makeTask;
+        } else if (this.state.whoami == this.state.taskOwner && this.state.taskStatus == 'assigned') {
+            taskDisplay = 'Close Task';
+            onClick = this.closeTask;
+        } else if (this.state.whoami != this.state.taskowner && this.state.taskStatus == 'assigned') {
             taskDisplay = 'Assign task to me';
-            onClick = this.toggleOwner;
+            onClick = this.takeTask;
         }
         return (
             <span onClick={onClick}>{taskDisplay}</span>
