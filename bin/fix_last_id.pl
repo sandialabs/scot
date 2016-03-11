5a2c2103ec278cdae6290c6b57ffb60265248c93
Todd Bruner <tbruner@sandia.gov>
2016-03-11 12:31:08 -0700

    cmd line rest api interface, work on Mail bot, move to hypnotoad
    Former-commit-id: 93e80a4aa84b0dabb622379fd529ebf3f6b2711b

---
+++ bin/fix_last_id.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl
+
+use lib '../lib';
+use Scot::Env;
+
+my $env = Scot::Env->new();
+
+my $mongo = $env->mongo;
+
+foreach my $colname (qw(alertgroup alert checklist config entity entry event file guide history incident intel source tag user link audit)) {
+
+    print "Getting Max Id of $colname\n";
+    my $collection  = $mongo->collection(ucfirst($colname));
+
+    my $cursor  = $collection->find({});
+    $cursor->sort({id=>-1});
+    my $object  = $cursor->next;
+
+    unless ($object) {
+        print "! No object returned !\n";
+        next;
+    }
+
+    my $max     = $object->id;
+    print "Got max id of $max\n";
+
+    $collection->set_next_id($max);
+}
+
+
+
