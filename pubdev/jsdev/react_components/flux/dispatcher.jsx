3fdcc4fababe79179b6c732d545ee444ab442f47
85080 <rjeffer@sandia.gov>
2016-03-11 14:10:45 -0700

    Changed the url name from alert to alertgroup, implemented flux to the open,close, and promoted events

---
+++ pubdev/jsdev/react_components/flux/dispatcher.jsx
@@ -0,0 +1,15 @@
+var Dispatcher = require('../../../node_modules/flux').Dispatcher
+var assign = require('object-assign')
+
+var AppDispatcher = assign(new Dispatcher(), {
+	handleViewAction: function(action){
+	    this.dispatch({
+	    message: action.actionType,
+	    action: action
+	})
+    }
+})
+
+
+module.exports = AppDispatcher 
+
