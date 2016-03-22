b31c7e335b9e942404441bbb1372ecce7dae82a0
85080 <rjeffer@sandia.gov>
2016-03-22 16:20:42 -0600

    added activemq
    Former-commit-id: 1bdefa4aebc74a09dbdd98037edc1582a59d6c5a

---
+++ pubdev/jsdev/react_components/activemq/dispatcher.jsx
@@ -0,0 +1,12 @@
+var Dispatcher = require('../../../node_modules/flux-activemq/flux').Dispatcher
+var assign = require('object-assign')
+
+var Dispatcher = assign(new Dispatcher(), {
+    handleActivemq: function(action){
+    this.dispatch({
+    action: action
+    })
+    }
+})
+
+module.exports = Dispatcher
