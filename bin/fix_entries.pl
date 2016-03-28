ed65c37bcd5e7152efc7c194287a03cd2ba228f9
Todd Bruner <tbruner@sandia.gov>
2016-03-28 12:01:54 -0600

    fix to poorly written pgm
    Former-commit-id: e4a89288cb3d058ff608f0d4c3fcbeaeda8e770b

--- bin/fix_entries.pl
+++ bin/fix_entries.pl
@@ -21,19 +21,21 @@ while ( my $link = $links->next ) {
     my $pair    = $link->pair;
     my $entryid;
 
-    if ( $pair->[0]->type eq "entry" ) {
-        $entryid    = $pair->[0]->id + 0;
+    if ( $pair->[0]->{type} eq "entry" ) {
+        $entryid    = $pair->[0]->{id} + 0;
     }
     else {
-        $entryid    = $pair->[1]->id + 0;
+        $entryid    = $pair->[1]->{id} + 0;
     }
 
     my $entry   = $entryc->find_iid($entryid);
 
     $entry->update({
         '$set'   => {
-            id      => $id,
-            type    => $type,
+            target  => {
+                id      => $id,
+                type    => $type,
+            }
         }
     });
 }
