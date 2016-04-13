cf4f3f07118b75812582d4f15ba710dcb7326b06
angeor <angeor@sandia.gov>
2016-04-13 16:58:08 -0600

    Prettied code
    Former-commit-id: 492e54d475e1b8ce4a30dbdffad0f7a4693ecf53

--- pubdev/jsdev/react_components/components/add_flair.jsx
+++ pubdev/jsdev/react_components/components/add_flair.jsx
@@ -1,47 +1,77 @@
-        
-        function infopop(ifr,entityid,flairToolbarToggle) {
-            flairToolbarToggle(entityid);
-        }
-        function linkWarningPopup(ifr,url,linkWarningToggle) {
-            linkWarningToggle(url);
-        }
-        function checkFlairHover(iframe,flairToolbarToggle,type,linkWarningToggle) {
-            if(type != 'alertgroup') {
-                if(iframe.contentDocument != null) {
-                    $(iframe).contents().find('.entity').each(function(index, entity) {
-                        if($(entity).css('background-color') == 'rgb(255, 0, 0)') {
-                            $(entity).data('state', 'down');
-                        } else if ($(entity).data('state') == 'down') {
-                            $(entity).data('state', 'up');
-                            var entityid = $(entity).attr('data-entity-id');
-                            infopop(iframe,entityid,flairToolbarToggle);
+var AddFlair = {
+    entityUpdate: function(entityData,flairToolbarToggle,type,linkWarningToggle) {
+        setTimeout(function() {
+            var entityResult = entityData;
+            if (type != 'alertgroup') {
+                $('iframe').each(function(index,ifr) {
+                    //requestAnimationFrame waits for the frame to be rendered (allowing the iframe to fully render before excuting the next bit of code!!!
+                    ifr.contentWindow.requestAnimationFrame( function() {
+                        if(ifr.contentDocument != null) {
+                            var ifrContents = $(ifr).contents();
+                            if($(ifr.contentDocument.body).find('.extras')[0] == null) {
+                                //This makes all href point to blank so they don't reload the iframe
+                                $(ifr.contentDocument.body).find('a').attr('target','_blank');
+                                //Copies href to a new attribute, url, before we make href an anchor (so it doesn't go anywhere when clicked)
+                                ifrContents.find('a').each(function(index,a) {
+                                    var url = $(a).attr('href');
+                                    $(a).attr('url',url);
+                                }.bind(this))
+                                //Make href an anchor so it doesn't go anywhere when clicked and instead opens up the modal in linkWarningPopup
+                                $(ifr.contentDocument.body).find('a').attr('href','#');
+                                $(ifr.contentDocument.body).append('<iframe id="targ" style="display:none;" name="targ"></iframe>');
+                                $(ifr.contentDocument.body).find('a').find('.entity').wrap("<a href='about:blank' target='targ'></a>");
+                                ifrContents.find('.entity').each(function(index,entity){
+                                    var currentEntityValue = $(entity).attr('data-entity-value');
+                                    if (currentEntityValue !== undefined) {
+                                        if (entityResult[currentEntityValue.toLowerCase()] !== undefined ) {
+                                            var entityType = entityResult[currentEntityValue.toLowerCase()].type;
+                                            var entityid = entityResult[currentEntityValue.toLowerCase()].id;
+                                            var entityCount = entityResult[currentEntityValue.toLowerCase()].count;
+                                            var circle = $('<span class="noselect">');
+                                            circle.addClass('circleNumber');
+                                            circle.addClass('extras');
+                                            circle.text(entityCount);
+                                            $(entity).append(circle);
+                                            $(entity).attr('data-entity-id',entityid)
+                                            $(entity).unbind('click');
+                                        }
+                                    }
+                                }.bind(this));
+                            }
+                        pentry(ifr,flairToolbarToggle,type,linkWarningToggle);
+                        }
+                    }.bind(this));
+                }.bind(this));
+            } else {
+                if($(document.body).find('.extras')[0] == null) {
+                    $(document.body).find('a').attr('target','_blank');
+                    $(document.body).append('<iframe id="targ" style="display:none;" name="targ"></iframe>');
+                    $(document.body).find('a').find('.entity').wrap("<a href='about:blank' target='targ'></a>");
+                    $(document.body).find('.entity').each(function(index,entity){
+                        var currentEntityValue = $(entity).attr('data-entity-value');
+                        if (currentEntityValue !== undefined) {
+                            if (entityResult[currentEntityValue.toLowerCase()] !== undefined ) {
+                                var entityType = entityResult[currentEntityValue.toLowerCase()].type;
+                                var entityid = entityResult[currentEntityValue.toLowerCase()].id;
+                                var entityCount = entityResult[currentEntityValue.toLowerCase()].count;
+                                var circle = $('<span class="noselect">');
+                                circle.addClass('circleNumber');
+                                circle.addClass('extras');
+                                circle.text(entityCount);
+                                $(entity).append(circle);
+                                $(entity).attr('data-entity-id',entityid)
+                                $(entity).unbind('click');
+                                pentry(null,flairToolbarToggle,type);
+                            }
                         }
                     }.bind(this));
                 }
-            } else {
-               $(document.body).find('.entity').each(function(index, entity) {
-                        if($(entity).css('background-color') == 'rgb(255, 0, 0)') {
-                            $(entity).data('state', 'down');
-                        } else if ($(entity).data('state') == 'down') {
-                            $(entity).data('state', 'up');
-                            var entityid = $(entity).attr('data-entity-id');
-                            infopop(null,entityid,flairToolbarToggle);
-                        }
-                    }.bind(this));
             }
-            if(iframe.contentDocument != null) {
-                $(iframe).contents().find('a').each(function(index,a) {
-                    if($(a).css('color') == 'rgb(255, 0, 0)') {
-                        $(a).data('state','down');
-                    } else if ($(a).data('state') == 'down') {
-                        $(a).data('state','up');
-                        var url = $(a).attr('url');
-                        linkWarningPopup(iframe,url,linkWarningToggle);
-                    }
-                }.bind(this));
-            }
-        }
-        function pentry(ifr,flairToolbarToggle,type,linkWarningToggle) {
+        }.bind(this));
+    },
+}
+
+function pentry(ifr,flairToolbarToggle,type,linkWarningToggle) {
             if(type != 'alertgroup') { 
                 $(ifr).mouseenter(function() {
                     var intervalID = setInterval(checkFlairHover, 100, ifr, flairToolbarToggle,type,linkWarningToggle);
@@ -57,78 +87,48 @@
                 setInterval(checkFlairHover, 100, null, flairToolbarToggle,type,linkWarningToggle);
             }
         }
-    var AddFlair = {
-        entityUpdate: function(entityData,flairToolbarToggle,type,linkWarningToggle) {
-            setTimeout(function() {
-                var entityResult = entityData;
-                if (type != 'alertgroup') {
-                    $('iframe').each(function(index,ifr) {
-                        //requestAnimationFrame waits for the frame to be rendered (allowing the iframe to fully render before excuting the next bit of code!!!
-                        ifr.contentWindow.requestAnimationFrame( function() {
-                            if(ifr.contentDocument != null) {
-                                var ifrContents = $(ifr).contents();
-                                if($(ifr.contentDocument.body).find('.extras')[0] == null) {
-                                    //This makes all href point to blank so they don't reload the iframe
-                                    $(ifr.contentDocument.body).find('a').attr('target','_blank');
-                                    //Copies href to a new attribute, url, before we make href an anchor (so it doesn't go anywhere when clicked)
-                                    ifrContents.find('a').each(function(index,a) {
-                                        var url = $(a).attr('href');
-                                        $(a).attr('url',url);
-                                    }.bind(this))
-                                    //Make href an anchor so it doesn't go anywhere when clicked and instead opens up the modal in linkWarningPopup
-                                    $(ifr.contentDocument.body).find('a').attr('href','#');
-                                    $(ifr.contentDocument.body).append('<iframe id="targ" style="display:none;" name="targ"></iframe>');
-                                    $(ifr.contentDocument.body).find('a').find('.entity').wrap("<a href='about:blank' target='targ'></a>");
-                                    ifrContents.find('.entity').each(function(index,entity){
-                                        var currentEntityValue = $(entity).attr('data-entity-value');
-                                        if (currentEntityValue !== undefined) {
-                                            if (entityResult[currentEntityValue.toLowerCase()] !== undefined ) {
-                                                var entityType = entityResult[currentEntityValue.toLowerCase()].type;
-                                                var entityid = entityResult[currentEntityValue.toLowerCase()].id;
-                                                var entityCount = entityResult[currentEntityValue.toLowerCase()].count;
-                                                var circle = $('<span class="noselect">');
-                                                circle.addClass('circleNumber');
-                                                circle.addClass('extras');
-                                                circle.text(entityCount);
-                                                $(entity).append(circle);
-                                                $(entity).attr('data-entity-id',entityid)
-                                                $(entity).unbind('click');
-                                            }
-                                        }
-                                    }.bind(this));
-                                }
-                            pentry(ifr,flairToolbarToggle,type,linkWarningToggle);
-                            }
-                        }.bind(this));
-                    }.bind(this));
-                } else {
-                    if($(document.body).find('.extras')[0] == null) {
-                        $(document.body).find('a').attr('target','_blank');
-                        $(document.body).append('<iframe id="targ" style="display:none;" name="targ"></iframe>');
-                        $(document.body).find('a').find('.entity').wrap("<a href='about:blank' target='targ'></a>");
-                        $(document.body).find('.entity').each(function(index,entity){
-                            var currentEntityValue = $(entity).attr('data-entity-value');
-                            if (currentEntityValue !== undefined) {
-                                if (entityResult[currentEntityValue.toLowerCase()] !== undefined ) {
-                                    var entityType = entityResult[currentEntityValue.toLowerCase()].type;
-                                    var entityid = entityResult[currentEntityValue.toLowerCase()].id;
-                                    var entityCount = entityResult[currentEntityValue.toLowerCase()].count;
-                                    var circle = $('<span class="noselect">');
-                                    circle.addClass('circleNumber');
-                                    circle.addClass('extras');
-                                    circle.text(entityCount);
-                                    $(entity).append(circle);
-                                    $(entity).attr('data-entity-id',entityid)
-                                    $(entity).unbind('click');
-                                    pentry(null,flairToolbarToggle,type);
-                                }
-                            }
-                        }.bind(this));
-                    }
+function checkFlairHover(iframe,flairToolbarToggle,type,linkWarningToggle) {
+    if(type != 'alertgroup') {
+        if(iframe.contentDocument != null) {
+            $(iframe).contents().find('.entity').each(function(index, entity) {
+                if($(entity).css('background-color') == 'rgb(255, 0, 0)') {
+                    $(entity).data('state', 'down');
+                } else if ($(entity).data('state') == 'down') {
+                    $(entity).data('state', 'up');
+                    var entityid = $(entity).attr('data-entity-id');
+                    infopop(iframe,entityid,flairToolbarToggle);
                 }
             }.bind(this));
-        },
+        }
+    } else {
+        $(document.body).find('.entity').each(function(index, entity) {
+                if($(entity).css('background-color') == 'rgb(255, 0, 0)') {
+                    $(entity).data('state', 'down');
+                } else if ($(entity).data('state') == 'down') {
+                    $(entity).data('state', 'up');
+                    var entityid = $(entity).attr('data-entity-id');
+                    infopop(null,entityid,flairToolbarToggle);
+                }
+            }.bind(this));
     }
-
+    if(iframe.contentDocument != null) {
+        $(iframe).contents().find('a').each(function(index,a) {
+            if($(a).css('color') == 'rgb(255, 0, 0)') {
+                $(a).data('state','down');
+            } else if ($(a).data('state') == 'down') {
+                $(a).data('state','up');
+                var url = $(a).attr('url');
+                linkWarningPopup(iframe,url,linkWarningToggle);
+            }
+        }.bind(this));
+    }
+}
+        
+function infopop(ifr,entityid,flairToolbarToggle) {
+    flairToolbarToggle(entityid);
+}
+function linkWarningPopup(ifr,url,linkWarningToggle) {
+    linkWarningToggle(url);
+}
 
 module.exports = AddFlair
