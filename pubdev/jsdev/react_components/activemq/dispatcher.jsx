e8f6f8a6a0eeb723eaeaa9471ffbd72234cb0c59
85080 <rjeffer@sandia.gov>
2016-04-18 14:31:33 -0600

    Reformatted code, fixed view entry modal
    Former-commit-id: 0c61ed9cd72515cc309153ab11feb1ed6297f75e

--- pubdev/jsdev/react_components/activemq/dispatcher.jsx
+++ pubdev/jsdev/react_components/activemq/dispatcher.jsx
@@ -1,10 +1,10 @@
-var Dispatcher = require('../../../node_modules/flux-activemq/flux').Dispatcher
-var assign = require('object-assign')
+var Dispatcher  = require('../../../node_modules/flux-activemq/flux').Dispatcher
+var assign      = require('object-assign')
 
 var Dispatcher = assign(new Dispatcher(), {
     handleActivemq: function(action){
     this.dispatch({
-    action: action
+        action: action
     })
     }
 })
