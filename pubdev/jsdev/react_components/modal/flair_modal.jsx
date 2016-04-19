cc100dbca7956a5ea9d282cadd7ce69364ce4ea4
angeor <angeor@sandia.gov>
2016-04-19 16:07:43 -0600

    Selected now works in entities. Added libs

--- pubdev/jsdev/react_components/modal/flair_modal.jsx
+++ pubdev/jsdev/react_components/modal/flair_modal.jsx
@@ -40,7 +40,7 @@ var Flair = React.createClass({
                         <img src="/images/close_toolbar.png" className="close_toolbar" onClick={this.props.flairToolbarToggle} />
                         <h3 id="myModalLabel">Entity {this.state.entityData != null ? <EntityValue value={this.state.entityData.value} /> :null }</h3>
                     </div>
-                    <div className="modal-body" style={{height: '75vh', overflowY:'auto',width:'800px'}}>
+                    <div className="modal-body" style={{height: '80vh', overflowY:'auto',width:'800px'}}>
                         <EntityBody data={this.state.entityData} entityid={this.props.entityid}/> 
                     </div>
                     <div className="modal-footer">
@@ -106,6 +106,7 @@ var EntityEventReferences = React.createClass({
             entityDataIncidentLoading:true,
             navigateType: '',
             navigateId: null,
+            selected:{}
         }
     },
     componentDidMount: function() {
@@ -113,41 +114,35 @@ var EntityEventReferences = React.createClass({
             var result = result.records
             this.setState({entityDataAlertGroup:result,entityDataAlertGroupLoading:false})
             if (result[0] != undefined) {
-                this.setState({defaultAlertGroupHeight:200})
+                this.setState({defaultAlertGroupHeight:175})
             }
         }.bind(this));
         this.sourceRequest = $.get('scot/api/v2/entity/' + this.props.entityid + '/event', function(result) {
             var result = result.records
             this.setState({entityDataEvent:result,entityDataEventLoading:false})
             if (result[0] != undefined) {
-                this.setState({defaultEventHeight:200})
+                this.setState({defaultEventHeight:175})
             }
         }.bind(this));
         this.sourceRequest = $.get('scot/api/v2/entity/' + this.props.entityid + '/incident', function(result) {
             var result = result.records
             this.setState({entityDataIncident:result,entityDataIncidentLoading:false})
             if (result[0] != undefined) {
-                this.setState({defaultIncidentHeight:200})
+                this.setState({defaultIncidentHeight:175})
             }
         }.bind(this));
     },
     onAlertGroupSelectionChange: function(newSelectedId, data) {
-        for(prop in newSelectedId) {
-            this.setState({navigateType:'alertgroup',navigateId:prop})
-        } 
+        this.setState({navigateType:'alertgroup',navigateId:data[0].alertgroup,selected:newSelectedId}) 
     },
     onEventSelectionChange: function(newSelectedId, data) {
-        for(prop in newSelectedId) {
-            this.setState({navigateType:'event',navigateId:prop})
-        }
+        this.setState({navigateType:'event',navigateId:data[0].id,selected:newSelectedId})
     },
     onIncidentSelectionChange: function(newSelectedId, data) {
-        for(prop in newSelectedId) {
-            this.setState({navigateType:'incident',navigateId:prop})
-        }
+        this.setState({navigateType:'incident',navigateId:data[0].id,selected:newSelectedId})
     },
     viewId: function() {
-        window.location.assign('#/'+this.state.navigateType+'/'+this.state.navigateId);
+        window.open('#/'+this.state.navigateType+'/'+this.state.navigateId);
     },
     render: function() {
         const rowFact = (rowProps) => {
@@ -165,14 +160,14 @@ var EntityEventReferences = React.createClass({
         return (
             <div>
                 <h4>AlertGroups</h4>
-                <DataGrid idProperty='alertgroup' dataSource={this.state.entityDataAlertGroup} columns={alertColumns} style={{height:this.state.defaultAlertGroupHeight}} onSelectionChange={this.onAlertGroupSelectionChange} selected={this.state.entityDataAlertGroup} emptyText={'No records'} loading={this.state.entityDataAlertGroupLoading} rowFactory={rowFact} loadMaskOverHeader={false}/>
+                <DataGrid idProperty='id' dataSource={this.state.entityDataAlertGroup} columns={alertColumns} style={{height:this.state.defaultAlertGroupHeight}} onSelectionChange={this.onAlertGroupSelectionChange} selected={this.state.selected} emptyText={'No records'} loading={this.state.entityDataAlertGroupLoading} rowFactory={rowFact} loadMaskOverHeader={false}/>
                 <div style={{marginTop:'90px'}}>
                     <h4>Events</h4>
-                    <DataGrid idProperty='id' dataSource={this.state.entityDataEvent} columns={columns} style={{height:this.state.defaultEventHeight}} onSelectionChange={this.onEventSelectionChange} selected={this.state.entityDataEvent} emptyText={'No records'} loading={this.state.entityDataEventLoading} rowFactory={rowFact} loadMaskOverHeader={false}/>
+                    <DataGrid idProperty='id' dataSource={this.state.entityDataEvent} columns={columns} style={{height:this.state.defaultEventHeight}} onSelectionChange={this.onEventSelectionChange} selected={this.state.selected} emptyText={'No records'} loading={this.state.entityDataEventLoading} rowFactory={rowFact} loadMaskOverHeader={false}/>
                 </div>
                 <div style={{marginTop:'90px'}}>
                     <h4>Incidents</h4>
-                    <DataGrid idProperty='id' dataSource={this.state.entityDataIncident} columns={columns} style={{height:this.state.defaultIncidentHeight}} onSelectionChange={this.onIncidentSelectionChange} selected={this.state.entityDataIncident} emptyText={'No records'} loading={this.state.entityDataIncidentLoading} rowFactory={rowFact} loadMaskOverHeader={false}/>
+                    <DataGrid idProperty='id' dataSource={this.state.entityDataIncident} columns={columns} style={{height:this.state.defaultIncidentHeight}} onSelectionChange={this.onIncidentSelectionChange} selected={this.state.selected} emptyText={'No records'} loading={this.state.entityDataIncidentLoading} rowFactory={rowFact} loadMaskOverHeader={false}/>
                 </div>
             </div>
         )
---
+++ pubdev/node_modules/react-bootstrap-table/node_modules/react-toastr/README.md
@@ -0,0 +1,154 @@
+# react-toastr
+> React.js toastr component
+
+[![Version][npm-image]][npm-url] [![Travis CI][travis-image]][travis-url] [![Quality][codeclimate-image]][codeclimate-url] [![Coverage][codeclimate-coverage-image]][codeclimate-coverage-url] [![Dependencies][gemnasium-image]][gemnasium-url] [![Gitter][gitter-image]][gitter-url]
+
+
+## Installation
+
+```sh
+npm i --save react-toastr
+```
+
+
+## Demo
+
+Static hosted [demo site][demo] on GitHub.
+
+
+## Example
+
+Please refer to [examples/gh-pages][examples/gh-pages] folder for example.
+
+
+## Usage
+
+This module requires to be bundled with [webpack][webpack]/browserify and loads `react/addons` internally.  
+You'll need to download animate.css from here [Animate @github](https://raw.github.com/daneden/animate.css/master/animate.css)
+
+Link to css/js properly..
+```html
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
+<link rel="stylesheet" href="/static/dist/css/animate.css">
+
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+```
+
+WIP: release a UMD version via bower/components.
+
+Then:
+
+```javascript
+var ReactToastr = require("react-toastr");
+var {ToastContainer} = ReactToastr; // This is a React Element.
+// For Non ES6...
+// var ToastContainer = ReactToastr.ToastContainer;
+var ToastMessageFactory = React.createFactory(ReactToastr.ToastMessage.animation);
+
+  // In a react component:
+  render () {
+    return (
+      <div>
+        <ToastContainer ref="container"
+                        toastMessageFactory={ToastMessageFactory}
+                        className="toast-top-right" />
+        <button onClick={this.addAlert}>GGininder</button>
+      </div>
+    );
+  }
+```
+
+Here's a list of React Elements:
+
+### ToastContainer
+
+This is the container where all `ToastMessage` elements will go. Use it by retaining a [ref][react-ref] to publish a new **toast message**:
+
+```javascript
+  addAlert () {
+    this.refs.container.success(
+      "Welcome welcome welcome!!",
+      "You are now home my friend. Welcome home my friend.", {
+      timeOut: 30000,
+      extendedTimeOut: 10000
+    });
+    window.open("http://youtu.be/3SR75k7Oggg");
+  }
+```
+
+or integrated with your [flux][flux] architecture?
+
+```javascript
+  componentDidMount: function() {
+    InInDerStore.addChangeListener(this.addAlert);
+  }
+```
+
+#### Options
+
+Directly migrated from `toastr.js` library, and can be overrided via `props` in a React way:
+
+[`ToastContainer::getDefaultProps`](http://git.io/RagItA)
+
+
+### ToastMessage
+
+Base class for holding a toast message, it will not animate in and out during it's lifecycle.
+Provides **`ToastMessage.animation`** and `ToastMessage.jQuery` for your choice.
+
+#### Options
+
+Directly migrated from `toastr.js` library, and can be overrided via `props` in a React way:
+
+* [`ToastMessage::getDefaultProps`](http://git.io/90CzSA)
+* [`ToastMessage.animation::getDefaultProps`](http://git.io/vU2sz)
+  Credits go to **@Janekk**
+* [`ToastMessage.jQuery::getDefaultProps`](http://git.io/YcbXvA)
+
+
+## Development
+
+```shell
+git clone ...
+npm install
+cd examples/gh-pages
+npm install
+npm start
+```
+
+Then open [http://localhost:8080](http://localhost:8080).
+
+
+## Contributing
+
+[![devDependency Status][david-dm-image]][david-dm-url]
+
+1. Fork it
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create new Pull Request
+
+
+[npm-image]: https://img.shields.io/npm/v/react-toastr.svg?style=flat-square
+[npm-url]: https://www.npmjs.org/package/react-toastr
+
+[travis-image]: https://img.shields.io/travis/tomchentw/react-toastr.svg?style=flat-square
+[travis-url]: https://travis-ci.org/tomchentw/react-toastr
+[codeclimate-image]: https://img.shields.io/codeclimate/github/tomchentw/react-toastr.svg?style=flat-square
+[codeclimate-url]: https://codeclimate.com/github/tomchentw/react-toastr
+[codeclimate-coverage-image]: https://img.shields.io/codeclimate/coverage/github/tomchentw/react-toastr.svg?style=flat-square
+[codeclimate-coverage-url]: https://codeclimate.com/github/tomchentw/react-toastr
+[gemnasium-image]: https://img.shields.io/gemnasium/tomchentw/react-toastr.svg?style=flat-square
+[gemnasium-url]: https://gemnasium.com/tomchentw/react-toastr
+[gitter-image]: https://badges.gitter.im/Join%20Chat.svg
+[gitter-url]: https://gitter.im/tomchentw/react-toastr?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge
+[david-dm-image]: https://img.shields.io/david/dev/tomchentw/react-toastr.svg?style=flat-square
+[david-dm-url]: https://david-dm.org/tomchentw/react-toastr#info=devDependencies
+
+
+[demo]: http://tomchentw.github.io/react-toastr/
+[examples/gh-pages]: https://github.com/tomchentw/react-toastr/tree/master/examples/gh-pages
+[webpack]: http://webpack.github.io/docs/tutorials/getting-started/
+[react-ref]: http://facebook.github.io/react/docs/more-about-refs.html
+[flux]: http://facebook.github.io/flux/docs/overview.html
