88728ef85de2578e8b88dc76ab00c26699a0bd0c
85080 <rjeffer@sandia.gov>
2016-05-11 18:01:09 -0600

    Added guide file
    Former-commit-id: 51c35716c27ad4a331207f0f098ef4a792a033b0

---
+++ pubdev/jsdev/react_components/tab-components/guide.jsx
@@ -0,0 +1,380 @@
+'use strict';
+
+var React                   = require('react')
+var SelectedContainer       = require('../entry/selected_container.jsx')
+var Notificationactivemq    = require('../../../node_modules/react-notification-system')
+var Search                  = require('../components/esearch.jsx')
+var Store                   = require('../activemq/store.jsx')
+var Page                    = require('../components/paging.jsx')
+var Popover                 = require('react-bootstrap/lib/Popover')
+var OverlayTrigger          = require('react-bootstrap/lib/OverlayTrigger')
+var ButtonToolbar           = require('react-bootstrap/lib/ButtonToolbar')
+var DateRangePicker         = require('../../../node_modules/react-daterange-picker')
+var Source                  = require('react-tag-input-tags/react-tag-input').WithContext
+var Tags                    = require('react-tag-input').WithContext
+var SORT_INFO;
+var colsort = "id"
+var start;
+var end;
+var valuesort = -1
+var SELECTED_ID = {}
+var filter = {}
+var sortarray = {}
+var names = 'none'
+var getColumn;
+var tab;
+var datasource
+var ids = []
+var stage = false
+var savedsearch = false
+var savedfsearch;
+var setfilter = false
+var savedid;
+var height;
+var width;
+var pageSize = 50;
+var readonly = []
+var colorrow = [];
+sortarray[colsort] = -1
+var columns = ['ID', 'Subject']
+
+module.exports = React.createClass({
+
+    getInitialState: function(){
+        var scrollHeight = $(window).height() - 170
+        var scrollWidth  = '650px'  
+        width = 650
+
+    return {
+            unbold: '', bold: 'bold', white: 'white', blue: '#AEDAFF',
+            idtext: '', totalcount: 0, activepage: 0,
+            subjecttext:'', idsarray: [], scrollheight: scrollHeight, 
+            scrollwidth: scrollWidth, reload: false, 
+            viewfilter: false, viewevent: false, showevent: true, objectarray:[], csv:true,fsearch: ''};
+    },
+
+    componentWillMount: function(){
+        var array = []
+        if(this.props.ids !== undefined){
+            if(this.props.ids.length > 0){
+                array = this.props.ids
+                stage = true
+                }
+          }
+        var finalarray = [];
+	    var sortarray = {}
+	    sortarray[colsort] = -1
+        Store.storeKey('guidegroup')
+        Store.addChangeListener(this.reloadactive)
+        $.ajax({
+	        type: 'GET',
+	        url: '/scot/api/v2/guide',
+	        data: {
+	            limit: 50,
+	            offset: 0,
+	            sort:  JSON.stringify(sortarray),
+	            match: JSON.stringify(filter)
+	        }
+	    }).then(function(response){
+  	        datasource = response	
+	        $.each(datasource.records, function(key, value){
+	            finalarray[key] = {}
+	            $.each(value, function(num, item){
+	                if(num == 'created' || num == 'updated' || num == 'discovered' || num == 'occurred' || num == 'reported')
+	                {
+	                    var date = new Date(1000 * item)
+	                    finalarray[key][num] = date.toLocaleString()
+	                }
+	                else{
+	                    finalarray[key][num] = item
+	                }
+	    })
+	    })
+        this.setState({idsarray: array, objectarray: finalarray,totalcount: response.totalRecordCount})
+        }.bind(this))
+    },
+
+    reloadactive: function(){    
+        var notification = this.refs.notificationSystem
+        if(notification != undefined && activemqwho != "" &&  activemqwho != 'api'){
+            notification.addNotification({
+                message: activemqwho + activemqmessage + activemqid,
+                level: 'info',
+                autoDismiss: 5,
+                action: {
+                    label: 'View',
+                    callback: function(){
+                        window.open('#/' + activemqtype + '/' + activemqid)
+                    }
+                }
+            })
+            savedid = activemqid
+        }  
+        this.getNewData(this.state.activepage) 
+    },
+    reloadItem: function(){
+        height = $(window).height() - 170
+        width = width + 40
+        $('.container-fluid').css('height', height) 
+        $('.container-fluid').css('max-height', height)
+        $('.container-fluid').css('max-width', '915px')
+        $('.container-fluid').css('width', width)
+    },
+    launchEvent: function(array){
+        stage = true
+        this.setState({idsarray:array})
+
+    },
+    render: function() {
+        var styles;
+	    if(this.state.viewfilter){
+	        styles = {'border-radius': '0px'}
+	    }
+	    else {
+	        styles = {'border-radius': '0px'}
+	    }
+	    $('.z-table').each(function(key, value){
+	        $(value).find('.z-cell').each(function(x,y){
+	            $(y).css('overflow', 'auto')
+	        })
+	    })
+
+        setTimeout(function(){
+            $('.allevents').find('.table-row').each(function(key, value){
+                $(value).find('.colorstatus').each(function(x,y){
+                    if($(y).text() == 'open'){
+                        $(y).css('color', 'red')
+                    }
+                    else if($(y).text() == 'closed'){
+                        $(y).css('color', 'green')
+                    }
+        else if($(y).text()  == 'promoted'){
+            $(y).css('color', 'orange')
+        }
+        })
+        })
+
+        }.bind(this),100)
+                
+        $(window).resize(function(){
+            this.reloadItem()
+        }.bind(this))
+        return (
+            React.createElement("div", {className: "allComponents", style: {'margin-left': '17px'}}, 
+                React.createElement('div', null, 
+                    React.createElement(Notificationactivemq, {ref: 'notificationSystem'})), 
+                        React.createElement("div", {className: 'entry-header-info-null', style: {'padding-bottom': '55px',width:'100%'}}, 
+                        React.createElement("div", {style: {top: '1px', 'margin-left': '10px', float:'left', 'text-align':'center', position: 'absolute'}}, 
+                        React.createElement('h2', {style: {'font-size': '30px'}}, 'Guide')), 
+                        React.createElement("div", {style: {float: 'right', right: '100px', left: '50px','text-align': 'center', position: 'absolute', top: '9px'}}, 
+                        React.createElement('h2', {style: {'font-size': '19px'}}, 'OUO')), 
+                        React.createElement(Search, null)),                        
+                        React.createElement('btn-group', {style: {'padding-left': '0px'}}, 
+                        React.createElement('button', {className: 'btn btn-default', onClick: this.clearAll, style: styles}, 'Clear All Filters'),
+                        React.createElement('button', {className: 'btn btn-default', onClick: this.exportCSV, style: styles}, 'Export to CSV')),
+            React.createElement('div', {className: 'guidewidth', style: {display:'flex'}},
+            React.createElement('div', null,  
+            React.createElement('div', {style:{display: 'flex'}},
+                React.createElement("div", {className: "container-fluid", style: {'max-width': '915px',resize:'horizontal','min-width': '650px', width:this.state.scrollwidth, 'max-height': this.state.scrollheight, 'margin-left': '0px',height: this.state.scrollheight, overflow: 'auto', 'padding-left':'5px'}}, 
+                    React.createElement("div", {className: "table-row header"},
+                        React.createElement("div", {className: "wrapper attributes"}, 
+                        React.createElement('div', {className: 'wrapper status-owner-severity'},
+                        React.createElement('div', {className: 'wrapper status-owner'}, 
+                        React.createElement(ButtonToolbar, {style: {'padding-left': '5px'}}, React.createElement(OverlayTrigger, {ref: 'myPopOverid', trigger:['click','focus'], placement:'bottom', rootClose: true, overlay: React.createElement(Popover, null, 
+                        React.createElement('div', {className: 'Filter and Sort', id: 'idheader'}, React.createElement('div',
+                        {style: {display: 'inline-flex'}}, React.createElement('div', null, 'ID'), React.createElement('div', 
+                        {style:{'padding-left': '100px'}}, 'Sort'), 
+                        React.createElement('btn-group', null, 
+                        React.createElement('button', {style: {height:'5px'},value: 'id', id: 1, onClick: this.handlesort, className: 'sort glyphicon glyphicon-triangle-top'}),
+                        React.createElement('button', {className: 'sort glyphicon glyphicon-triangle-bottom', value: 'id', onClick: this.handlesort, id: -1, style:{height:'5px'}}))),
+                        React.createElement('input',  {autoFocus: true, id:'id',onKeyUp: this.filterUp, defaultValue: this.state.idtext, placeholder: 'Search', style: {background: 'white', width: '200px'}, type:'text', className:'idinput'}), 
+                        React.createElement('btn-group', null, 
+                        React.createElement('button', {className:'btn btn-default clear', value: 'id', onClick: this.filterclear}, 'Clear'),
+                        React.createElement('button', {value: 'id',className:'filter btn btn-default', onClick: this.handlefilter}, 'Filter')))
+                        )}, 
+                        React.createElement('div',{className: 'column index'}, 'ID'))))),
+
+                        React.createElement("div", {className: "wrapper title-comment-module-reporter"}, 
+                        React.createElement(ButtonToolbar, {style: {'padding-left': '5px'}}, 
+                        React.createElement(OverlayTrigger, {trigger:['click','focus'], placement:'bottom', ref: 'myPopOversubject', rootClose: true, overlay: React.createElement(Popover, null, 
+                        React.createElement('div', {className: 'Filter and Sort', id: 'subjectheader'}, React.createElement('div',
+                        {style: {display: 'inline-flex'}}, React.createElement('div', null, 'Subject'), React.createElement('div', 
+                        {style:{'padding-left': '80px'}}, 'Sort'), 
+                        React.createElement('btn-group', null, 
+                        React.createElement('button', {style: {height:'5px'},value: 'subject', id: 1, onClick: this.handlesort, className: 'sort glyphicon glyphicon-triangle-top'}),
+                        React.createElement('button', {className: 'glyphicon glyphicon-triangle-bottom sort', value: 'subject', id: -1, onClick: this.handlesort, style:{height:'5px'}}))),
+                        React.createElement('input', {autoFocus: true, id: 'subject',onKeyUp: this.filterUp, defaultValue:this.state.subjecttext, placeholder: 'Search', style: {background: 'white', width: '200px'}, type:'text', className:'subjectinput'}), 
+                        React.createElement('btn-group', null, 
+                        React.createElement('button', {className:'btn btn-default clear', value: 'subject', onClick: this.filterclear}, 'Clear'),
+                        React.createElement('button', {className:'btn btn-default filter', value: 'subject', onClick: this.handlefilter}, 'Filter')))
+                        )},
+                            React.createElement("div", {className: "wrapper title-comment"}, 
+                            React.createElement("div", {className: "column title"}, "Subject")))))
+                        
+                    )
+                    ), 
+
+                    this.state.objectarray.map((value) => React.createElement('div', {className:'allevents', id: value.id}, 
+                        React.createElement(ButtonToolbar, {style: {'padding-left': '5px'}}, React.createElement(OverlayTrigger, {trigger:['hover', 'focus'], placement:'top', positionTop: 50, title: value.id, style: {overflow: 'auto'}, overlay: React.createElement(Popover, null, 
+                        React.createElement('div', null,
+                        React.createElement('div', {style: {display:'flex'}}, React.createElement('div', {style: {'font-weight': 'bold'}}, 'ID:'),
+                        React.createElement('div', null, value.id)),
+                         React.createElement('div', {style: {display:'flex'}},
+                        React.createElement('div', {style: {'font-weight': 'bold'}}, 'Subject:  '), React.createElement('div', null, value.subject))
+                        ))}, 
+                        React.createElement("div", {style: {background: this.state.idsarray[0] == value.id ? this.state.blue : this.state.white},onClick: this.clickable, className: "table-row", id: value.id}, 
+                        React.createElement("div", {className: "wrapper attributes"},
+                        React.createElement('div', {className: 'wrapper status-owner-severity'},
+                        React.createElement('div', {className: 'wrapper status-owner'}, 
+                            React.createElement("div", {className: 'column index'}, value.id))),
+                        React.createElement("div", {className: "wrapper title-comment-module-reporter"}, 
+                            React.createElement("div", {className: "wrapper title-comment"},  
+                            React.createElement("div", {className: "column title"}, value.subject) 
+                            )
+                        )
+                        )
+                    )
+                    )
+                    )
+                    )))), 
+                        React.createElement(Page, {paginationToolbarProps: { pageSizes: [5, 20, 100]}, pagefunction: this.getNewData, defaultPageSize: 50, count: this.state.totalcount, pagination: true})) , stage ? 
+                        React.createElement(SelectedContainer, {height: height - 117,ids: this.state.idsarray, type: 'guide'}) : null) 
+
+        ));
+    },
+    clearAll: function(){
+        sortarray['id'] = -1
+        filter = {}
+        this.setState({idtext: '',subjecttext: ''})
+            this.getNewData({page: 0, limit: pageSize})
+    },
+    filterUp: function(v){
+        if(v.keyCode == 13){
+            if($($(v.currentTarget).find('.idinput').context).attr('id') == 'id'){
+                filter['id'] = [$('.idinput').val()]
+                this.refs.myPopOverid.hide()
+                this.setState({idtext: $('.idinput').val()})
+            }
+            else if($($(v.currentTarget).find('.subjectinput').context).attr('id') == 'subject'){
+                filter['subject'] = $('.subjectinput').val()
+                this.refs.myPopOversubject.hide()
+                this.setState({subjecttext: $('.subjectinput').val()})
+            }
+            this.getNewData({page: 0, limit: pageSize})
+        }
+    },
+    clickable: function(v){
+        $('#'+$(v.currentTarget).find('.index').text()).find('.table-row').each(function(x,y){
+            var array = []
+            array.push($(y).attr('id'))
+            colorrow.push($(y).attr('id'))
+            this.launchEvent(array)
+        }.bind(this))
+    },
+
+    handlePageChange: function(pageNumber){
+        this.getNewData(pageNumber)
+    },
+    getNewData: function(page){
+        pageSize = page.limit
+        var newPage;
+        if(page.page != 0){
+            newPage = (page.page - 1) * page.limit
+        }
+        else {
+               page.limit = pageSize
+               newPage = 0
+        }
+        var newarray = []
+        $.ajax({
+	        type: 'GET',
+	        url: '/scot/api/v2/guide',
+	        data: {
+	            limit:  page.limit,
+	            offset: newPage,
+	            sort:  JSON.stringify(sortarray),
+	            match: JSON.stringify(filter)
+	        }
+	    }).then(function(response){
+  	        datasource = response	
+	        $.each(datasource.records, function(key, value){
+	            newarray[key] = {}
+	            $.each(value, function(num, item){
+	                if(num == 'created' || num == 'updated' || num == 'discovered' || num == 'occurred' || num == 'reported')
+	                {
+	                    var date = new Date(1000 * item)
+	                    newarray[key][num] = date.toLocaleString()
+	                }
+	                else{
+	                    newarray[key][num] = item
+	                }
+	            })
+	        })
+                this.setState({totalcount: response.totalRecordCount, activepage: page, objectarray: newarray})
+        }.bind(this))
+    },
+    reloadCSS: function(){
+        this.setState({})
+    },
+    exportCSV: function(){
+        var keys = []
+	    $.each(columns, function(key, value){
+            keys.push(value);
+	    });
+	    var csv = ''
+    	$('.allevents').find('.table-row').each(function(key, value){
+	        var storearray = []
+            $(value).find('.column').each(function(x,y) {
+                var obj = $(y).text()
+		        obj = obj.replace(/,/g,'|')
+		        storearray.push(obj)
+	    });
+	        csv += storearray.join() + '\n'
+	    });
+        var result = keys.join() + "\n"
+	    csv = result + csv;
+	    var data_uri = 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv)
+	    window.open(data_uri)		
+    },
+    handlesort : function(v){
+         if($($(v.currentTarget).find('.sort').context).attr('value') == 'id'){
+            sortarray['id'] = Number($($(v.currentTarget).find('.sort').context).attr('id')) 
+            this.refs.myPopOverid.hide()
+        }
+        else if($($(v.currentTarget).find('.sort').context).attr('value') == 'subject'){
+            sortarray['subject'] = Number($($(v.currentTarget).find('.sort').context).attr('id')) 
+            this.refs.myPopOversubject.hide()
+        }
+        this.getNewData({page:0, limit:pageSize})   
+	},
+
+    filterclear: function(v){
+        if($($(v.currentTarget).find('.clear').context).attr('value') == 'id'){
+            delete filter.id
+            this.refs.myPopOverid.hide()
+            this.setState({idtext: ''})
+        }
+        else if($($(v.currentTarget).find('.clear').context).attr('value') == 'subject'){
+            delete filter.subject
+            this.refs.myPopOversubject.hide()
+            this.setState({subjecttext: ''})
+        }
+        this.getNewData({page:0, limit: pageSize})
+    },
+
+    handlefilter: function(v){
+        if($($(v.currentTarget).find('.filter').context).attr('value') == 'id'){
+            filter['id'] = [$('.idinput').val()]
+            this.refs.myPopOverid.hide()
+            this.setState({idtext: $('.idinput').val()})
+        }
+        else if($($(v.currentTarget).find('.filter').context).attr('value') == 'subject'){
+            filter['subject'] = $('.subjectinput').val()
+            this.refs.myPopOversubject.hide()
+            this.setState({subjecttext: $('.subjectinput').val()})
+        }
+        this.getNewData({page: 0, limit: pageSize})
+    }
+    
+});
+
