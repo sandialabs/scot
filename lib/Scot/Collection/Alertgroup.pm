e3108d564a926ee47d77670ae28b4c83c0ef333b
Todd Bruner <tbruner@sandia.gov>
2016-05-16 10:57:44 -0600

    fixes incorrect entities being returned by subthing query

--- lib/Scot/Collection/Alertgroup.pm
+++ lib/Scot/Collection/Alertgroup.pm
@@ -224,7 +224,7 @@ override get_subthing => sub {
             id => $id, type => 'alertgroup' 
         });
         &$ft;
-        my @lnk = map { $_->{id} } $cur->all;
+        my @lnk = map { $_->{entity_id} } $cur->all;
         &$timer;
 
         $timer  = $env->get_timer("generating entity cursor");
--- lib/Scot/Collection/Intel.pm
+++ lib/Scot/Collection/Intel.pm
@@ -93,7 +93,7 @@ override get_subthing => sub {
             id => $id, type => 'intel' 
         });
         &$ft;
-        my @lnk = map { $_->id } $cur->all;
+        my @lnk = map { $_->{entity_id} } $cur->all;
         &$timer;
 
         $timer  = $env->get_timer("generating entity cursor");
