48318c26c32b8beb6f8b9ef83f0d6b0a60e76190
Todd Bruner <tbruner@sandia.gov>
2016-05-16 16:42:38 -0600

    partial fix for #47

--- lib/Scot/Role/Tags.pm
+++ lib/Scot/Role/Tags.pm
@@ -3,14 +3,14 @@ package Scot::Role::Tags;
 use Moose::Role;
 use namespace::autoclean;
 
-=item B<tags>
+=item B<tag>
 
 Tags may be applied to consuming object.
 Tags are in their own collection with a targets array 
 
 =cut
 
-has tags    => (
+has tag    => (
     is          => 'ro',
     isa         => 'ArrayRef',
     required    => 1,
