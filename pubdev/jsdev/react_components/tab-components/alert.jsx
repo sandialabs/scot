3ffa0e84ec3a1a7f38327dd685ac9bc738f05283
85080 <rjeffer@sandia.gov>
2016-05-17 08:07:31 -0600

    Change router config
    Former-commit-id: d72ae7a98acc999be5c215b0d8edba1eb7dbaf9f

--- pubdev/jsdev/react_components/tab-components/alert.jsx
+++ pubdev/jsdev/react_components/tab-components/alert.jsx
@@ -507,6 +507,7 @@ module.exports = React.createClass({
             var array = []
             array.push($(y).attr('id'))
             colorrow.push($(y).attr('id'))
+            window.history.pushState('Page', 'SCOT', '/#/alertgroup/'+$(y).attr('id'))  
             this.launchEvent(array)
         }.bind(this))
     },
