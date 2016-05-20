0d5b4e9cc6f60b0e657b11d833db56c17e115892
Todd Bruner <tbruner@sandia.gov>
2016-05-20 10:22:18 -0600

    a added space blew up the sourcefire regex

--- lib/Scot/Parser/Sourcefire.pm
+++ lib/Scot/Parser/Sourcefire.pm
@@ -32,17 +32,11 @@ sub parse_message {
 
     $log->trace("Parsing Sourcefire email");
 
-    my $regex   = qr{ \[(?<sid>.*?)\] "(?<rule>.*?)" \[Impact: (?<impact>.*?)\] +From "(?<from>.*?)" at (?<when>.*?) +\[Classification: (?<class>.*?)\] \[Priority: (?<pri>.*?)\] {(?<proto>.*)} (?<rest>.*) *};
+    my $regex   = qr{\[(?<sid>.*?)\] "(?<rule>.*?)" \[Impact: (?<impact>.*?)\] +From "(?<from>.*?)" at (?<when>.*?) +\[Classification: (?<class>.*?)\] \[Priority: (?<pri>.*?)\] {(?<proto>.*)} (?<rest>.*) *};
 
     my $body    = $href->{body_html} // $href->{body_plain};
        $body    =~ s/[\n\r]/ /g;
        $body    =~ m/$regex/g;
-
-    my $rest    = $+{rest};
-    my ($fullsrc, $fulldst) = split(/->/, $rest);
-    my ($srcip, $srcport)   = split(/:/, $fullsrc);
-    my ($dstip, $dstport)   = Split(/:/, $fulldst);
-
 
     $json{data}     = {
         sid         => $+{sid},
@@ -53,11 +47,25 @@ sub parse_message {
         class       => $+{class},
         priority    => $+{pri},
         proto       => $+{proto},
-        srcip       => $srcip,
-        srcport     => $srcport,
-        dstip       => $dstip,
-        dstport     => $dstport,
     };
+
+    my $rest    = $+{rest};
+
+    unless ($rest) {
+        my $badsrc = $href->{body_html} // $href->{body_plain};
+        $log->error("PARSE ERROR on: ".$badsrc);
+        return wantarray ? %json : \%json;
+    }
+
+    my ($fullsrc, $fulldst) = split(/->/, $rest);
+    my ($srcip, $srcport)   = split(/:/, $fullsrc);
+    my ($dstip, $dstport)   = split(/:/, $fulldst);
+
+
+    $json{data}{srcip}       = $srcip;
+    $json{data}{srcport}     = $srcport;
+    $json{data}{dstip}       = $dstip;
+    $json{data}{dstport}     = $dstport;
     $json{columns}  = keys %{$json{data}};
     return wantarray ? %json : \%json;
 }
