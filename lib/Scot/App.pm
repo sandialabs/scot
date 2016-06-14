2ad09362f3f486c674360bcac6233fbbf06b41a0
Todd Bruner <tbruner@sandia.gov>
2016-06-14 08:55:12 -0600

    more xfer work to configfiles

--- lib/Scot/App.pm
+++ lib/Scot/App.pm
@@ -4,10 +4,8 @@ use lib '../../lib';
 use v5.18;
 use strict;
 use warnings;
-use Log::Log4perl;
-use Log::Log4perl::Level;
-use Log::Log4perl::Layout::PatternLayout;
-use Log::Log4perl::Appender;
+use Scot::Util::Logger;
+use File::Find;
 use Safe;
 
 use Moose;
@@ -36,25 +34,35 @@ sub _build_config {
     unless ( -e $file ) {
         die "Error: Unable to find configuration file $file";
     }
-    my $c   = new Safe 'CONFIG';
-    my $r   = $c->rdo($file);
-    return \%CONFIG::config;
+    return $self->get_config($file);
 }
 
-has logname => (
-    is          => 'ro',
-    isa         => 'Str',
-    lazy        => 1,
-    required    => 1,
-    builder     => '_build_logname',
-);
+sub get_config {
+    my $self    = shift;
+    my $file    = shift;
+    my $paths   = shift;
 
-sub _build_logname {
-    my $self    =shift;
-    if ( $self->config->{logname} ) {
-        return $self->config->{logname};
+    unless ($paths) {
+        push @$paths, '/opt/scot/etc';
     }
-    return 'SCOT';
+    my $fqname;
+    find(sub {
+        if ( $_ eq $file ) {
+            $fqname = $File::Find::name;
+            return;
+        }
+    }, @$paths);
+
+    unless ( -e $fqname ) {
+        die "Config file $file not found!\n";
+    }
+
+    no strict 'refs';
+    my $c   = new Safe 'CONFIG';
+    my $r   = $c->rdo($fqname);
+    my $n   = 'CONFIG::environment';
+    my $h   = \%$n;
+    return $h;
 }
 
 has log => (
@@ -62,44 +70,14 @@ has log => (
     isa         => 'Log::Log4perl::Logger',
     required    => 1,
     lazy        => 1,
-    builder     => '_build_logger',
+    builder     => '_get_logger',
 );
 
-sub _build_logger {
-    my $self        = shift;
-    my $file        = $self->logfile;
-    my $name        = $self->logname;
-
-    my $log     = Log::Log4perl->get_logger($name);
-    my $layout  = Log::Log4perl::Layout::PatternLayout->new(
-        '%d %7p [%P] %15F{1}: %4L %m%n'
-    );
-    my $appender    = Log::Log4perl::Appender->new(
-        "Log::Log4perl::Appender::File",
-        name        => $name,
-        filename    => $file,
-        autoflush   => 1,
-    );
-    $appender->layout($layout);
-    $log->add_appender($appender);
-    $log->level($TRACE);
-    return $log;
-}
-
-has logfile => (
-    is          => 'ro',
-    isa         => 'Str',
-    required    => 1,
-    lazy        => 1,
-    builder     => '_get_logfile',
-);
-
-sub _get_logfile {
+sub _get_logger {
     my $self    = shift;
-    if ( $self->config->{logfile} ) {
-        return $self->config->{logfile};
-    }
-    return '/var/log/scot/scot.log';
+    my $cfile   = $self->config->{log_config};
+    my $config  = $self->get_config($cfile);
+    return Scot::Util::Logger->new($config);
 }
 
 has base_url    => (
