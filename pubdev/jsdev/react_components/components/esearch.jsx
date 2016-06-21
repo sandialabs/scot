7ecd74e4ad9a6f21241bd4b382f9346750a75359
85080 <rjeffer@sandia.gov>
2016-06-21 12:39:45 -0600

    Bug fix

--- pubdev/jsdev/react_components/components/esearch.jsx
+++ pubdev/jsdev/react_components/components/esearch.jsx
@@ -3,10 +3,9 @@ var SearchkitProvider       = require('../../../node_modules/searchkit').Searchk
 var SearchkitManager       = require('../../../node_modules/searchkit').SearchkitManager;
 var SearchBox       = require('../../../node_modules/searchkit').SearchBox;
 const searchkit = new SearchkitManager("/scot/api/v2")
-dd
 var Search = React.createClass({
 	render: function(){
-	return (
+	return (/*
                     React.createElement('div', null, 
                     React.createElement(SearchkitProvider, {ref: 'search', searchkit: searchkit},
                     React.createElement(SearchBox, {autofocus: true, searchOnChange: true, prefixQueryFields: ["My Lord"]}
@@ -14,12 +13,12 @@ var Search = React.createClass({
                     )
                     ) 
     
-    /*
+    
 
-
+*/
 
 	    React.createElement('input', {type:'text', className: 'search', id: 'searchid', placeholder: 'Search', style: {position: 'relative', 'margin-top': '7.5px', 'border-radius': '50px', width: '30%', float: 'right', padding: '10px 20px',color: 'black', 'background-color': 'white'}})
-	*/
+	
     
     )
 	}
