b460fa7d6c35f6612cf5e2ef1ccedd3da2cb8a14
85080 <rjeffer@sandia.gov>
2016-06-29 16:05:01 -0600

    new dev

--- pubdev/jsdev/react_components/components/esearch.jsx
+++ pubdev/jsdev/react_components/components/esearch.jsx
@@ -1,26 +1,52 @@
-var React = require('react')
-var SearchkitProvider       = require('../../../node_modules/searchkit').SearchkitProvider;
-var SearchkitManager       = require('../../../node_modules/searchkit').SearchkitManager;
-var SearchBox       = require('../../../node_modules/searchkit').SearchBox;
-const searchkit = new SearchkitManager("/scot/api/v2")
-var Search = React.createClass({
-	render: function(){
-	return (/*
-                    React.createElement('div', null, 
-                    React.createElement(SearchkitProvider, {ref: 'search', searchkit: searchkit},
-                    React.createElement(SearchBox, {autofocus: true, searchOnChange: true, prefixQueryFields: ["My Lord"]}
-                    )
-                    )
-                    ) 
-    
-    
+var React               = require('react')
+var SearchkitProvider   = require('../../../node_modules/searchkit').SearchkitProvider;
+var SearchkitManager    = require('../../../node_modules/searchkit').SearchkitManager;
+var SearchBox           = require('../../../node_modules/searchkit').SearchBox;
+var Hits                = require('../../../node_modules/searchkit').Hits;
+var FilteredQuery       = require('../../../node_modules/searchkit').FilteredQuery;
+var TermQuery           = require('../../../node_modules/searchkit').TermQuery;
+var BoolShould          = require('../../../node_modules/searchkit').BoolShould;
+var LayoutBody          = require('../../../node_modules/searchkit').LayoutBody;
+var LayoutResults       = require('../../../node_modules/searchkit').LayoutResults;
+const searchkit         = new SearchkitManager("/scot/api/v2/event")
 
+
+/*
+searchkit.addDefaultQuery((query)=>{
+    return query.addFilter('color',BoolShould([
+            TermQuery("color", "My Lord"),
+            TermQuery("color", "orange")
+        ]))
+})
+    
 */
 
-	    React.createElement('input', {type:'text', className: 'search', id: 'searchid', placeholder: 'Search', style: {position: 'relative', 'margin-top': '7.5px', 'border-radius': '50px', width: '30%', float: 'right', padding: '10px 20px',color: 'black', 'background-color': 'white'}})
-	
-    
+class Results extends React.Component{
+    render() {
+        console.log(this.props)
+        return (
+            React.createElement('div', null,
+                React.createElement('span', {style: {top: '0px', 'padding-top': '35px !important', width: '600%', left: '-500%', 'z-index': '-999', position: 'absolute', 'background-color': 'white', 'padding-left': '10px', overflow: 'auto', color: 'black', resize: 'vertical', display: 'inline', height: '508px'}}))
     )
+    }
+}
+
+var Search = React.createClass({
+	render: function(){
+        console.log(searchboxtext)
+        return (
+                React.createElement(SearchkitProvider, {searchkit: searchkit},
+                    React.createElement('div', {className: 'search'},
+                    React.createElement('div', {className: 'search_query'},
+                        React.createElement(SearchBox, {autofocus: true, searchOnChange: true})
+                            ),
+                            searchboxtext != '' ?
+                            React.createElement('div', {className: 'search_results'},
+                            React.createElement(Hits, {itemComponent: Results, mod: 'sk-hits-grid'})
+                            ) : null
+                       )
+                )
+        )
 	}
 })
 
