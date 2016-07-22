f7f9ec94890452dbc52e119a44dc8a89bacc9049
A. Nick Georgieff <angeor@as3006snllx.sandia.gov>
2016-07-22 14:51:04 -0600

    Added viewed by history and updated changed history
    Former-commit-id: d03c64fa00bed1d385894f507ce5f03053389b3f

--- pubdev/jsdev/react_components/modal/viewed_by_history.jsx
+++ pubdev/jsdev/react_components/modal/viewed_by_history.jsx
@@ -14,7 +14,7 @@ const customStyles = {
     }
 }
 
-var History = React.createClass({
+var ViewedByHistory = React.createClass({
     getInitialState: function() {
         return {
             historyBody:false,
@@ -22,8 +22,7 @@ var History = React.createClass({
         }
     },
     componentDidMount: function() {
-        this.serverRequest = $.get('/scot/api/v2/'+ this.props.type + '/' + this.props.id + '/history', function (result) {
-            var result = result.records;
+        this.serverRequest = $.get('/scot/api/v2/'+ this.props.type + '/' + this.props.id, function (result) {
             this.setState({historyBody:true, data:result})
         }.bind(this));
     }, 
@@ -32,17 +31,17 @@ var History = React.createClass({
             <div>
                 <Modal
                     isOpen={true}
-                    onRequestClose={this.props.historyToggle}
+                    onRequestClose={this.props.viewedByHistoryToggle}
                     style={customStyles}>
                     <div className="modal-header">
-                        <img src="/images/close_toolbar.png" className="close_toolbar" onClick={this.props.historyToggle} />
-                        <h3 id="myModalLabel">Current History</h3>
+                        <img src="/images/close_toolbar.png" className="close_toolbar" onClick={this.props.viewedByHistoryToggle} />
+                        <h3 id="myModalLabel">{this.props.subjectType} Viewed By</h3>
                     </div>
                     <div className="modal-body" style={{height: '75vh', width:'700px',overflowY:'auto'}}>
-                       {this.state.historyBody ? <HistoryData data={this.state.data} /> : null }
+                       {this.state.historyBody ? <ViewedByHistoryData data={this.state.data} /> : null }
                     </div>
                     <div className="modal-footer">
-                        <Button onClick={this.props.historyToggle}>Done</Button>
+                        <Button onClick={this.props.viewedByHistoryToggle}>Done</Button>
                     </div>
                 </Modal>
             </div>
@@ -50,12 +49,12 @@ var History = React.createClass({
     }
 });
 
-var HistoryData = React.createClass({
+var ViewedByHistoryData = React.createClass({
     render: function() {
         var rows = [];
         data = this.props.data;
-        for (var prop in data) {
-            rows.push(<HistoryDataIterator data={data[prop]} />); 
+        for (var prop in data.view_history) {
+            rows.push(<ViewedByHistoryDataIterator data={data.view_history[prop]} prop={prop}/>); 
         }
         return (
             <div>
@@ -66,14 +65,15 @@ var HistoryData = React.createClass({
 })
 
 
-var HistoryDataIterator = React.createClass({
+var ViewedByHistoryDataIterator = React.createClass({
     render: function() {
     data = this.props.data;
+    prop = this.props.prop;
     return (
-        <div>ID: {data.id} - <ReactTime value={data.when * 1000} format="MM/DD/YYYY hh:mm:ss a" /> - {data.who} - {data.what}</div>
+        <div><b>{prop}</b> at <ReactTime value={data.when * 1000} format="MM/DD/YYYY hh:mm:ss a" /> from IP: {data.where}</div>
     )}
 });
 
 
 
-module.exports = History;
+module.exports = ViewedByHistory;
