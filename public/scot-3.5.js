368a032c446ab8229f824c1ffc5c89c0dce94173
A. Nick Georgieff <angeor@as3006snllx.sandia.gov>
2016-08-11 14:35:57 -0600

    Fixes #154

--- public/scot-3.5.js
+++ public/scot-3.5.js
@@ -864,7 +864,7 @@ var AddFlair = {
                                         if (entityResult[currentEntityValue.toLowerCase()] != undefined ) {
                                             var entityType = entityResult[currentEntityValue.toLowerCase()].type;
                                             var entityid = entityResult[currentEntityValue.toLowerCase()].id;
-                                            var entityCount = entityResult[currentEntityValue.toLowerCase()].count;
+                                            var entityCount = abbreviateNumber(parseInt(entityResult[currentEntityValue.toLowerCase()].count),0);
                                             var entitydata = entityResult[currentEntityValue.toLowerCase()].data;
                                             var entityEntryCount = entityResult[currentEntityValue.toLowerCase()].entry;
                                             var circle = $('<span class="noselect">');
@@ -938,7 +938,7 @@ var AddFlair = {
                             if (entityResult[currentEntityValue.toLowerCase()] != undefined ) {
                                 var entityType = entityResult[currentEntityValue.toLowerCase()].type;
                                 var entityid = entityResult[currentEntityValue.toLowerCase()].id;
-                                var entityCount = entityResult[currentEntityValue.toLowerCase()].count;
+                                var entityCount = abbreviateNumber(entityResult[currentEntityValue.toLowerCase()].count);
                                 var entitydata = entityResult[currentEntityValue.toLowerCase()].data;
                                 var entityEntryCount = entityResult[currentEntityValue.toLowerCase()].entry;
                                 var circle = $('<span class="noselect">');
@@ -1091,7 +1091,17 @@ function infopop(entityid, entityvalue, flairToolbarToggle) {
 function linkWarningPopup(url,linkWarningToggle) {
     linkWarningToggle(url);
 }
-
+function abbreviateNumber(num, fixed) {
+    if (num === null) { return null; } // terminate early
+    if (num === 0) { return '0'; } // terminate early
+    fixed = (!fixed || fixed < 0) ? 0 : fixed; // number of decimal places to show
+    var b = (num).toPrecision(2).split("e"), // get power
+        k = b.length === 1 ? 0 : Math.floor(Math.min(b[1].slice(1), 14) / 3), // floor at decimals, ceiling at trillions
+        c = k < 1 ? num.toFixed(0 + fixed) : (num / Math.pow(10, k * 3) ).toFixed(1 + fixed), // divide by power
+        d = c < 0 ? c : Math.abs(c), // enforce -0 is 0
+        e = d + ['', 'K', 'M', 'B', 'T'][k]; // append power
+    return e;
+}
 module.exports = {AddFlair, Watcher}
 
 },{}],7:[function(require,module,exports){
