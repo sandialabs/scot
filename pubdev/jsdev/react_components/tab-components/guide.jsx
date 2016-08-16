99ba6ad4308c8f6e025adfb68c290abd076be1ec
A. Nick Georgieff <angeor@as3006snllx.sandia.gov>
2016-08-16 10:20:17 -0600

    Fixes #58. Keyboard shortcuts working in Chrome too
    Former-commit-id: 24fad93282afa6211dfdc0bd7266cbbc16afa09a

--- pubdev/jsdev/react_components/tab-components/guide.jsx
+++ pubdev/jsdev/react_components/tab-components/guide.jsx
@@ -69,7 +69,8 @@ module.exports = React.createClass({
         $(t2).resize(function(){
             this.reloadItem()
         }.bind(this))
-        $('.container-fluid2').keydown(function(e){
+        $(document.body).keydown(function(e){
+            if ($('input').is(':focus')) {return};
             var obj = $(toggle[0]).find('#'+this.state.idsarray[0]).prevAll('.allevents')
             var obj2 = $(toggle[0]).find('#'+this.state.idsarray[0]).nextAll('.allevents')
             if((e.keyCode == 74 && obj2.length != 0) || (e.keyCode == 40 && obj2.length != 0)){
