0273201a722e309d460bbbf3a6610d5b282d0fe3
A. Nick Georgieff <angeor@as3006snllx.sandia.gov>
2016-09-02 17:54:43 -0600

    Getting up to dote

--- pubdev/jsdev/react_components/entry/selected_container.jsx
+++ pubdev/jsdev/react_components/entry/selected_container.jsx
@@ -4,9 +4,7 @@ var SelectedHeader = require('./selected_header.jsx');
 var SelectedContainer = React.createClass({
     render: function() {
         var datarows = [];
-        for (i=0; i < this.props.ids.length; i++) { 
-            datarows.push(<SelectedHeader key={this.props.ids[i]} id={this.props.ids[i]} type={this.props.type} toggleEventDisplay={this.props.viewEvent} taskid={this.props.taskid} alertPreSelectedId={this.props.alertPreSelectedId}/>); 
-        }
+        datarows.push(<SelectedHeader key={this.props.id} id={this.props.id} type={this.props.type} toggleEventDisplay={this.props.viewEvent} taskid={this.props.taskid} alertPreSelectedId={this.props.alertPreSelectedId}/>); 
         //var width = this.state.width;
         var width = '100%';
         if ($('#list-view')[0] != undefined ) {
