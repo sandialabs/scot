eeb9615a7e4d30c725496ea239e18d5e50385b7c
Todd Bruner <tbruner@sandia.gov>
2016-09-13 15:53:16 -0600

    Flair improvement (actually saves entries now)

--- bin/user_admin.pl
+++ bin/user_admin.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 use lib '../lib';
 use Scot::Env;
-use Scot::Util::Scot;
+use Scot::Util::Scot2;
 use Data::Dumper;
 use Getopt::Long qw(GetOptions);
 use File::Slurp;
@@ -12,16 +12,13 @@ use v5.18;
 
 my $logfile     = "/var/log/scot/user_admin.log";
 my $server      = "127.0.0.1";  # domain names will work too
-my $path        = "";
+my $path        = "/scot/api/v2/user";
 my $jsonfile    = "";
 my $verb        = "";
 
 GetOptions(
     "log=s"     => \$logfile,
     "server=s"  => \$server,
-    "path=s"   => \$path,
-    "json=s"    => \$jsonfile,
-    "action=s"  => \$verb,
 ) or die <<EOF
 
 Invalid Option!
@@ -29,100 +26,53 @@ Invalid Option!
     usage: $0
         [-log=/tmp/ua.log]          Set the file for logging (def: /var/log/scot/ua.log)
         [-server=scotsrvr]          Set the SCOT server you wish to communicate with (def: 127.0.0.1)
-        [-action=http_verb]         Set the HTTP verb {get|put|post|delete} you want to perform
-        [-path=/scot/api/v2/event]  Set the REST endpoint. Not setting will prompt you interactively.
-        [-json=/tmp/data.json]      Set the JSON you wish to send with request. 
                                         not setting this will allow you to enter interactively.
 EOF
 ;
 
+say "Log into SCOT";
+prompt "Username: ";
+my $user    = $_;
+
+prompt( -e => '*', -p => "Password: ");
+my $pass    = $_;
 
 
-my $env = Scot::Env->new({
-    logfile => $logfile,
-});
-
-my $client  = Scot::Util::Scot->new({
+my $client  = Scot::Util::Scot2->new({
     servername  => $server,
+    username    => $user,
+    password    => $pass,
 });
 
-ACTION:
-while ( $action eq "" ) {
-    $action    = prompt "Enter Action (list|add|delete|change|quit)> ";
+my $entry = "continue";
 
-    unless ( grep {/$action/} qw(list add delete change quit) ) {
-        say "Invalid action!"
-        next ACTION;
+while ( $entry eq "continue" ) {
+    prompt( -menu => {
+        "Create User"       => "create",
+        "Reset Password"    => "password",
+        "Delete User"       => "delete",
+        "quit"              => "quit",
+    });
+
+    my $action = $_;
+
+    say $action;
+
+    if ( $action eq "quit") {
+        $entry = "no";
     }
-    if ( $action eq "list" ) {
-        list();
+
+    if ( $action eq "create" ) {
+        create_user();
     }
-    elsif ( $action eq "add" ) {
-        add();
+    elsif ( $action eq "password" ) {
+        reset_user();
     }
-    elsif ( $action eq "delete" ) {
-        delete();
-    }
-    elsif ( $action eq "change" ) {
-        change();
-    }
-    else ( $action eq "quit" ) {
-        exit 0;
-    }
-}
-
-sub list {
-    say "coming soon...";
-}
-
-sub add {
-
-}
-
-
-prompt -menu=>{
-    "Enter JSON in VIM" => 1,
-    "Load JSON from File" => 2,
-    "No JSON" => 3,
-};
-
-print "You selected $_\n";
-my $json;
-
-if ( $_ == 1 ) {
-    $json = Proc::InvokeEditor->edit("");
-    unless ( is_valid_json $json ) {
-        die "JSON input is invalid! ". Dumper($json)."\n";
+    elsif ( $action eq "delete") {
+        delete_user();
     }
 }
 
-if ( $_ == 2 ) {
-    unless ( $jsonfile ) {
-        $jsonfile = prompt "Enter filename for JSON > ";
-    }
-    $json = read_file($jsonfile);
-    unless ( is_valid_json $json ) {
-        die "JSON input is invalid! ". Dumper($json)."\n";
-    }
-}
 
-if ( $_ == 3 ) {
-    undef $json;
-}
-
-my $tx;
-if ( $json ) {
-    $tx = $client->$verb($path, $json);
-}
-else {
-    $tx = $client->$verb($path);
-}
-
-if ( my $res = $tx->success ) {
-    print Dumper($tx->res->json)."\n";
-}
-else {
-    print "Error! ".Dumper($tx);
-}
 
 exit 0;
