f5d3834545da004c0c22ee8367d50642256a873e
A. Nick Georgieff <angeor@as3006snllx.sandia.gov>
2016-09-15 08:08:54 -0600

    Fixes #249 Fixes #255 Filter/Sort cookie now shows input fields. Autocomplete for Source/Tag Filtering Added. Notification On/Off now persists via cookie

--- pubdev/jsdev/react_components/tab-components/list-view-header.jsx
+++ pubdev/jsdev/react_components/tab-components/list-view-header.jsx
@@ -17,12 +17,13 @@ var ListViewHeader = React.createClass({
         var columnsDisplay = this.props.columnsDisplay;
         var data = this.props.data;
         var sort = this.props.sort;
+        var filter = this.props.filter;
         var handleSort = this.props.handleSort;
         var handleFilter = this.props.handleFilter;
         var arr = [];
         var className = 'wrapper table-row header';
         for (i=0; i < columns.length; i++) {
-            arr.push(<ListViewHeaderEach columnsOne={columns[i]} columnsDisplayOne={columnsDisplay[i]} sort={sort} handleSort={handleSort} handleFilter={handleFilter} />)
+            arr.push(<ListViewHeaderEach columnsOne={columns[i]} columnsDisplayOne={columnsDisplay[i]} sort={sort} filter={filter} handleSort={handleSort} handleFilter={handleFilter} />)
         }
         return (
             <tbody className='list-view-table-header'>
@@ -44,7 +45,11 @@ var ListViewHeaderEach = React.createClass({
         this.props.handleSort(this.props.columnsOne);  
     },
     handleFilter: function(v) {
-        this.props.handleFilter(this.props.columnsOne,v.target.value);
+        if (v.target != undefined) {
+            this.props.handleFilter(this.props.columnsOne,v.target.value);
+        } else {
+            this.props.handleFilter(this.props.columnsOne,v)
+        }
     },
     handleEnterKey: function(e) {
         if (e.key == 'Enter') {
@@ -95,26 +100,40 @@ var ListViewHeaderEach = React.createClass({
         var columnsDisplayOne = this.props.columnsDisplayOne;
         var dataOne = this.props.dataOne;
         var sortDirection;
+        var filterValue;
         var sort = this.props.sort;
+        var filter = this.props.filter;
         var handleSort = this.props.handleSort;
         var handleFilter = this.props.handleFilter;
         var className = columnsOne + '-list-header-column'
         var showSort = false;
         var epochInputValue = '';
-        $.each(sort, function(key, value) {
-            if (key == columnsOne) {
-                showSort = true;
-                if (value == -1) {
-                    sortDirection = 'down'
-                } else {
-                    sortDirection = 'up'
+        if (sort != undefined) {
+            $.each(sort, function(key, value) {
+                if (key == columnsOne) {
+                    showSort = true;
+                    if (value == -1) {
+                        sortDirection = 'down'
+                    } else {
+                        sortDirection = 'up'
+                    }
                 }
-            }
-        })
+            })
+        }
+        if (filter != undefined) {
+            $.each(filter, function(key, value) {
+                if (key == columnsOne) {
+                    filterValue = value.toString();
+                }
+            })
+        }
         if (columnsOne =='created' || columnsOne == 'updated' || columnsOne == 'occurred') {
             
             if (this.state.startepoch != '' || this.state.endepoch != '') {
                 epochInputValue = this.state.startepoch + ',' + this.state.endepoch;
+            }
+            if (filterValue != undefined) {
+                epochInputValue = filterValue;
             }
             return (
                 <th className={className}>
@@ -126,11 +145,11 @@ var ListViewHeaderEach = React.createClass({
                         null}
                     </div>
                     <OverlayTrigger trigger='click' placement='bottom' ref={popOverRef} overlay={<Popover id='dateRangePicker'><DateRangePicker numberOfCalendars={2} selectionType={'range'} showLegend={true} onSelect={this.handleFilterDate} singleDateRange={true} /><Button eventKey={'1'} onClick={this.handleFilterDateSubmit} bsSize={'xsmall'} value={columnsOne}>Filter</Button><Button eventKey={'2'} onClick={this.clearFilter} bsSize={'xsmall'} value={columnsOne}>Clear</Button></Popover>}>
-                        <input style={{width:'inherit'}} onKeyPress={this.handleEnterKey} value={epochInputValue}/>
+                        <input style={{width:'inherit'}} onKeyPress={this.handleEnterKey} value={epochInputValue} />
                     </OverlayTrigger>
                 </th>
             )
-        } /*else if(columnsOne == 'tag' || columnsOne == 'source'){
+        } else if(columnsOne == 'tag' || columnsOne == 'source'){
             return( 
                 <th className={className}>
                     <div onClick={this.handleSort}>
@@ -140,10 +159,10 @@ var ListViewHeaderEach = React.createClass({
                         :
                         null}
                     </div> 
-                    <TagSourceFilter />
+                    <TagSourceFilter columnsOne={columnsOne} handleFilter={this.handleFilter} defaultValue={filterValue}/>
                 </th>
             )
-        }*/else {
+        }else {
             return (
                 <th className={className}>
                     <div onClick={this.handleSort}>
@@ -153,7 +172,7 @@ var ListViewHeaderEach = React.createClass({
                         : 
                         null}
                     </div>
-                    <input style={{width:'inherit'}} onKeyPress={this.handleEnterKey}/>
+                    <input style={{width:'inherit'}} onKeyPress={this.handleEnterKey} defaultValue={filterValue}/>
                 </th>
             )
         }
