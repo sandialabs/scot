e5cc0bf81d27bf99dc8b7e3672b453d52607e811
A. Nick Georgieff <angeor@as3006snllx.sandia.gov>
2016-09-29 14:03:09 -0600

    Fixes relative links in editor.
    Former-commit-id: 70c834ee2c8ea99e9b9fdd805d5d19bae6474778

--- pubdev/jsdev/react_components/modal/link_warning.jsx
+++ pubdev/jsdev/react_components/modal/link_warning.jsx
@@ -18,34 +18,34 @@ var LinkWarning = React.createClass({
         window.open(this.props.link);
         this.props.linkWarningToggle();
     },
-    render: function() {
-        if ($.isUrlExternal(this.props.link)) {
-            return (
-                <div>
-                    <Modal
-                        isOpen={true}
-                        onRequestClose={this.props.linkWarningToggle}
-                        style={customStyles}>
-                        <div className="modal-header">
-                            <img src="/images/close_toolbar.png" className="close_toolbar" onClick={this.props.linkWarningToggle} />
-                            <h3 id="myModalLabel">Browse to site?</h3>
-                        </div>
-                        <div className="modal-body"> 
-                            The link you clicked may take you to a site outside SCOT. If this is a link an attacker controls you may be tipping your hand.
-                            <br/>
-                            <b>{this.props.link}</b>
-                        </div>
-                        <div className="modal-footer">
-                            <Button id='cancel-delete' onClick={this.props.linkWarningToggle}>Cancel</Button>
-                            <Button bsStyle='info' id='proceed' onClick={this.proceed}>Proceed</Button>
-                        </div>
-                    </Modal>
-                </div>
-            )
-        } else {
+    componentWillMount: function() {
+        if ($.isUrlInternal(this.props.link)) {
             this.proceed();
-            return(<div></div>)
         }
+    },
+    render: function() {
+        return (
+            <div>
+                <Modal
+                    isOpen={true}
+                    onRequestClose={this.props.linkWarningToggle}
+                    style={customStyles}>
+                    <div className="modal-header">
+                        <img src="/images/close_toolbar.png" className="close_toolbar" onClick={this.props.linkWarningToggle} />
+                        <h3 id="myModalLabel">Browse to site?</h3>
+                    </div>
+                    <div className="modal-body"> 
+                        The link you clicked may take you to a site outside SCOT. If this is a link an attacker controls you may be tipping your hand.
+                        <br/>
+                        <b>{this.props.link}</b>
+                    </div>
+                    <div className="modal-footer">
+                        <Button id='cancel-delete' onClick={this.props.linkWarningToggle}>Cancel</Button>
+                        <Button bsStyle='info' id='proceed' onClick={this.proceed}>Proceed</Button>
+                    </div>
+                </Modal>
+            </div>
+        )
     }
 });
 module.exports = LinkWarning;
