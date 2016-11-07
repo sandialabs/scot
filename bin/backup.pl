8aadb33ed593acd405016c9cd616ff6c940aeb56
Todd Bruner <tbruner@sandia.gov>
2016-11-07 09:28:48 -0700

    backup improvements

--- bin/backup.pl
+++ bin/backup.pl
@@ -4,10 +4,12 @@ use strict;
 use warnings;
 use v5.18;
 use lib '../lib';
+use lib '/opt/scot/lib';
 
 use Scot::Util::Config;
 use Scot::Env;
 use DateTime;
+use Mojo::JSON qw(decode_json);
 
 my $cfgobj  = Scot::Util::Config->new({
     file    => "backup.cfg",
@@ -85,40 +87,65 @@ if ( $config->{tarloc} ) {
 
 # now backup elasticsearch
 
-# is there a repo?
+print "Backing up ElasticSearch...\n";
+
+my $curl    = "curl -s";
+
+my $repo_cmd        = $config->{es_server} . "/_snapshot/scot_backup";
+my $repo_status     = `curl -XGET $repo_cmd`;
+my $repo_loc        = "location\": ".$config->{es_backup_location};
+
+if ( $repo_status !~ /$repo_loc/ ) {
+    print "\nElasticSearch Repo back up is not storing snapshots in ".
+          "expected location\nFixing...\n";
+    my $stat    = `$curl -XDELETE $repo_cmd`;
+}
+
+$repo_status = `curl -XGET $repo_cmd`;
+
+if ( $repo_status =~ /repository_missing_exception/ ) {
+    print "Missing Repo, creating scot_backup repo...\n";
+
+    my $create_repo_template = <<'EOF';
+%s/_snapshot/scot_backup -d '{
+    "type": "fs",
+    "settings": {
+        "compress": "true",
+        "location": "%s"
+    }
+}'
+EOF
+
+    my $create_repo_string = sprintf($create_repo_template, 
+                                    $config->{es_server},
+                                    $config->{es_backup_location});
+
+    print "Creating Repo with: $create_repo_string\n";
+    my $repocreatestat = `$curl -XPUT $create_repo_string`;
+}
+
 my $escmd   = $config->{es_server}."/_snapshot/scot_backup/snapshot_1";
 
-my $repo_query = `curl -XGET $escmd`;
-if ( $repo_query =~ /repository_missing_exception/ ) {
-    # need to create the repo 
-    my $create = $config->{es_server} .qq|/_snapshot/scot_backup/ -d '{ "scot_backup": { "type": "fs", "settings": { "compress": "true", "location": "|. $config->{es_backup_location} .qq|" } } }'|;
-    my $status = `curl -XPUT $create`;
-    unless ( $status =~ /acknowledged\":true/ ) {
-        die "Failed to create create repo for ES backup: $status\n";
-    }
-}
+print "Deleting existing snapshot...\n";
+my $del_stat = `$curl -XDELETE $escmd`;
 
-my $status = `curl -XPUT $escmd`;
-unless ( $status =~ /acknowledged\":true/ ) {
-    warn "FAILED to create a snapshot of ES data!";
+print "Request new snapshot...\n";
+my $snap_stat   = `$curl -XPUT $escmd`;
+
+unless ( $snap_stat =~ /accepted\":true/ ) {
+    warn "Failed to create a snapshot! $snap_stat";
 }
 else {
-    # loop and wait for snapshot to complete;
-    my $status = `curl -XGET $escmd`;
-    my $count   = 0;
-    while ( $status !~ /SUCCESS/ ) {
+    print "Waiting for Snapshot to complete...\n";
+    my $this_stat = `$curl -XGET $escmd`;
+    my $count     = 0;
+    while ( $this_stat !~ /SUCCESS/ and $count < 100 ) {
+        print ".";
         sleep 5;
-        $status = `curl -XGET $escmd`;
+        $this_stat = `$curl -XGET $escmd`;
         $count++;
-
-        if ( $count > 100 ) {
-            warn "Snapshot did not complete in timely matter. Continuing...";
-            $status = "SUCCESS";
-        }
     }
 }
-
-
 
 my $esdir   = $config->{es_backup_location};
 my $dt  = DateTime->now();
