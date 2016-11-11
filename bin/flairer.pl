16c2af64f6c48fe19f3b144214416bfe30633b7f
Todd Bruner <tbruner@sandia.gov>
2016-11-11 13:38:09 -0700

    going highlander on Flair modules (there can only be one)

--- bin/flairer.pl
+++ bin/flairer.pl
@@ -7,10 +7,10 @@ use lib '../../lib';
 use lib '../../Scot-Internal-Modules/lib';
 use lib '/opt/scot/lib';
 use v5.18;
-use Scot::App::Flair2;
+use Scot::App::Flair;
 
 
-my $loop    = Scot::App::Flair2->new( 
+my $loop    = Scot::App::Flair->new( 
     configuration_file  => 'flair.app.cfg',
     interactive  => 1,
 );
