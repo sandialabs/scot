153f9ecdfab715fa382b3d1408d9313d413f938b
A. Nick Georgieff <angeor@as3006snllx.sandia.gov>
2016-11-17 13:28:00 -0700

    Bug fix for alertgroup columns not being correctly parsed

--- public/scot-3.5.js
+++ public/scot-3.5.js
@@ -3328,7 +3328,7 @@ var AlertParent = React.createClass({displayName: "AlertParent",
                 if (this.props.headerData != undefined) {
                     if (this.props.headerData.columns != undefined) {
                         if (this.props.headerData.columns.length != 0) {
-                            col_names = items[0].data.columns.slice(0);
+                            col_names = this.props.headerData.columns.slice(0);
                         }
                     }
                 }
