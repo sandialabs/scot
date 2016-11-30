d1a093fc33bd8c330c80d558c5ace933029f0035
Todd Bruner <tbruner@sandia.gov>
2016-11-30 10:27:34 -0700

    working on Stats

--- lib/Scot.pm
+++ lib/Scot.pm
@@ -180,6 +180,10 @@ relies on the browser BasicAuth popup.
             ->to    ('controller-graph#get_graph')
             ->name  ('get_graph');
 
+    $scot   ->route ('/api/v2/status')
+            ->to    ('controller-api#get_status')
+            ->name  ('get_status');
+
 =pod
 
 @api {get} /scot/api/v2/esearch Search Scot
