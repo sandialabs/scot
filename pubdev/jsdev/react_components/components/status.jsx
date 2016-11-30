eecbb77585078b99b5705acdf588f32072542799
A. Nick Georgieff <angeor@as3006snllx.sandia.gov>
2016-11-30 11:50:55 -0700

    Added Status support

--- pubdev/jsdev/react_components/components/status.jsx
+++ pubdev/jsdev/react_components/components/status.jsx
@@ -2,10 +2,10 @@ var React = require('react');
 var Panel = require('react-bootstrap/lib/Panel.js');
 var Badge = require('react-bootstrap/lib/Badge.js');
 
-var Stats = React.createClass({
+var Status = React.createClass({
     getInitialState: function() {
         return {
-            StatData: null
+            StatusData: null
         }
     },
     componentDidMount: function() {
@@ -13,24 +13,24 @@ var Stats = React.createClass({
             type: 'get',
             url: '/scot/api/v2/status',
         }).success(function(response) {
-            this.setState({StatData:response});
+            this.setState({StatusData:response});
         }.bind(this))
     },
     render: function() {
-        var StatRows = [];
-        if (this.state.StatData != null) {
-            for (var key in this.state.StatData) {
-                StatRows.push(
+        var StatusRows = [];
+        if (this.state.StatusData != null) {
+            for (var key in this.state.StatusData) {
+                StatusRows.push(
                     <Panel header={key} >
                         <div style={{display:'flex', flexFlow:'column'}}>
-                            <div style={{fontWeight:'bold'}}>{this.state.StatData[key]}</div>
+                            <div style={{fontWeight:'bold'}}>{this.state.StatusData[key]}</div>
                         </div>
                     </Panel>
                 )
             }
         } else {
-            StatRows.push(
-                <Panel header={'SCOT 3.5 Stats'}>
+            StatusRows.push(
+                <Panel header={'SCOT 3.5 Status'}>
                     <br/>
                     <div style={{fontWeight:'bold'}}>Coming Soon</div>
                     <br/>
@@ -40,14 +40,14 @@ var Stats = React.createClass({
         return (
             <div id='stats' className="stats">
                 <div style={{textAlign:'center'}}>
-                    <h2>Stats</h2>
+                    <h2>Status</h2>
                 </div>
                 <div style={{display:'flex'}}>
-                    {StatRows}
+                    {StatusRows}
                 </div>
             </div>
         );
     }
 });
 
-module.exports = Stats;
+module.exports = Status;
