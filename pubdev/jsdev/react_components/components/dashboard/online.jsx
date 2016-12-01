7b8b0c33a2c077573a96d8d63f052e0a3e75eaec
A. Nick Georgieff <angeor@as3006snllx.sandia.gov>
2016-12-01 16:15:17 -0700

    Front page goodness and responsiveness

--- pubdev/jsdev/react_components/components/dashboard/online.jsx
+++ pubdev/jsdev/react_components/components/dashboard/online.jsx
@@ -2,35 +2,36 @@ var React = require('react');
 var Panel = require('react-bootstrap/lib/Panel.js');
 var Badge = require('react-bootstrap/lib/Badge.js');
 
-var Status = React.createClass({
+var Online = React.createClass({
     getInitialState: function() {
         return {
-            StatusData: null
+            OnlineData: null
         }
     },
     componentDidMount: function() {
         $.ajax({
             type: 'get',
-            url: '/scot/api/v2/status',
+            url: '/scot/api/v2/who',
         }).success(function(response) {
-            this.setState({StatusData:response});
+            this.setState({OnlineData:response.records});
         }.bind(this))
     },
     render: function() {
-        var StatusRows = [];
-        if (this.state.StatusData != null) {
-            for (var key in this.state.StatusData) {
-                StatusRows.push(
-                    <Panel header={key} >
+        var OnlineRows = [];
+        if (this.state.OnlineData != null) {
+            for (i=0; i < this.state.OnlineData.length; i++) {
+                var timeago = timeSince(this.state.OnlineData[i].last_activity);
+                OnlineRows.push(
+                    <Panel header={this.state.OnlineData[i].username} >
                         <div style={{display:'flex', flexFlow:'column'}}>
-                            <div style={{fontWeight:'bold'}}>{this.state.StatusData[key]}</div>
+                            <div>{timeago} ago</div>
                         </div>
                     </Panel>
                 )
             }
         } else {
-            StatusRows.push(
-                <Panel header={'SCOT 3.5 Status'}>
+            OnlineRows.push(
+                <Panel header={'SCOT 3.5 Online'}>
                     <br/>
                     <div style={{fontWeight:'bold'}}>Coming Soon</div>
                     <br/>
@@ -38,16 +39,16 @@ var Status = React.createClass({
             )
         }
         return (
-            <div id='stats' className="stats">
+            <div id='online' className="dashboard col-md-2">
                 <div style={{textAlign:'center'}}>
-                    <h2>Status</h2>
+                    <h2>Activity</h2>
                 </div>
-                <div style={{display:'flex'}}>
-                    {StatusRows}
+                <div>
+                    {OnlineRows}
                 </div>
             </div>
         );
     }
 });
 
-module.exports = Status;
+module.exports = Online;
