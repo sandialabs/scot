71308b884a0c80872ca6cac6fdbdcfa49f751bf3
Todd Bruner <todd.bruner@gmail.com>
2017-01-10 13:28:14 -0700

    java versions, why wont 16.04 support 7?

--- install/install_java.sh
+++ install/install_java.sh
@@ -8,10 +8,10 @@ function install_java {
 
     if [[ $OS == "Ubuntu" ]]; then
         if [[ $OSVERSION == "16" ]]; then
-            add-apt-repository ppa:openjdk-r/ppa
-            apt-get update
+            apt-get install -y openjdk-8-jdk -y
+        else
+            apt-get install -y openjdk-7-jdk -y
         fi
-        apt-get install -y openjdk-7-jdk -y
     else
         yum install java-1.7.0-openjdk -y
     fi
