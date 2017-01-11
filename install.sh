c1a8a02937657f01d175c0d81794ba8885ce3f42
Todd Bruner <todd.bruner@gmail.com>
2017-01-10 17:17:27 -0700

    if this doesn't kill me...
    Former-commit-id: 543b0d1376cdd4bd5d1d428bedba94eb22cb6acf

--- install.sh
+++ install.sh
@@ -82,7 +82,7 @@ if [[ $INSTMODE != "SCOTONLY" ]]; then
     . ./install/install_mongodb.sh
     install_mongodb
     . ./install/install_apache.sh
-    install_apache.sh
+    install_apache
     . ./install/install_perl.sh
     install_perl
     configure_filestore
@@ -118,8 +118,18 @@ echo "---- Install completed"
 echo "----"
 echo "----"
 
+. ./install/status.sh
 
-
+echo "========= "
+echo "========= Please let us know you are using SCOT"
+echo "========= it helps us convince our management to continue work on it."
+echo "========= "
+echo "========= ways to communicate with us:"
+echo "========= follow us on twitter: @scotincresp "
+echo "========= subscribe to        : majordomo@sandia.gov "
+echo "=========             with body "subscribe scot-users"
+echo "========= email the dev team  : scot-dev@sandia.gov "
+echo "========= "
 
 
 
