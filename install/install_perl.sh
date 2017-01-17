333e3fbd098161df3ad5fef6ad123cb5bbf180b6
Todd Bruner <tbruner@sandia.gov>
2017-01-17 12:54:48 -0700

    attempt to fix failed config load

--- install/install_perl.sh
+++ install/install_perl.sh
@@ -326,6 +326,7 @@ function install_perl_modules {
         Data::Dumper
         Data::Dumper::HTML
         Data::Dumper::Concise
+        Data::Clean@0.48
         Data::Clean::FromJSON
         Daemon::Control
         Net::LDAP
@@ -374,6 +375,7 @@ function install_perl_modules {
         HTML::StripTags
         HTML::TreeBuilder
         HTML::FromText
+        HTML::FormatText
         MIME::Base64
         IPC::Run
         IO::Prompt
