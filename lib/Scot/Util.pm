b00296c2c216192ac7cce0f10105a071e0aec602
Todd Bruner <tbruner@sandia.gov>
2017-01-19 15:56:43 -0700

    start of work to have a consistent config system mark ii
    Former-commit-id: b9951bde63cad7cc3ba497eb42ab86f20c06b3d9

---
+++ lib/Scot/Util.pm
@@ -0,0 +1,51 @@
+package Scot::Util;
+
+=head1 Name
+
+Scot::Util
+
+=head1 Description
+
+Parent object for some of utils in Util.  Extending this class
+gives the child a log attribute and the ability to load a config file
+via Scot::Role::Configurable
+
+=cut
+
+use lib '../../lib';
+use v5.18;
+use strict;
+use warnings;
+
+use Scot::Util::Logger;
+use Moose;
+
+with qw(Scot::Role::Configurable);
+
+has log => (
+    is          => 'ro',
+    isa         => 'Log::Log4perl::Logger',
+    required    => 1,
+    lazy        => 1,
+    builder     => '_get_log',
+);
+
+sub _get_log {
+    my $self        = shift;
+    my $config      = $self->config;    # from scot::role::configurable
+    my $logconfig   = $config->{log};
+
+    unless ( $logconfig ) {
+        $logconfig  = {
+            logger_name     => 'SCOT',
+            layout          => '%d %7p [%P] %15F{1}: %4L %m%n',
+            appender_name   => 'scot_log',
+            logfile         => '/var/log/scot/scot.log',
+            log_level       => 'DEBUG',
+        };
+    }
+    return Scot::Util::Logger->new($logconfig);
+}
+
+1;
+
