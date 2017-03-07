afac8fa56718b4dca7f09630ce9c465a2e45977a
A. Nick Georgieff <angeor@as3006snllx.sandia.gov>
2017-03-07 14:33:26 -0700

    gulpfile to include coffee base for compiling

--- pubdev/gulpfile.js
+++ pubdev/gulpfile.js
@@ -3,16 +3,20 @@ var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
 
-gulp.task('build', function () {
+gulp.task('watch', ['build-coffee', 'build'], function () {
+    gulp.watch('./jsdev/react_components/**/**', ['build-coffee']);
+});
+
+gulp.task('build-coffee'), function() {
+    //build coffee code here
+}
+
+gulp.task('build', ['build-coffee'], function () {
     return browserify({entries: './jsdev/react_components/main/index.jsx', extensions: ['.jsx'], debug: true})
         .transform('babelify', {presets: ['es2015', 'react']})
         .bundle()
         .pipe(source('scot-3.5.js'))
         .pipe(gulp.dest('../public/'),{overwrite:true});
-});
-
-gulp.task('watch', ['build'], function () {
-    gulp.watch('./jsdev/react_components/**/**', ['build']);
 });
 
 gulp.task('default', ['watch']);
