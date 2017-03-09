4b1b0efd6744053c49015b2cbb69b8c77443b4de
A. Nick Georgieff <angeor@as3006snllx.sandia.gov>
2017-03-09 16:15:38 -0700

    No longer prevent refresh of page when updates on a page occur. Reply submit now keeps content on page if submit fails

--- pubdev/jsdev/react_components/activemq/store.jsx
+++ pubdev/jsdev/react_components/activemq/store.jsx
@@ -9,10 +9,11 @@ function activeMQ(payload){
 
 var Storeaq = assign({}, EventEmitter.prototype, {
     emitChange: function(key){
-        if ($('.mce-tinymce')[0]) {
+        //TODO Clean the following comment out once we verify that the scrolling issue due to pre-processing in tinymce is gone at refresh time
+        /*if ($('.mce-tinymce')[0]) {
             console.log('Entry box open - holding off update')
             return;
-        }
+        }*/
         this.emit(key)
     },
     addChangeListener: function(callback){
