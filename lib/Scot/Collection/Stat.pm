6d7473080ee7d3b09642f88c56577feeb4f5ebee
Todd Bruner <tbruner@sandia.gov>
2017-03-22 08:06:24 -0600

    Stat work
    Former-commit-id: 204fb73d604ee12d390771000ed7993a8bd0a23f

--- lib/Scot/Collection/Stat.pm
+++ lib/Scot/Collection/Stat.pm
@@ -62,4 +62,74 @@ sub increment {
     return $obj;
 }
 
+sub get_statistics {
+    my $self    = shift;
+    my $metric  = shift;
+    my %command;
+    my $tie = tie(%command, "Tie::IxHash");
+    %command = (
+        mapreduce   => "stat",
+        map         => $self->_get_map_js,
+        reduce      => $self->_get_reduce_js,
+        finalize    => $self->_get_finalize_js,
+        query       => $metric,
+        out         => { inline => 1 },
+    );
+    my $mongo   = $self->meerkat;
+    my $json    = $self->_try_mongo_op(
+        get_stats   => sub {
+            my $db_name = $mongo->database_name;
+            my $db      = $mongo->mongo_database($db_name);
+            my $job     = $db->run_command(\%command);
+            return $job;
+        }
+    );
+    return $json;
+}
+
+sub _get_map_js {
+    return <<EOF;
+function () {
+    var key = this.dow;
+    var value = {
+        sum: this.value,
+        min: this.value,
+        max: this.value,
+        count: 1,
+        diff: 0
+    };
+    emit(key, value);
+}
+EOF
+}
+
+sub _get_reduce_js {
+    return <<EOF;
+function(dow, value) {
+    var a = value[0];
+    for (var i = 1; i < value.length; i++) {
+        var b = value[i];
+        var delta = a.sum / a.count - b.sum / b.count;
+        var weight = (a.count * b.count) / (a.count + b.count);
+        a.diff += b.diff + delta * delta * weight;
+        a.sum += b.sum;
+        a.count += b.count;
+        a.min = Math.min(a.min, b.min);
+        a.max = Math.max(a.max, b.max);
+    }
+    return a;
+}
+EOF
+}
+
+sub _get_finalize_js {
+    return <<EOF;
+function (key, value) {
+    value.avg = value.sum / value.count;
+    value.variance = value.diff / value.count;
+    value.stddev = Math.sqrt(value.variance);
+    return value;
+EOF
+}
+
 1;
