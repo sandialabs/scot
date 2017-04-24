262ff4171ed81ffed09d8a9a21fc2468ab5e9b0e
Todd Bruner <tbruner@sandia.gov>
2017-04-24 09:27:46 -0600

    syncing work to provide tar to customer

--- lib/Scot/App/Metric.pm
+++ lib/Scot/App/Metric.pm
@@ -18,6 +18,42 @@ use MongoDB::Code;
 use Moose;
 extends 'Scot::App';
 
+sub march {
+    my $self    = shift;
+    my $reports = shift; # match the sub name
+    my $curdt   = shift;
+    my $stopdt  = shift;
+
+    while ( DateTime->compare($curdt, $stopdt) > 0 ) {
+        my ($sdt,$edt) = $self->get_daily_dts($curdt);
+        say "Examining ".$curdt->ymd." ".$sdt->hms." to ".$edt->hms;
+        say "    ".$sdt->epoch." to ".$edt->epoch;
+
+        my $t;
+        foreach my $report (@$reports) {
+            say "    $report calculation";
+            $t = $self->env->get_timer($report);
+            $self->$report($sdt,$edt);
+            say "        ".&$t." elapsed seconds";
+        }
+
+        $curdt->subtract( days => 1 );
+    }
+}
+    
+
+sub get_daily_dts {
+    my $self    = shift;
+    my $dt      = shift;
+    my $start   = $dt->clone();
+    my $end     = $dt->clone();
+
+    $start->set( hour => 0,  minute => 0,  second => 0 );
+    $end->set  ( hour => 23, minute => 59, second => 59);
+
+    return $start, $end;
+}
+
 sub get_hourly_epochs {
     my $self    = shift;
     my $dt      = shift;
@@ -158,9 +194,32 @@ sub alert_response_time {
     my $enddt   = shift;
     my $mongo   = $self->env->mongo;
     my $statcol = $mongo->collection('Stat');
-    my $aggcmd  = $self->build_response_time_aggregation($startdt,$enddt);
-    my $aggcol  = $mong->collection('Alert');
 
+    my $aggcmd  = $self->build_response_time_aggregation($startdt,$enddt);
+    my $aggcol  = $mongo->collection('Alert');
+    my $cursor  = $aggcol->get_aggregate_cursor($aggcmd);
+
+    while ( my $href = $cursor->next ) {
+            if ( $href->{value} == 0 ) {
+                say "0";
+            }
+            else {
+                say Dumper($href);
+            }
+            # need to translate dowx (mongo 1=Sunday to perl dow 1=Monday)
+            my $res = delete $href->{_id};
+            my $dowx = delete $res->{dowx};
+            # first metric sum(response times)
+            $res->{value} = $href->{value} // 0; 
+            $res->{metric} = "Sum of alert response times";
+            $statcol->upsert_metric($res);
+            # second metric count of the alerts summed above
+            $res->{value} = $href->{alerts} // 0;
+            $res->{metric} = "Total of viewed alerts";
+            $statcol->upsert_metric($res);
+            # now a later query to Stat collection can rapidly calcuate
+            # averages based on the rollup selected.
+    }
 }
 
 sub build_response_time_aggregation {
@@ -170,6 +229,7 @@ sub build_response_time_aggregation {
     my @cmd     = (
         {
             '$match'  => {
+                view_count => {'$gt'    => 0},
                 created => {
                     '$lte'  => $enddt->epoch,
                     '$gte'  => $startdt->epoch,
@@ -192,8 +252,282 @@ sub build_response_time_aggregation {
                         { '$multiply' => [ '$created', 1000 ] }
                     ]
                 },
+                response => {
+                    '$cond' => {
+                        if  => {
+                            '$eq'   => [
+                                { '$arrayElemAt'=>['$adoc.firstview',0] },
+                                -1
+                            ]
+                        },
+                        then    => 0,
+                        else    => {
+                            '$subtract' => [
+                                { '$arrayElemAt'=>['$adoc.firstview',0] },
+                                '$created',
+                            ],
+                        },
+                    },
+                },
+                count   => {
+                    '$cond' => {
+                        if  => {
+                            '$eq'   => [
+                                { '$arrayElemAt'=>['$adoc.firstview',0] },
+                                -1
+                            ]
+                        },
+                        then    => 0,
+                        else    => 1,
+                    },
+                },
             }
         },
+        {
+            '$group'    => {
+                _id => {
+                    metric  => "alert response time",
+                    year    => { '$year'    => '$dt' },
+                    month   => { '$month'   => '$dt' },
+                    day     => { '$dayOfMonth' => '$dt' },
+                    hour    => { '$hour'    => '$dt' },
+                    dowx    => { '$dayOfWeek'   => '$dt' },
+                },
+                value   => { '$sum' => '$response' },
+                alerts  => { '$sum' => '$count' },
+            }
+        },
+        {
+            '$sort' => {
+                '_id.month' => 1,
+                '_id.day'   => 1,
+                '_id.hour'  => 1,
+            }
+        }
+    );
+    return wantarray ? @cmd : \@cmd;
+}
+
+# alerttype metric help determine usefulness of an alerttype
+# {
+#     alert_subject: 
+#     year:
+#     month:
+#     count:
+#     open:
+#     close:
+#     promoted:
+#     incident: # number that yielded an incident # might need to do it post
+#     avg response time:
+# }
+
+sub alerttype_metrics {
+    my $self    = shift;
+    my $startdt = shift;
+    my $enddt   = shift;
+    my $mongo   = $self->env->mongo;
+    my $statcol = $mongo->collection('Stat');
+    my $ag_col  = $mongo->collection('Alertgroup');
+    my $a_col   = $mongo->collection('Alert');
+    my $e_col   = $mongo->collection('Event');
+    my $s_col   = $mongo->collection('Atmetric');
+    my $log     = $self->env->log;
+
+    $log->debug("Getting Alertgroups");
+    my $group_cursor    = $ag_col->find({
+        created => {
+            '$lte'  => $enddt->epoch,
+            '$gte'  => $startdt->epoch,
+        }
+    });
+    $group_cursor->immortal(1);
+
+    my %r   = ();
+    while (my $alertgroup = $group_cursor->next ) {
+        my $created = $alertgroup->created;
+        my $subject = $alertgroup->subject;
+        my ($year, $month, $day,
+            $dow, $quarter, $hour ) = $self->get_dt_breakout($created);
+        $log->debug("   Getting alerts in alertgroup ".$alertgroup->id);
+        my $alert_cursor = $a_col->find({alertgroup => $alertgroup->id});
+
+        while ( my $alert = $alert_cursor->next ) {
+            my $status  = $alert->status;
+
+            $r{$subject}{$year}{$month}{$day}{$hour}{count}++;
+            $r{$subject}{$year}{$month}{$day}{$hour}{$status}++;
+
+            if ($alertgroup->firstview != -1) {
+                my $rt = ($alertgroup->firstview - $alert->created);
+                push @{$r{$subject}{$year}{$month}{$day}{$hour}{rt}}, $rt;
+            }
+            if ($alert->promotion_id != 0) {
+                my $event   = $e_col->find_iid($alert->promotion_id);
+                if ( defined $event ) {
+                    if ( $event->status eq "promoted" ) {
+                        $r{$subject}{$year}{$month}{$day}{$hour}{incident} ++;
+                    }
+                }
+            }
+        }
+    }
+    foreach my $s (keys %r) {
+        foreach my $y (sort keys %{$r{$s}} ) {
+            foreach my $m (sort keys %{$r{$s}{$y}} ) {
+                foreach my $d (sort keys %{$r{$s}{$y}{$m}} ) {
+                    foreach my $h (sort keys %{$r{$s}{$y}{$m}{$d}} ) {
+                        my $p   = $r{$s}{$y}{$m}{$d}{$h};
+                        my $stats = $self->get_stats($p->{rt});
+                        my $row = {
+                            alerttype   => $s,
+                            year        => $y,
+                            month       => $m,
+                            day         => $d,
+                            hour        => $h,
+                            count       => $p->{count}//0,
+                            open        => $p->{open}//0,
+                            closed      => $p->{closed}//0,
+                            promoted    => $p->{promoted}// 0,
+                            rt_sum      => $stats->{sum} // 0,
+                            rt_count    => $stats->{count} // 0,
+                            incident    => $p->{incident} // 0,
+                        };
+                        # now stuff into AtMetric
+                        $log->debug("upserting atmetric...");
+                        $s_col->upsert_metric($row);
+                    }
+                }
+            }
+        }
+    }
+}
+
+sub get_stats {
+    my $self    = shift;
+    my $aref    = shift;
+    my $stat    = Statistics::Descriptive::Sparse->new();
+    $stat->add_data(@$aref);
+    return {
+#        avg => $stat->mean,
+        sum => $stat->sum,
+        count => $stat->count,
+#        min => $stat->min,
+#        max => $stat->max,
+#        sd  => $stat->standard_deviation,
+    };
+}
+
+sub get_dt_breakout {
+    my $self    = shift;
+    my $epoch   = shift;
+    my $dt      = DateTime->from_epoch(epoch => $epoch);
+    return $dt->year, $dt->month, $dt->day, $dt->dow, $dt->quarter, $dt->hour;
+}
+
+# this would be nice if I could actually get this shit to work
+sub build_alerttype_agg_cmd {
+    my $self    = shift;
+    my $startdt = shift;
+    my $enddt   = shift;
+    my @cmd     = (
+        {
+            '$match'  => {
+                created => {
+                    '$lte'  => $enddt->epoch,
+                    '$gte'  => $startdt->epoch,
+                }
+            }
+        },
+        {
+            '$lookup'   => {
+                from        => 'alertgroup',
+                localField  => 'alertgroup',
+                foreignField=> 'id',
+                as          => "agdoc",
+            }
+        },
+        {
+           '$project'   => {
+                id  => 1,
+                'agdoc.subject'  => 1,
+                status  => 1,
+                dt  => {
+                    '$add'  => [
+                        DateTime->from_epoch( epoch => 0 ),
+                        { '$multiply' => [ '$created', 1000 ] }
+                    ]
+                },
+                response => {
+                    '$cond' => {
+                        if  => {
+                            '$eq'   => [
+                                { '$arrayElemAt'=>['$agdoc.firstview',0] },
+                                -1
+                            ]
+                        },
+                        then    => undef,
+                        else    => {
+                            '$subtract' => [
+                                { '$arrayElemAt'=>['$agdoc.firstview',0] },
+                                '$created',
+                            ],
+                        },
+                    },
+                },
+            }
+        },
+        {
+            '$group'    => {
+                _id => {
+                    subject => '$agdoc.subject',
+                    year    => { '$year'    => '$dt' },
+                    month   => { '$month'   => '$dt' },
+                    day     => { '$dayOfMonth' => '$dt' },
+                },
+                count   => { '$sum' => 1 },
+                open    => {
+                    '$sum'  => {
+                        '$cond' => {
+                            if  => { '$eq'  => [ '$status', "open" ] },
+                            then => 1,
+                            else => 0,
+                        }
+                    }
+                },
+                closed    => {
+                    '$sum'  => {
+                        '$cond' => {
+                            if  => { '$eq'  => [ '$status', "closed" ] },
+                            then => 1,
+                            else => 0,
+                        }
+                    }
+                },
+                promoted    => {
+                    '$sum'  => {
+                        '$cond' => {
+                            if  => { '$eq'  => [ '$status', "promoted" ] },
+                            then => 1,
+                            else => 0,
+                        }
+                    }
+                },
+                response_time_min   => { '$min'   =>  '$response' },
+                response_time_avg   => { '$avg'   => '$response' },
+                response_time_max   => { '$max'   => '$response' },
+                response_time_sd    => { '$stdDevPop' => '$response' },
+            },
+        },
+        {
+            '$sort' => {
+                '_id.subject'   => 1,
+                '_id.year'      => 1,
+                '_id.month'     => 1,
+            }
+        }
+    );
+    return wantarray ? @cmd : \@cmd;
+}
 
 
 
