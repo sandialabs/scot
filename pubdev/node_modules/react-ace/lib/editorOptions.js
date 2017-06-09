a989544946cd8cb017cb6cad9232d51b8a379172
James McKeever <jmckeev@sandia.gov>
2017-06-09 16:57:53 -0600

    Massive node_module changes, with new packages...

---
+++ pubdev/node_modules/react-ace/lib/editorOptions.js
@@ -0,0 +1,11 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var editorOptions = ['minLines', 'maxLines', 'readOnly', 'highlightActiveLine', 'tabSize', 'enableBasicAutocompletion', 'enableLiveAutocompletion', 'enableSnippets'];
+
+var editorEvents = ['onChange', 'onFocus', 'onBlur', 'onCopy', 'onPaste', 'onSelectionChange', 'onScroll', 'handleOptions', 'updateRef'];
+
+exports.editorOptions = editorOptions;
+exports.editorEvents = editorEvents;
