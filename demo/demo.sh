28bd010085fbaeeb59bbb16a250b53612742afce
A. Nick Georgieff <angeor@as3006snllx.sandia.gov>
2017-07-20 13:08:55 -0600

    Demo added

---
+++ demo/demo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+SHELL=/bin/sh PATH=/bin:/sbin:/usr/bin:/usr/sbin 
+
+echo 'reset elastic db'
+curl -X DELETE 'http://localhost:9200/scot'
+/opt/scot/install/src/elasticsearch/mapping.sh
+
+echo 'reset mongodb'
+mongo scot-prod < /usr/lib/scotdemo/reset.js
+/usr/lib/scotdemo/alertgenerator.pl
