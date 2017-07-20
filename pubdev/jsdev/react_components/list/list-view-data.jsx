76048b7989232d6ca8e9b59c5cbeac197b2d285e
A. Nick Georgieff <angeor@as3006snllx.sandia.gov>
2017-07-20 12:25:58 -0600

    Alert in search navigation bug squashed.

--- pubdev/jsdev/react_components/list/list-view-data.jsx
+++ pubdev/jsdev/react_components/list/list-view-data.jsx
@@ -159,7 +159,7 @@ var ListViewStatus = React.createClass({
             classStatus = 'alertgroup_promoted'
             color = 'orange'
         };
-        if (this.props.type == 'alertgroup') {
+        if (this.props.type == 'alertgroup' || this.props.type == 'alert') {
             open = this.props.dataOneRow.open_count;
             closed = this.props.dataOneRow.closed_count;
             promoted = this.props.dataOneRow.promoted_count;
