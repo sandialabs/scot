4f60004cb10de4c308495957e5346c281c4b6330
Todd Bruner <tbruner@sandia.gov>
2017-07-21 16:11:49 -0600

    untested demogenerator improvements
    Former-commit-id: 06010bcd59a34539f1a20af3d79f52232899184a

--- demo/demogenerator.pl
+++ demo/demogenerator.pl
@@ -165,6 +165,7 @@ my $alerts  = [
 
 my $events  = [
     {
+        username        => 'joplin',
         source         => [ qw(email_examinr) ],
         subject         => "Bad External HREF in email",
         tag            => [qw(email href) ],
@@ -174,6 +175,7 @@ my $events  = [
         },
     },
     {
+        username        => 'montgomery',
         source         => [ qw(internal_scanner) ],
         subject         => "Unsuccessful attempts to access admin account",
         tag            => [qw(scanner password) ],
@@ -183,6 +185,7 @@ my $events  = [
         },
     },
     {
+        username        => 'pilgrim',
         source         => [ qw(lateral_movement) ],
         subject         => "Lateral Movement Detected",
         tag            => [qw(movement) ],
@@ -192,6 +195,7 @@ my $events  = [
         },
     },
     {
+        username        => 'kelly',
         source         => [ qw(proxy) ],
         subject         => "Unauthorized Web Traffic",
         tag            => [qw(web) ],
@@ -201,6 +205,7 @@ my $events  = [
         },
     },
     {
+        username        => 'pilgrim',
         source         => [ qw(analyst) ],
         subject         => "Hunting and found a bad file",
         tag            => [qw(malware hunting) ],
@@ -213,6 +218,7 @@ my $events  = [
 
 my $intel  = [
     {
+        username        => 'montgomery',
         source         => [ qw(twitter) ],
         subject         => "Twitter feed reporting 0 day",
         tag            => [qw(0day) ],
@@ -222,6 +228,7 @@ my $intel  = [
         },
     },
     {
+        username        => 'kelley',
         source         => [ qw(twitter) ],
         subject         => "Twitter feed reporting new malicious md5 dump",
         tag            => [qw(md5) ],
@@ -234,6 +241,7 @@ my $intel  = [
 
 my $entries = [
     {
+        username => 'pilgrim',
 	body 		=> "<p>More content found here: www.purple.com www.mytestsite.com; Can someone look into this?</p>",
 	target_id	=> 1,
 	target_type	=> 'event',
@@ -241,11 +249,13 @@ my $entries = [
 
     },
     {
+        username => 'kelley',
 	body 		=> "<p>I checked the logs and can see that the user did indeed go the www.mytestsite.com. User has been contacted, but this needs to be escalated to determine if the connection was blocked.</p>",
 	target_id	=> 1,
 	target_type	=> 'event',
     },
     {
+        username => 'joplin',
 	body 		=> "<p>The connection wasn't blocked. After further investigation I can see that it also went out to 1.2.3.4.</p>",
 	target_id	=> 1,
 	target_type	=> 'event',
@@ -256,31 +266,37 @@ my $entries = [
 	target_type	=> 'event',
     },
     {
+        username => 'montgomery',
 	body 		=> "<p>The user involved was testuser1 and their IP at the time of the action was 192.168.1.2.</p>",
 	target_id	=> 2,
 	target_type	=> 'event',
     },
     {
+        username => 'joplin',
 	body 		=> "<p>Checked proxy logs and found the following IPs attempting to scan: 123.456.789.012. They were attempting to scan the entire subnet. Need to check logs to see if this was blocked.</p>",
 	target_id	=> 3,
 	target_type	=> 'event',
     }, 
     {
+        username => 'pilgrim',
 	body 		=> "<p>Domains Found: www.foo.com www.hackingfoo.com; IPs found: 123.456.789.012</p>",
 	target_id	=> 4,
 	target_type	=> 'event',
     },
     {
+        username    => 'pilgrim',
 	body 		=> "<p>New CVE's from https://twitter.com/CVEnew/ reported the following 0 day: CVE-2017-0378. Do we have a signature built that will detect this? Keep your eyes open for delivery mechanisms. Original file was being sent in an email with the file name: openme.exe</p>",
 	target_id	=> 1,
 	target_type	=> 'intel',
     },
     {
+        username    => 'kelley',
     body 		=> "<p>MD5 dump containing a list of known bad hashes. <p>098f6bcd4621d373cade4e832627b4f6 8253053f2c9e565a136264e6f96aa57b 5a105e8b9d40e1329780d62ea2265d8a ad0234829205b9033196ba818f7a872b</p></p>",
     target_id	=> 2,
     target_type	=> 'intel',
     }, 
     {
+        username    => 'joplin',
 	body 		=> "<p>While scouring through pcap I stumbled across some interesting traffic that I followed down a rabbit hole. I noticed an odd file name. Have we seen this file before? Here's the MD5 8253053f2c9e565a136264e6f96aa57b</p>",
 	target_id	=> 5,
 	target_type	=> 'event',
@@ -290,7 +306,7 @@ my $entries = [
 
 #Add Alertgroups
 foreach my $a (@$alerts) {
-    my $tx  = $ua->{admin}->post($url."/alertgroup" => json => $a);
+    my $tx  = $ua{admin}->post($url."/alertgroup" => json => $a);
     if ( my $res = $tx->success ) {
         print $res->body;
     }
@@ -305,7 +321,8 @@ foreach my $a (@$alerts) {
 #Add Events
 foreach my $a (@$events) {
     my $username = delete $a->{username};
-    my $tx  = $ua->post($url."/event" => json => $a);
+    my $agent  = $ua{$username};
+    my $tx  = $agent->post($url."/event" => json => $a);
     if ( my $res = $tx->success ) {
         print $res->body;
     }
@@ -318,7 +335,9 @@ foreach my $a (@$events) {
 
 #Add Intel
 foreach my $a (@$intel) {
-    my $tx  = $ua->post($url."/intel" => json => $a);
+    my $username = delete $a->{username};
+    my $agent  = $ua{$username};
+    my $tx  = $agent->post($url."/intel" => json => $a);
     if ( my $res = $tx->success ) {
         print $res->body;
     }
@@ -331,7 +350,9 @@ foreach my $a (@$intel) {
 
 #Add Entries
 foreach my $a (@$entries) { 
-    my $tx = $ua->post($url."/entry" => json => $a);
+    my $username = delete $a->{username};
+    my $agent  = $ua{$username};
+    my $tx = $agent->post($url."/entry" => json => $a);
     if ( my $res = $tx->success) {
         print $res->body;
     }
@@ -343,20 +364,20 @@ foreach my $a (@$entries) {
 }
 
 #Promote alerts
-$tx = $ua->put($url."/alert/6" => json => {
+my $tx = $ua{'pilgrim'}->put($url."/alert/6" => json => {
 	promote => "new",
 });
 
 sleep 5;
 
-$tx = $ua->put($url."/alert/2" => json => {
+$tx = $ua{'joplin'}->put($url."/alert/2" => json => {
 	promote => "new",
 });
 
 sleep 5;
 
 #Promote Events
-$tx = $ua->put($url."/event/1" => json => {
+$tx = $ua{'kelley'}->put($url."/event/1" => json => {
 	promote => "new",
 });
 
@@ -364,7 +385,7 @@ $tx = $ua->put($url."/event/1" => json => {
 
 sleep 2;
 
-$tx = $ua->post($url."/entry" => json => {});
+$tx = $ua{'pilgrim'}->post($url."/entry" => json => {});
 	
 exit 0;
 sleep 20;
