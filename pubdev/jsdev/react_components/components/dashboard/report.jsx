1c67393e35caf23c11976d66db982ea7dc11bfd2
James McKeever <jmckeev@sandia.gov>
2017-07-25 11:28:19 -0600

    Improving layout of reports page
    Former-commit-id: 19c0e3b91ac9cd6bdb1024df7dc076353c25805b

--- pubdev/jsdev/react_components/components/dashboard/report.jsx
+++ pubdev/jsdev/react_components/components/dashboard/report.jsx
@@ -5,21 +5,36 @@ import ReportHeatmap from './report_heatmap';
 import ReportArt from './report_art';
 // import ReportAlertPower from './report_alertpower';
 
-const Report = ( props ) => (
+const Report = ( { frontPage } ) => (
 	<div id='report' className='dashboard'>
 		<div style={{textAlign:'center'}}>
 			<h2>Reports</h2>
 		</div>
-		<div id='heatmap' className="dashboard col-md-4">
-			<div>
-				<Panel header='Heatmap'>
-					<ReportHeatmap />
-				</Panel>
-				<Panel header='Alert Response Time'>
-					<ReportArt />
-				</Panel>
+		{ frontPage ? (
+			<div id='heatmap' className="dashboard col-md-4">
+				<div>
+					<Panel header='Heatmap'>
+						<ReportHeatmap />
+					</Panel>
+					<Panel header='Alert Response Time'>
+						<ReportArt />
+					</Panel>
+				</div>
 			</div>
-		</div>
+		) : (
+			<div className='container-fluid'>
+				<div className='col-md-6'>
+					<Panel header='Heatmap'>
+						<ReportHeatmap />
+					</Panel>
+				</div>
+				<div className='col-md-6'>
+					<Panel header='Alert Response Time'>
+						<ReportArt />
+					</Panel>
+				</div>
+			</div>
+		) }
 	</div>
 )
 
