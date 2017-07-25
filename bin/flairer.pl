e8b2179b253c2df3f733e948b1e1f324f3a88995
Todd Bruner <tbruner@sandia.gov>
2017-07-25 09:30:37 -0600

    forgot to add logging

--- bin/flairer.pl
+++ bin/flairer.pl
@@ -8,6 +8,13 @@ use lib '../../Scot-Internal-Modules/lib';
 use lib '/opt/scot/lib';
 use v5.18;
 use Scot::App::Flair;
+use Data::Dumper;
+
+my $config_file = $ENV{'scot_app_flair_config_file'} //
+                        '/opt/scot/etc/flair.cfg.pl';
+my $env = Scot::Env->new(
+    config_file => $config_file,
+);
 
 $SIG{__DIE__} = sub { our @reason = @_ };
 
@@ -15,16 +22,12 @@ END {
     our @reason;
     if (@reason) {
         say "Flairer died because: @reason";
+        $env->log->error("Flairer died because: ",{filter=>\&Dumper, value=>\@reason});
     }
 }
 
 
-my $config_file = $ENV{'scot_app_flair_config_file'} //
-                        '/opt/scot/etc/flair.cfg.pl';
 
-my $env = Scot::Env->new(
-    config_file => $config_file,
-);
 
 my $loop    = Scot::App::Flair->new({ 
     env => $env,
