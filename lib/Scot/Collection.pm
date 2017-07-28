7d0e97995240072a7c5eaa510fc4bda6c49360f1
Todd Bruner <tbruner@sandia.gov>
2017-07-28 15:22:24 -0600

    link tests, removal of sarlacc hard coding
    Former-commit-id: 2d62321032d27e67945dc8d49e02c30f8f3e7b1d

--- lib/Scot/Collection.pm
+++ lib/Scot/Collection.pm
@@ -520,7 +520,8 @@ sub api_list {
     my $match   = $self->build_match_ref($href->{request});
 
     if (  ref($self) ne "Scot::Collection::Group" 
-       && ref($self) ne "Scot::Collection::Entity" ) {
+       && ref($self) ne "Scot::Collection::Entity" 
+       && ref($self) ne "Scot::Collection::Link" ) {
         $match->{'groups.read'} = { '$in' => $groups };
     }
 
