d81ac491219f7f1a41a76148ed3c58cc53b12364
A. Nick Georgieff <angeor@as3006snllx.sandia.gov>
2017-08-31 09:17:16 -0600

    Added correct data fields to the tables. Need to add context

--- pubdev/jsdev/react_components/modal/links.jsx
+++ pubdev/jsdev/react_components/modal/links.jsx
@@ -40,11 +40,6 @@ class Links extends Component {
                 maxWidth: 100,
                 sortable: true,
             },
-            {   Header: 'Subject',
-                accessor: 'subject',
-                maxWidth: '100%',
-                sortable: true,
-            },   
         
         ]
         
@@ -52,7 +47,7 @@ class Links extends Component {
             <Modal dialogClassName='links-modal' show={ this.props.modalActive } onHide={ this.props.linksModalToggle }>
                 <Modal.Header closeButton={ true } >
                     <Modal.Title>
-                        Links
+                        {this.state.data.length} items linked to {this.props.type} {this.props.id}
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
@@ -81,7 +76,24 @@ class Links extends Component {
             type: 'get',
             url: '/scot/api/v2/' + this.props.type + '/' + this.props.id + '/link',
             success: function( data ) {
-                this.setState({ data: data.records, loading: false });
+                let arr = [];
+                
+                for ( let i = 0; i < data.records.length; i++ ) {
+                    for ( let j = 0; j < data.records[i].vertices.length; j++ ) {
+                        //if ids are equal, check if the type is equal. If so, it's what is displayed so don't show it.
+                        if ( data.records[i].vertices[j].id == this.props.id  ) {
+                            if ( data.records[i].vertices[j].type != this.props.type ) {
+                                arr.push( data.records[i].vertices[j] );
+                            } else {
+                                continue;
+                            }
+                        } else {
+                            arr.push( data.records[i].vertices[j] );
+                        }
+                    }
+                }
+
+                this.setState({ data: arr, loading: false });
             }.bind(this),
             error: function( data ) {
                 this.setState({ loading: false });
