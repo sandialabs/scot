d3ccf91ff84b2d46317cfb8e77910884ba7492ad
A. Nick Georgieff <angeor@as3006snllx.sandia.gov>
2017-09-18 15:50:20 -0600

    amq reload happens after relogin

--- pubdev/jsdev/react_components/modal/login.jsx
+++ pubdev/jsdev/react_components/modal/login.jsx
@@ -1,6 +1,7 @@
 import React, { PureComponent, Component } from 'react';
 import PropTypes from 'prop-types';
 import { Modal } from 'react-bootstrap';
+import Actions from '../activemq/actions';
 
 class Login extends Component {
     constructor( props ) { 
@@ -74,6 +75,7 @@ class Login extends Component {
             data: data,
             success: function(data) {
                 console.log('success logging in');
+                Actions.restartClient();
                 this.props.loginToggle( null, true );
             }.bind(this),
             error: function(data) {
@@ -94,6 +96,7 @@ class Login extends Component {
             data: data,
             success: function() {
                 console.log('success logging in');
+                Actions.restartClient();
                 this.props.loginToggle( null, true );
             }.bind(this),
             error: function(data) {
