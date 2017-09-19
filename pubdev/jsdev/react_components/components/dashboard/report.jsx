4911dc8d1ca788474bb0fef5061ca934224c641b
James McKeever <jmckeev@sandia.gov>
2017-09-19 16:55:06 -0600

    Added routing for individual reports

--- pubdev/jsdev/react_components/components/dashboard/report.jsx
+++ pubdev/jsdev/react_components/components/dashboard/report.jsx
@@ -1,10 +1,47 @@
 import React, { PureComponent } from 'react';
-import { Panel } from 'react-bootstrap';
+import { Panel, Button } from 'react-bootstrap';
 
 import ReportHeatmap from './report_heatmap';
 import ReportArt from './report_art';
 import ReportAlertpower from './report_alertpower';
 import ReportCreated from './report_created';
+
+const reportComponentByType = ( reportType ) => {
+	switch( reportType ) {
+		default:
+		case 'heatmap':
+			return <ReportHeatmap />
+		case 'alertpower':
+			return <ReportAlertpower />
+		case 'art':
+			return <ReportArt />
+		case 'created':
+			return <ReportCreated />
+	}
+}
+
+const reportTitleByType = ( reportType ) => {
+	switch( reportType ) {
+		default:
+		case 'heatmap':
+			return "Heatmap";
+		case 'alertpower':
+			return "Alert Power";
+		case 'art':
+			return "Alert Response Time";
+		case 'created':
+			return "Items Created";
+	}
+}
+
+const reportPanelHeader = ( title, expand = null, back = null ) => (
+	<span>
+		{title}
+		{expand &&
+			<Button bsSize="small" href={expand} style={{ float: 'right' }}>+</Button>
+		}
+	</span>
+)
 
 export const ReportDashboard = () => (
 	<div id='report' className='dashboard'>
@@ -24,6 +61,8 @@ export const ReportDashboard = () => (
 	</div>
 )
 
+//<Panel header={reportPanelHeader( reportTitleByType( 'heatmap' ), '/heatmap' )}>
+
 export const ReportPage = () => (
 	<div id='report' className='dashboard' style={{height: 'calc( 100vh - 51px )', overflow: 'auto'}}>
 		<div style={{textAlign:'center'}}>
@@ -31,7 +70,7 @@ export const ReportPage = () => (
 		</div>
 		<div className='container-fluid'>
 			<div className='col-md-6'>
-				<Panel header='Heatmap'>
+				<Panel header="Heatmap">
 					<ReportHeatmap />
 				</Panel>
 				<Panel header='Alert Power'>
@@ -46,6 +85,16 @@ export const ReportPage = () => (
 					<ReportCreated />
 				</Panel>
 			</div>
+		</div>
+	</div>
+)
+
+export const SingleReport = ( { reportType = 'heatmap' } ) => (
+	<div id='report' className='dashboard' style={{height: 'calc( 100vh - 51px )', overflow: 'auto'}}>
+		<div className='container'>
+			<Panel header={reportTitleByType( reportType )}>
+				{reportComponentByType( reportType )}
+			</Panel>
 		</div>
 	</div>
 )
