8aa9a07d4cbd2f4a7d7363396fccd502cb09ef60
A. Nick Georgieff <angeor@as3006snllx.sandia.gov>
2017-10-09 16:33:22 -0600

    tlp added and flair fix for attachment columns being blank

--- pubdev/jsdev/react_components/components/traffic_light_protocol.jsx
+++ pubdev/jsdev/react_components/components/traffic_light_protocol.jsx
@@ -4,6 +4,9 @@ var Dropdown                = require('react-bootstrap/lib/Dropdown');
 var MenuItem                = require('react-bootstrap/lib/MenuItem');
 var DropdownToggle          = require("react-bootstrap/lib/DropdownToggle");
 var DropdownMenu            = require("react-bootstrap/lib/DropdownMenu");
+var OverlayTrigger          = require('react-bootstrap/lib/OverlayTrigger');
+var Tooltip                 = require('react-bootstrap/lib/Tooltip');
+
 var TrafficLightProtocol = React.createClass({
     
     selectColor: function(e) {
@@ -26,29 +29,31 @@ var TrafficLightProtocol = React.createClass({
     render: function() {
         return (
             <div>
-                <Dropdown bsSize='xsmall'>
-                    <DropdownToggle>
-                        <svg id='trafficlight1' style={{width: '20px', height: '20px'}}>
-                            <circle id="circle1" r="10" cx="10" cy="10" style={{fill: ((this.props.tlp == 'red' || this.props.tlp == 'white')  ? this.props.tlp : 'gray'), stroke: 'black', strokeWidth: '2'}} />
-                        </svg>    
-                        <svg id='trafficlight2' style={{width: '20px', height: '20px'}}>
-                            <circle id="circle2" r="10" cx="10" cy="10" style={{fill: ((this.props.tlp == 'amber' || this.props.tlp == 'white') ? this.props.tlp : 'gray') , stroke: 'black', strokeWidth: '2'}} />
-                        </svg>
-                        <svg id='trafficlight2' style={{width: '20px', height: '20px'}}>
-                            <circle id="circle3" r="10" cx="10" cy="10" style={{fill: ((this.props.tlp == 'green' || this.props.tlp == 'white') ? this.props.tlp : 'gray'), stroke: 'black', strokeWidth: '2'}} />
-                        </svg>
-                    </DropdownToggle>
-                    <DropdownMenu>
-                        <MenuItem header>Traffic Light Protocol (TLP) Color</MenuItem>
-                        <MenuItem eventKey='unset' onSelect={this.selectColor}>Unset</MenuItem>
-                        <MenuItem eventKey='red' onSelect={this.selectColor}>Red</MenuItem>
-                        <MenuItem eventKey='amber' onSelect={this.selectColor}>Amber</MenuItem>
-                        <MenuItem eventKey='green' onSelect={this.selectColor}>Green</MenuItem>
-                        <MenuItem eventKey='white' onSelect={this.selectColor}>White</MenuItem>
-                        <MenuItem divider/>
-                        <MenuItem href="https://www.us-cert.gov/tlp">What is TLP?</MenuItem>
-                    </DropdownMenu>
-                </Dropdown> 
+                <OverlayTrigger placement="top" overlay={<Tooltip id='tlp-tooltip'>{this.props.tlp}</Tooltip>}>
+                    <Dropdown bsSize='xsmall'>
+                        <DropdownToggle>
+                            <svg id='trafficlight1' style={{width: '20px', height: '20px'}}>
+                                <circle id="circle1" r="10" cx="10" cy="10" style={{fill: ((this.props.tlp == 'red' || this.props.tlp == 'white')  ? this.props.tlp : 'gray'), stroke: 'black', strokeWidth: '2'}} />
+                            </svg>    
+                            <svg id='trafficlight2' style={{width: '20px', height: '20px'}}>
+                                <circle id="circle2" r="10" cx="10" cy="10" style={{fill: ((this.props.tlp == 'amber' || this.props.tlp == 'white') ? this.props.tlp : 'gray') , stroke: 'black', strokeWidth: '2'}} />
+                            </svg>
+                            <svg id='trafficlight2' style={{width: '20px', height: '20px'}}>
+                                <circle id="circle3" r="10" cx="10" cy="10" style={{fill: ((this.props.tlp == 'green' || this.props.tlp == 'white') ? this.props.tlp : 'gray'), stroke: 'black', strokeWidth: '2'}} />
+                            </svg>
+                        </DropdownToggle>
+                        <DropdownMenu>
+                            <MenuItem header>Traffic Light Protocol (TLP) Color</MenuItem>
+                            <MenuItem eventKey='unset' onSelect={this.selectColor}>Unset</MenuItem>
+                            <MenuItem eventKey='red' onSelect={this.selectColor}>Red</MenuItem>
+                            <MenuItem eventKey='amber' onSelect={this.selectColor}>Amber</MenuItem>
+                            <MenuItem eventKey='green' onSelect={this.selectColor}>Green</MenuItem>
+                            <MenuItem eventKey='white' onSelect={this.selectColor}>White</MenuItem>
+                            <MenuItem divider/>
+                            <MenuItem href="https://www.us-cert.gov/tlp">What is TLP?</MenuItem>
+                        </DropdownMenu>
+                    </Dropdown> 
+                </OverlayTrigger>
             </div>
         )
     }
