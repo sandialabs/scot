9c83fd105891031daf80afcf299c3eccbfc8ba3b
A. Nick Georgieff <angeor@as3006snllx.sandia.gov>
2017-10-12 11:34:40 -0600

    Initial commit. Lots of work needed. nothing working.

---
+++ pubdev/jsdev/react_components/ListView/LoadingContainer/index.jsx
@@ -0,0 +1,37 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { CSSTransitionGroup } from 'react-transition-group';
+
+import Icon from '../Icon';
+import './styles.scss';
+
+class LoadingContainer extends PureComponent {
+	static propTypes = {
+		loading: PropTypes.bool,
+	}
+
+	render() {
+		return (
+			<div styleName='LoadingContainer'>
+				{ this.props.loading &&
+					<CSSTransitionGroup
+						transitionName='fade'
+						transitionAppear={true}
+						transitionLeave={true}
+						transitionAppearTimeout={0}
+						transitionEnterTimeout={0}
+						transitionLeaveTimeout={0}
+					>
+						<div key={0} styleName='loading'>
+							<span styleName='helper'></span>
+							<Icon icon='refresh' />
+						</div>
+					</CSSTransitionGroup>
+				}
+				{this.props.children}
+			</div>
+		)
+	}
+}
+
+export default LoadingContainer;
