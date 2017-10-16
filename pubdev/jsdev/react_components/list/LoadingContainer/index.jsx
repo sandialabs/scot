42403fa7d668d9a36c0494cdbe0ccf2683409e97
A. Nick Georgieff <angeor@as3006snllx.sandia.gov>
2017-10-16 14:47:03 -0600

    Filter works, working on sort
    Former-commit-id: fa10775ea464c255adf2cf5fd5cc907d492cfbe8

---
+++ pubdev/jsdev/react_components/list/LoadingContainer/index.jsx
@@ -0,0 +1,36 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { CSSTransitionGroup } from 'react-transition-group';
+
+import Icon from '../Icon';
+
+class LoadingContainer extends PureComponent {
+	static propTypes = {
+		loading: PropTypes.bool,
+	}
+
+	render() {
+		return (
+			<div styleName='LoadingContainer'>
+				{ this.props.loading &&
+					<CSSTransitionGroup
+						transitionName='fade'
+						transitionAppear={true}
+						transitionLeave={true}
+						transitionAppearTimeout={0}
+						transitionEnterTimeout={0}
+						transitionLeaveTimeout={0}
+					>
+						<div key={0} styleName='loading'>
+							<span styleName='helper'></span>
+							<Icon icon='refresh' />
+						</div>
+					</CSSTransitionGroup>
+				}
+				{this.props.children}
+			</div>
+		)
+	}
+}
+
+export default LoadingContainer;
