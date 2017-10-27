4c186461b3df0c091f0101f186a7bb38026eef13
A. Nick Georgieff <angeor@as3006snllx.sandia.gov>
2017-10-27 15:20:20 -0600

    removed node modules.. CELEBRATE!

--- pubdev/node_modules/semantic-ui-react/dist/es/lib/isBrowser.js
+++
@@ -1,5 +0,0 @@
-import _typeof from 'babel-runtime/helpers/typeof';
-var hasDocument = (typeof document === 'undefined' ? 'undefined' : _typeof(document)) === 'object' && document !== null;
-var hasWindow = (typeof window === 'undefined' ? 'undefined' : _typeof(window)) === 'object' && window !== null && window.self === window;
-
-export default hasDocument && hasWindow;
