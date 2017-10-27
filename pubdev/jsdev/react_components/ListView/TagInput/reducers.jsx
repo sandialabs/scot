ec585b2815c27fd59b10ae01cafa302da20cde98
A. Nick Georgieff <angeor@as3006snllx.sandia.gov>
2017-10-27 14:56:17 -0600

    removing unnecessary files
    Former-commit-id: dc3f357f9c005e7ecde3b613c2c9f7d7d1f50fa5

--- pubdev/jsdev/react_components/ListView/TagInput/reducers.jsx
+++
@@ -1,7 +0,0 @@
-import { buildFetchReducer } from '../../../redux/reducers/builder';
-
-export const tagInputPrefix = 'TAGINPUT';
-export const stateKey = 'tagSuggestions';
-export const clearTagSuggestionsAction = 'CLEAR_TAG_INPUT';
-
-export const handleTagInput = buildFetchReducer( { actionPrefix: tagInputPrefix, clearAction: clearTagSuggestionsAction, } );
