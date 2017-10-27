e6efb745793801c66503519269ff021218424a1e
A. Nick Georgieff <angeor@as3006snllx.sandia.gov>
2017-10-27 15:20:20 -0600

    removed node modules.. CELEBRATE!
    Former-commit-id: d98b9e015f9d59dbee208136f0babb72587b2384

--- pubdev/node_modules/semantic-ui-react/dist/es/umd.js
+++
@@ -1,9 +0,0 @@
-import _extends from 'babel-runtime/helpers/extends';
-// Heads Up!
-//
-// Do not replace this with named exports.
-// We need to export an object here for browser builds.
-// Otherwise, we end up with every component on the window.
-import * as semanticUIReact from './index';
-
-module.exports = _extends({}, semanticUIReact);
