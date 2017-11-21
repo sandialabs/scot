4c5856ccce3d9ec78d506115db09b9b850424b73
bemonta <bemonta@sandia.gov>
2017-11-21 13:22:28 -0700

    Cleaning up mongo scripts

--- docker-configs/mongodb/set_mongodb_config.sh
+++ docker-configs/mongodb/set_mongodb_config.sh
@@ -3,17 +3,7 @@
 # delete all admin accounts
 mongo scot-prod --host mongodb --eval "db.user.deleteMany({username:'admin'})"
 
-
-#admin User
-MONGODB_ADMIN_USER=${MONGODB_ADMIN_USER:-"admin"}
-MONGODB_ADMIN_PASS=${MONGODB_ADMIN_PASS:-"admin"}
-
-# Application Database User
-MONGODB_APPLICATION_DATABASE=${MONGODB_APPLICATION_DATABASE:-"scot-prod"}
-MONGODB_APPLICATION_USER=${MONGODB_APPLICATION_USER:-"admin"}
-MONGODB_APPLICATION_PASS=${MONGODB_APPLICATION_PASS:-"admin"}
-
-#HASH='{:X-PBKDF2}HMACSHA2+512:AAAnEA:sFAw7jFy0WoaZViC9H7P:7uJJ0HmGwzl6s/s79UNcyHpIbBvAIEF6jXeVuDzP+F5CYwQcxPCZM7z/XqC+4oZFLCNJLqCL0R2meU9bF8YiYg=='
+#set default password has (admin/admin)
 HASH='{X-PBKDF2}HMACSHA2+512:AAAnEA:nCpmgiazs0SRt8Z53T9I:Z97YBqqp7H+uDl+oti+buLxayC9k+geSv3/zY63kctusQWqVJl5h+rx6t07LhhYM+hO9Fk8FpuzEhKx73v3zmw=='
 
 # Wait for MongoDB to boot
@@ -30,13 +20,6 @@ mongo --host mongodb scot-prod /opt/scot/install/src/mongodb/admin_user.js
 mongo --host mongodb scot-prod --eval "db.user.update({username:'admin'}, {$set:{pwhash:'$HASH'}}, {multi:true})"
 
 sleep 3
-
-# If we've defined the MONGODB_APPLICATION_DATABASE environment variable and it's a different database
-# than admin, then create the user for that database.
-# First it authenticates to Mongo using the admin user it created above.
-# Then it switches to the REST API database and runs the createUser command 
-# to actually create the user and assign it to the database.
-
 
 # If everything went well, add a file as a flag so we know in the future to not re-create the
 # users if we're recreating the container (provided we're using some persistent storage)
