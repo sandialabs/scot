c54d537c4854ac575926232683addbf51b4fbb0c
bemonta <bemonta@sandia.gov>
2017-11-28 11:46:39 -0700

    stuff

--- docker-configs/game/game.pl
+++ docker-configs/game/game.pl
@@ -16,17 +16,6 @@ say "--- Starting Game Tally ---";
 
 my $config_file = $ENV{'scot_game_config_file'} // '/opt/scot/etc/game.cfg.pl';
 
-#$SIG{__DIE__} = sub { our @reason = @_ };
-#
-#END {
-#    our @reason;
-#    if (@reason) {
-#        say "Game died because: @reason";
-#        $env->log->error("Game died because: ",{filter=>\&Dumper, value=>\@reason});
-#    }
-#}
-
-
 my $env = Scot::Env->new({
     config_file => $config_file,
 });
