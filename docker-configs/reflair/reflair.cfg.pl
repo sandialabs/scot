e99705e3889dd27b1111b4bc4b2f1b9b178686d1
bemonta <bemonta@sandia.gov>
2017-11-28 11:16:19 -0700

    Adding in reflair

---
+++ docker-configs/reflair/reflair.cfg.pl
@@ -0,0 +1,56 @@
+%environment = (
+    time_zone   => 'America/Denver',
+    # server name of the SCOT server
+    servername  => 'scot',
+    # username with sufficient scot perms to create alert(groups)
+    username    => 'scot-alerts',
+    # the password for that user
+    password    => 'changemenow',
+    # authentication type: RemoteUser, LDAP, Local
+    authtype    => 'Local',
+    # interactive
+    interactive => 0,
+    # max workers
+    max_workers => 20,
+    # log config
+    log_config  => {
+        logger_name     => 'SCOT',
+        layout          => '%d %7p [%P] %15F{1}: %4L %m%n',
+        appender_name   => 'scot_log',
+        logfile         => '/var/log/scot/scot.flair.log',
+        log_level       => 'DEBUG',
+    },
+    stomp_host  => "activemq",
+    stomp_port  => 61613,
+    topic       => "/topic/scot",
+    # modules used by flair app
+    modules => [
+        {
+            attr    => 'mongo',
+            class   => 'Scot::Util::MongoFactory',
+            config  => {
+                db_name         => 'scot-prod',
+                host            => 'mongodb://mongodb',
+                write_safety    => 1,
+                find_master     => 1,
+            },
+        },
+        {
+            attr    => 'mq',
+            class   => 'Scot::Util::Messageq',
+            config  => {
+                destination => "scot",
+                stomp_host  => "activemq",
+                stomp_port  => 61613,
+            },
+        },
+        {
+            attr    => 'es',
+            class   => 'Scot::Util::ElasticSearch',
+            config  => {
+                nodes       => [ qw(elastic:9200) ],
+                max_workers => 1,
+            },
+        },
+    ],
+);
