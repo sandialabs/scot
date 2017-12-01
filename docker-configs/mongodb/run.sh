2cc88b87ac3b41a976b26d5a7b6a488b4f36d6a4
bemonta <bemonta@sandia.gov>
2017-12-01 14:23:13 -0700

    removed mapping container and moved into mongodb

--- docker-configs/mongodb/run.sh
+++ docker-configs/mongodb/run.sh
@@ -11,6 +11,7 @@ $cmd &
 
 #check to see if file exists (meaning an admin user exists) and if it doesnt, create one
 if [ ! -f /var/lib/mongodb/.mongodb_password_set ]; then
+    /mapping.sh
     /set_mongodb_config.sh 
 fi
 
