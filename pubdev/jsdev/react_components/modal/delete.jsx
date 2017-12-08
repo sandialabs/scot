ff3f4ff71b912d8ad3fba849252fb9c5cc17a7a8
James McKeever <jmckeev@sandia.gov>
2017-12-07 19:57:40 -0700

    Convert delete modals to es6, start generic modal

--- pubdev/jsdev/react_components/modal/delete.jsx
+++ pubdev/jsdev/react_components/modal/delete.jsx
@@ -1,7 +1,8 @@
-var React               = require('react');
-var Modal               = require('react-modal');
-var Button              = require('react-bootstrap/lib/Button');
-var Link                = require('react-router-dom').Link;
+import React, { PureComponent } from 'react';
+import Modal from 'react-modal';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 const customStyles = {
     content : {
@@ -17,13 +18,18 @@ const customStyles = {
     }
 }
 
-var DeleteEvent = React.createClass({
-    getInitialState: function() {
-        return {
-            key:this.props.id,
-        }
-    },
-    toggle: function() {  
+export class DeleteEvent extends PureComponent {
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			key: props.id,
+		};
+
+		this.toggle = this.toggle.bind(this);
+	}
+
+	toggle() {
         $.ajax({
             type: 'delete',
             url: 'scot/api/v2/' + this.props.type + '/'  + this.props.id,
@@ -37,32 +43,36 @@ var DeleteEvent = React.createClass({
             }.bind(this)
         });  
         this.props.history.push( '/' + this.props.type );       
-    },
-    render: function() { 
-        return (
-            <div>
-                <Modal isOpen={true} onRequestClose={this.props.deleteToggle} style={customStyles}>
-                    <div className='modal-header'>
-                        <img src='images/close_toolbar.png' className='close_toolbar' onClick={this.props.deleteToggle} />
-                        <h3 id='myModalLabel'>Are you sure you want to delete {this.props.subjectType}: {this.props.id}?</h3>
-                    </div> 
-                    <div className='modal-footer'>
-                        <Button id='cancel-delete' onClick={this.props.deleteToggle}>Cancel</Button>
-                        <Button bsStyle='danger' id='delete' onClick={this.toggle}>Delete</Button>     
-                    </div>
-                </Modal>
-            </div>
-        )
-    }
-});
+	}
 
-var DeleteEntry = React.createClass({
-    getInitialState: function() {
-        return {
-            key:this.props.id,
-        }
-    },
-    toggle: function() {
+	render() {
+		return (
+			<Modal isOpen={true} onRequestClose={this.props.deleteToggle} style={customStyles}>
+				<div className='modal-header'>
+					<img src='images/close_toolbar.png' className='close_toolbar' onClick={this.props.deleteToggle} />
+					<h3 id='myModalLabel'>Are you sure you want to delete {this.props.subjectType}: {this.props.id}?</h3>
+				</div> 
+				<div className='modal-footer'>
+					<Button id='cancel-delete' onClick={this.props.deleteToggle}>Cancel</Button>
+					<Button bsStyle='danger' id='delete' onClick={this.toggle}>Delete</Button>     
+				</div>
+			</Modal>
+		)
+	}
+}
+
+export class DeleteEntry extends PureComponent {
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			key: props.id,
+		};
+
+		this.toggle = this.toggle.bind( this );
+	}
+
+	toggle() {
         $.ajax({
            type: 'delete',
            url: 'scot/api/v2/entry/' + this.props.entryid,
@@ -75,26 +85,50 @@ var DeleteEntry = React.createClass({
            }.bind(this)
         }); 
         this.props.deleteToggle();
-    },
-    render: function() {
-        return (
-            <div>
-                <Modal isOpen={true} onRequestClose={this.props.deleteToggle} style={customStyles}>
-                    <div className='modal-header'>
-                        <img src='images/close_toolbar.png' className='close_toolbar' onClick={this.props.deleteToggle} />
-                        <h3 id='myModalLabel'>Are you sure you want to delete Entry: {this.props.entryid}?</h3>
-                    </div>
-                    <div className='modal-footer'>
-                        <Button id='cancel-delete' onClick={this.props.deleteToggle}>Cancel</Button>
-                        <Button bsStyle='danger' id='delete' onClick={this.toggle}>Delete</Button>
-                    </div>
-                </Modal>
-            </div>
-        )
-    }  
-});
+	}
 
-module.exports = {
-    DeleteEvent:DeleteEvent,
-    DeleteEntry:DeleteEntry
+	render() {
+        return (
+			<Modal isOpen={true} onRequestClose={this.props.deleteToggle} style={customStyles}>
+				<div className='modal-header'>
+					<img src='images/close_toolbar.png' className='close_toolbar' onClick={this.props.deleteToggle} />
+					<h3 id='myModalLabel'>Are you sure you want to delete Entry: {this.props.entryid}?</h3>
+				</div>
+				<div className='modal-footer'>
+					<Button id='cancel-delete' onClick={this.props.deleteToggle}>Cancel</Button>
+					<Button bsStyle='danger' id='delete' onClick={this.toggle}>Delete</Button>
+				</div>
+			</Modal>
+        )
+	}
+}
+
+const deleteObjectType = 
+			PropTypes.shape([
+				type: PropTypes.string,
+				id: PropTypes.number,
+			])
+
+export class DeleteModal extends PureComponent {
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+		};
+
+		this.deleteAll = this.deleteAll.bind(this);
+	}
+
+	static propTypes = {
+		things: PropTypes.oneOfType([
+			deleteObjectType,
+			PropTypes.arrayOf(deleteObjectType),
+		]).isRequired,
+		errorToggle: PropTypes.func.isRequired,
+	}
+
+	deleteAll() {
+	}
+
+	render() {}
 }
