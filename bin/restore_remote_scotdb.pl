d5bdfc883aa9e852b797f08a3fc840e3abb58fdd
Todd Bruner <tbruner@sandia.gov>
2017-12-13 13:41:37 -0700

    catching changes

--- bin/restore_remote_scotdb.pl
+++ bin/restore_remote_scotdb.pl
@@ -71,7 +71,7 @@ system($cmd);
 say "Restoring Cached images...";
 system("cp -r $downdir/opt/scotbackup/cached_images/ /opt/scot/public/");
 
-say "Now fixing IDs in mongodb"
+say "Now fixing IDs in mongodb";
 system("perl fix_last_id.pl");
 
 
