e33dd686c506884a86bd54c1c36df2a1c40a1ed0
bemonta <bemonta@sandia.gov>
2017-12-18 10:20:04 -0700

    updated script

--- docker-scripts/restart-build-deploy.sh
+++ docker-scripts/restart-build-deploy.sh
@@ -5,7 +5,7 @@ sed -i 's/proxy: " "/proxy: http:\/\/wwwproxy.sandia.gov:80/g' docker-compose.ym
 sed -i 's/-s -S/-x $proxy -s -S/g' Dockerfile-Activemq
 sed -i 's/" "/http:\/\/wwwproxy.sandia.gov:80/g' Dockerfile-Scot
 #build open-source scot
-sudo docker-compose build
+docker-compose build
 #Replace sandia with open-source ubuntu stuff
 find . -type f \( -iname "Dockerfile-*" ! -iname "" \) -exec sed -i 's/gitlab.sandia.gov:3434\/docker\/ubuntu/ubuntu:16.04/g' {} +
 sed -i 's/proxy: http:\/\/wwwproxy.sandia.gov:80/proxy: " "/g' docker-compose.yml
