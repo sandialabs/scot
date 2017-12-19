bc276eccc236f406db27278ef6cb0c1ce8b15074
bemonta <bemonta@sandia.gov>
2017-12-19 11:45:06 -0700

    Hopefully this works

--- docker-configs/mongodb/set_mongodb_config.sh
+++ docker-configs/mongodb/set_mongodb_config.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
+
+function setup_scot_admin {
+    MONGOADMIN=$(mongo scot-prod --eval "printjson(db.user.count({username:'admin'}))" --quiet)
+    
+    if [[ "$MONGOADMIN" == "0" ]] || [[ "$RESETDB" == "yes" ]]; then
+        echo "-------"
+        echo "------- USER INPUT NEEDED"
+        echo "-------"
+        echo "------- Choose a SCOT Admin login Password "
+        set='$set'
+        HASH=`$SCOT_CONFIG_SRC/mongodb/passwd.pl`
+
+        mongo scot-prod /opt/scot/install/src/mongodb/admin_user.js
+        mongo scot-prod --eval "db.user.update({username:'admin'}, {$set:{pwhash:'$HASH'}})"
+    fi
+}
+
+
+
+set_scot_admin
+
+
 # delete all admin accounts
-mongo scot-prod --host mongodb --eval "db.user.deleteMany({username:'admin'})"
+#mongo scot-prod --host mongodb --eval "db.user.deleteMany({username:'admin'})"
 
 #set default password has (admin/admin)
-HASH='{X-PBKDF2}HMACSHA2+512:AAAnEA:nCpmgiazs0SRt8Z53T9I:Z97YBqqp7H+uDl+oti+buLxayC9k+geSv3/zY63kctusQWqVJl5h+rx6t07LhhYM+hO9Fk8FpuzEhKx73v3zmw=='
+#HASH='{X-PBKDF2}HMACSHA2+512:AAAnEA:nCpmgiazs0SRt8Z53T9I:Z97YBqqp7H+uDl+oti+buLxayC9k+geSv3/zY63kctusQWqVJl5h+rx6t07LhhYM+hO9Fk8FpuzEhKx73v3zmw=='
 
 # Wait for MongoDB to boot
 RET=1
@@ -21,8 +43,8 @@ set='$set'
 # users if we're recreating the container (provided we're using some persistent storage)
 
 if [ ! -f /var/lib/mongodb/.mongodb_password_set ]; then
-    mongo --host mongodb scot-prod /opt/scot/install/src/mongodb/admin_user.js
-    mongo --host mongodb scot-prod --eval "db.user.update({username:'admin'}, {$set:{pwhash:'$HASH'}}, {multi:true})"
+    #mongo --host mongodb scot-prod /opt/scot/install/src/mongodb/admin_user.js
+    #mongo --host mongodb scot-prod --eval "db.user.update({username:'admin'}, {$set:{pwhash:'$HASH'}}, {multi:true})"
     echo "hello" > /var/lib/mongodb/.mongodb_password_set
 fi
 
