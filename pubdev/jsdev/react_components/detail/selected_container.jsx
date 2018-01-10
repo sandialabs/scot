a328e546c06b3c18b77a905680b07ac853772725
A. Nick Georgieff <angeor@sandia.gov>
2018-01-10 15:26:12 -0700

    custom metadata table complete. Use scot.cfg.pl to configure this table

--- pubdev/jsdev/react_components/detail/selected_container.jsx
+++ pubdev/jsdev/react_components/detail/selected_container.jsx
@@ -4,7 +4,7 @@ var SelectedHeader = require('./selected_header.jsx');
 var SelectedContainer = React.createClass({
     render: function() {
         var datarows = [];
-        datarows.push(<SelectedHeader key={this.props.id} id={this.props.id} type={this.props.type} toggleEventDisplay={this.props.viewEvent} taskid={this.props.taskid} alertPreSelectedId={this.props.alertPreSelectedId} handleFilter={this.props.handleFilter} errorToggle={this.props.errorToggle} history={this.props.history} />); 
+        datarows.push(<SelectedHeader key={this.props.id} id={this.props.id} type={this.props.type} toggleEventDisplay={this.props.viewEvent} taskid={this.props.taskid} alertPreSelectedId={this.props.alertPreSelectedId} handleFilter={this.props.handleFilter} errorToggle={this.props.errorToggle} history={this.props.history} form={this.props.form}/>); 
         //var width = this.state.width;
         var width = '100%';
         if ($('#list-view')[0] != undefined ) {
