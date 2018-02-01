b3b22cb83a782b8636e35ecdf547c3818a2d33c3
bemonta <bemonta@sandia.gov>
2018-02-01 08:50:16 -0700

    hopefully this fix works

--- docker-scripts/build-push.sh
+++ docker-scripts/build-push.sh
@@ -14,7 +14,7 @@ export ELASTICGID=`id elasticsearch -g`
 export MONGODBUID=`id mongodb -u`
 export MONGODBGID=`id mongodb -g`
 
-cd ../
+
 #build open-source scot
 docker-compose -f docker-compose-custom.yml build
 
