b67ab2115871c98ea1dbef9b3690e00127518e0d
James McKeever <jmckeev@sandia.gov>
2018-02-01 10:59:06 -0700

    Standardizing thinglist widgets

--- pubdev/jsdev/react_components/components/dashboard/dazzle/widgets.jsx
+++ pubdev/jsdev/react_components/components/dashboard/dazzle/widgets.jsx
@@ -1,25 +1,8 @@
 import { ReportWidgets } from '../report';
 import Status from '../status';
-import ThingList from '../thinglist';
+import ThingList, { Widgets as ThingWidgets } from '../thinglist';
 
 export const Widgets = {
-	intel: {
-		type: ThingList,
-		title: "Thing List",
-		description: "A list of things",
-		props: {
-			thingType: 'intel',
-			title: 'Top Intel',
-		}
-	},
-	events: {
-		type: ThingList,
-		title: "Thing List2",
-		description: "A list of things",
-		props: {
-			thingType: 'event',
-			title: 'Top Events',
-		}
-	},
+	...ThingWidgets(),
 	...ReportWidgets(),
 };
