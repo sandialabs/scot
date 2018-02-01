89256dcb7a7dda82a74c10909e7d135e8d3db15f
bemonta <bemonta@sandia.gov>
2018-02-01 14:30:36 -0700

    updated formatting for script

--- restart-build-deploy.sh
+++ restart-build-deploy.sh
@@ -1,3 +1,5 @@
+sudo docker-compose down
+
 unset MONGODBUID
 unset MONGODBGID
 unset SCOTUID
@@ -95,14 +97,14 @@ echo " "
 echo "**************** "
 echo "Hello, and welcome to the Docker version of SCOT 🔍 "
 echo " "
-echo "This script will walk you through the installationprocess. First, we have a couple questions for you"
+echo "This script will walk you through the installation process. First, we have a couple questions for you"
 echo " "
 echo "SCOT has two installation modes:" 
 echo " "
-echo "1. Easy Mode - In this mode, we will pull pre-built images from Dockerhub. There is no customization such as insertion of your servers SSL certs, no LDAP integration, etc."
-echo "2. Custom mode: In this mode, we will build the Docker containers from the Dockerfile's contained in the cloned source code. You can also make changes to the Dockerfile's, source code, etc. as you see fit"
+echo "1. Easy Mode - In this mode, we will pull pre-built images from Dockerhub. Note: Using this mode, there is no customization such as insertion of your own server's SSL certs, no LDAP integration, etc."
+echo "2. Custom mode: In this mode, we will build the Docker containers from the Dockerfiles contained in the cloned source code. You can also make changes to the Dockerfiles, source code, etc. as you see fit"
 echo " "
-echo -n "Please enter your selection: 1 for easy mode 2 for custom mode"
+echo -n "Please enter your selection: 1 for easy mode / 2 for custom mode"
 read -n 1 selection
 
 if [ "$selection" == "1" ]; then
