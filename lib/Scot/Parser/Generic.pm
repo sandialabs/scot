4ddb1c6c2ad75f73b1e66f4017c595e2ed373452
Todd Bruner <tbruner@sandia.gov>
2018-03-02 16:53:09 -0700

    another attempt to get plain emails into alertgroup view

--- lib/Scot/Parser/Generic.pm
+++ lib/Scot/Parser/Generic.pm
@@ -1,6 +1,7 @@
 package Scot::Parser::Generic;
 
 use lib '../../../lib';
+use HTML::TreeBuilder;
 use Moose;
 
 extends 'Scot::Parser';
@@ -18,14 +19,25 @@ sub parse_message {
     my $self    = shift;
     my $href    = shift;
     my $log     = $self->log;
+    my $body;
+    if ( defined $href->{body_plain} ) {
+        $body   = $href->{body_plain};
+    }
+    else {
+        my $html    = $href->{body_html};
+        my $tree    = HTML::TreeBuilder->new;
+        $tree->parse_content($html);
+        $body   = $tree->as_text;
+    }
+
     my %json    = (
         subject     => $href->{subject},
         message_id  => $href->{message_id},
         body_plain  => $href->{body_plain},
-        body        => $href->{body_html},
+        body        => $body,
         data        => [{
             sender  => $href->{from},
-            alert   => $href->{body_plain},
+            alert   => $body,
             columns => [qw(sender alert)],
         }],
         source      => [ qw(email generic) ],
