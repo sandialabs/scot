9e01915378f41367d08a25abba8c86784a8784ff
Todd Bruner <tbruner@sandia.gov>
2018-03-29 09:44:29 -0600

    testing programs for queue

---
+++ bin/amq_queue_read_test.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/env perl
+
+use lib '../lib';
+use v5.18;
+
+use strict;
+use warnings;
+
+use AnyEvent::STOMP::Client;
+use Scot::Env;
+use JSON;
+use Data::Dumper;
+use DateTime;
+
+
+my $stomp   = AnyEvent::STOMP::Client->new("localhost");
+
+$stomp->connect();
+$stomp->on_connected(
+    sub { 
+        my $stomp   = shift;
+        $stomp->subscribe('/queue/coe');
+    }
+);
+
+$stomp->on_error(
+    sub {
+        say "ERROR";
+        say Dumper(\@_);
+    }
+);
+
+$stomp->on_message(
+    sub {
+        my ( $stomp, $header, $body ) = @_;
+
+        my $json = decode_json $body;
+
+        my $dt  = DateTime->now();
+       
+        my $date    = $dt->ymd . " ". $dt->hms;
+        my $dl  = length($date);
+
+        my $nd = 80 - $dl - 10;
+
+        say "-"x10 . $dt->ymd . " ". $dt->hms. "-"x$nd;
+        say Dumper($json);
+        say "-"x80;
+    }
+);
+say "===== Watching ActiveMQ =========";
+AnyEvent->condvar->recv;
+
+
+
