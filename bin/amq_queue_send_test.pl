71afc811b08f8295b9c6e4798a02742d214562a4
Todd Bruner <tbruner@sandia.gov>
2018-03-29 09:44:29 -0600

    testing programs for queue

---
+++ bin/amq_queue_send_test.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/env perl
+use lib '../lib';
+use Data::Dumper;
+use JSON;
+use Net::STOMP::Client;
+use v5.18;
+
+my $stomp = Net::STOMP::Client->new(
+    host    => '127.0.0.1',
+    port    => 61613,
+);
+$stomp->connect;
+
+for my $i (1..10) {
+
+    my $href    = {
+        id      => $i,
+        type    => "foo",
+        data    => {
+            key1    => "value1",
+            key2    => "value2",
+            key3    => "value3",
+            key4    => "value4",
+        }
+    };
+
+    my $json = encode_json $href;
+    $stomp->send(
+        destination => "/queue/coe",
+        body        => $json,
+    );
+
+    sleep 1;
+}
+
+$stomp->disconnect;
