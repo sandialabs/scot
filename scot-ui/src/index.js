cd05d80fc91912ee9962d5091416d82180eef0b4
bemonta <bemonta@sandia.gov>
2018-08-16 14:51:50 -0600

    change to file names, and a bit of refactoring

--- scot-ui/src/index.js
+++ scot-ui/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { AMQ } from './activemq/amq';
 import registerServiceWorker from './registerServiceWorker';
+import Switch from "react-router-dom/es/Switch";
+import Route from "react-router-dom/es/Route";
+import HashRouter from "react-router-dom/es/HashRouter";
+let customHistory   = require( 'history' ).createBrowserHistory;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<HashRouter history={customHistory()}>
+    <Switch>
+        <Route exact path = '/' component = {AMQ} />
+        <Route exact path = '/:value' component = {AMQ} />
+        <Route exact path = '/:value/:id' component = {AMQ} />
+        <Route exact path = '/:value/:id/:id2' component = {AMQ} />
+        <Route path = '/:value/:type/:id/:id2' component = {AMQ} />
+    </Switch>
+</HashRouter>, document.getElementById('root'));
 registerServiceWorker();
