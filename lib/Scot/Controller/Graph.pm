d305a389b0fcdc065ba37fdfb3a797a11a3c276a
Todd Bruner <tbruner@sandia.gov>
2018-09-04 16:13:00 -0600

    look for sparklines in more places

--- lib/Scot/Controller/Graph.pm
+++ lib/Scot/Controller/Graph.pm
@@ -1,115 +1,232 @@
 package Scot::Controller::Graph;
 
-
-=head1 Name
-
-Scot::Controller::Graph
-
-=head1 Description
-
-generate data sets suitable for force directed graph display
-
-=cut
-
 use Data::Dumper;
-use Try::Tiny;
-use DateTime;
-use Mojo::JSON qw(decode_json encode_json);
-use Data::Dumper::HTML qw(dumper_html);
-use List::MoreUtils qw(uniq);
+use v5.18;
 use strict;
 use warnings;
 use base 'Mojolicious::Controller';
 
-=head1 Routes
-
-=over 4
-
-=item I<GET> /graph
-
-=cut
 
 sub get_graph {
     my $self    = shift;
-    my $type    = $self->stash('thing');    # event | entity
-    my $id      = $self->stash('id');       # int id of the type
-    my $depth   = $self->stash('depth')//0;    # number of nodes to traverse
+    my $type    = $self->stash('thing');
+    my $id      = $self->stash('id');
+    my $depth   = $self->stash('depth');
+    my $params  = $self->req->params->to_hash;
 
-    my @links   = ();       # ... array of links
-    my %seen    = ();       # ... deduplication hash
+    my %db  = ();
     my $target  = { type => $type, id => $id };
 
-    $self->build_graph(\@links, \%seen, $target, $depth);
-    $self->render(json => \@links);
+    $self->build_graph(\%db, $target, $depth);
+    $self->rendergraph(\%db);
 }
 
 sub build_graph {
-    my $self        = shift;
-    my $link_aref   = shift;
-    my $seen_href   = shift;
-    my $target      = shift; # ... { type => $type, id => $id }
-    my $depth       = shift;
+    my $self    = shift;
+    my $dbhref  = shift;
+    my $target  = shift;
+    my $depth   = shift;
 
-    $depth --;
-
-    return if ( $depth < 0 );   # done recursing
-
-    my $type    = $target->{type};
-    my $id      = $target->{id};
-    my $skey    = $type.$id;
-
-    if ( $type eq "event" ) {
-        foreach my $entity_id ( $self->get_entities($target) ) {
-            my $dkey = "entity".$entity_id;
-            if ( ! defined  $seen_href->{seen}->{$skey.$dkey} ) {
-                push @{$link_aref}, { event => $id, entity => $entity_id };
-                $seen_href->{seen}->{$skey.$dkey}++;
-                $seen_href->{count}->{$dkey}++;
-            }
-            my $target  = { type => "entity", id => $entity_id };
-            $self->build_graph($link_aref, $seen_href, $target, $depth);
-        }
+    if ( $depth == 0 ) {
+        return;
     }
-    else {
-        foreach my $event_id ( $self->get_events($target) ) {
-            my $dkey = "event".$event_id;
-            if ( ! defined $seen_href->{seen}->{$skey.$dkey} ) {
-                push @{$link_aref}, { event => $event_id, entity => $id };
-                $seen_href->{seen}->{$skey.$dkey}++;
-                $seen_href->{count}->{$dkey}++;
-            }
-            my $target = { type => "event", id => $event_id };
-            $self->build_graph($link_aref, $seen_href, $target, $depth);
-        }
+
+    my $cursor  = $self->get_links($target);
+    my @newnode = ();
+    while ( my $link = $cursor->next ) {
+        $self->conditionally_add_to_graph($link, $dbhref, \@newnode);
+    }
+    $depth--;
+    foreach my $newnode (@newnode) {
+        $self->build_graph($dbhref, $newnode, $depth);
     }
 }
 
-sub get_entities {
+sub get_links {
     my $self    = shift;
     my $target  = shift;
-    my $type    = $target->{type};
-    my $id      = $target->{id};
+    my $filter  = [qw(alert)];
 
-    my $col  = $self->env->mongo->collection('Event');
-    my $cur  = $col->get_subthing($type, $id, "entity");
-    my @ids  = map { $_->{id} } $cur->all; # mongo returns ->all as array of hashes not objs
-    my @uids = uniq(@ids);
-    
-    return wantarray ? @uids : \@uids;
+    my $cursor  = $self->env->mongo->collection('Link')
+                        ->get_links_by_target($target, $filter);
+    return $cursor;
 }
 
-sub get_events {
+sub conditionally_add_to_graph {
     my $self    = shift;
-    my $target  = shift;
-    my $type    = $target->{type};
-    my $id      = $target->{id};
-    my $col  = $self->env->mongo->collection('Entity');
-    my $cur  = $col->get_subthing($type, $id, "event");
-    my @ids  = map { $_->{id} } $cur->all; # mongo returns ->all as array of hashes not objs
-    my @uids = uniq(@ids);
-    
-    return wantarray ? @uids : \@uids;
+    my $link    = shift;
+    my $dbhref  = shift;
+    my $naref   = shift;
+    my $env     = $self->env;
+    my $mongo   = $env->mongo;
+    my $log     = $env->log;
 
+    my ( $v0, $v1 ) = @{$link->vertices};
+
+    if ( my $n0 = $self->ok_to_add_node($dbhref, $v0) ) {
+        $self->add_node($v0, $n0, $dbhref);
+        push @$naref, $v0;
+    }
+
+    if ( my $n1 = $self->ok_to_add_node($dbhref, $v1) ) {
+        $self->add_node($v1, $n1, $dbhref);
+        push @$naref, $v1;
+    }
+
+    if ( my $edge = $self->ok_to_add_edge($dbhref, $v0, $v1) ) {
+        $self->add_edge($dbhref, $edge, $v0, $v1);
+    }
+}
+
+sub ok_to_add_node {
+    my $self    = shift;
+    my $dbhref  = shift;
+    my $v       = shift;
+    my $k       = $v->{type}."_".$v->{id};
+    if ( defined $dbhref->{nodes}->{$k} ) {
+        return undef;
+    }
+    my $n       = $self->get_vertice($v);
+    if ( ! defined $n ) {
+        return undef;
+    }
+    if ( $self->node_filtered($n) ) {
+        return undef;
+    }
+    return $n;
+}
+
+sub node_filtered {
+    my $self    = shift;
+    my $node    = shift;
+    my $filters = $self->env->graph_entity_filter;
+
+    if ( ref($node) eq "Scot::Model::Entity" ) {
+        if ( $node->status eq "untracted" ) {
+            return 1;
+        }
+    }
+    if ( grep { /^$node->value$/i } @$filters ) {
+        return 1;
+    }
+    return undef;
+}
+
+sub get_degree {
+    my $self    = shift;
+    my $node    = shift;
+    my $count   = $self->env->mongo->collection('Link')
+                       ->get_display_count($node);
+    return $count;
+}
+
+sub ok_to_add_edge {
+    my $self    = shift;
+    my $dbhref  = shift;
+    my $v0      = shift;
+    my $v1      = shift;
+
+    my $key = $v0->{type} . "-".
+              $v0->{id}. "_" .
+              $v1->{type} . "-".
+              $v1->{id};
+
+    if ( defined $dbhref->{edges}->{$key} ) {
+        return undef;
+    }
+    return $key;
+}
+
+sub add_edge {
+    my $self    = shift;
+    my $dbhref  = shift;
+    my $edge    = shift;
+    my $v0      = shift;
+    my $v1      = shift;
+    $dbhref->{edges}->{$edge} = {
+        id      => $edge,
+        from    => $self->get_key($v0),
+        to      => $self->get_key($v1),
+    };
+}
+
+sub get_vertice {
+    my $self    = shift;
+    my $vertex  = shift;
+    my $type    = $vertex->{type};
+    my $id      = $vertex->{id};
+    my $mongo   = $self->env->mongo;
+    my $node    = $mongo->collection(ucfirst($type))->find_iid($id);
+    return $node;
+}
+
+sub rendergraph {
+    my $self    = shift;
+    my $dbhref  = shift;
+
+}
+
+sub add_node {
+    my $self    = shift;
+    my $dbhref  = shift;
+    my $vertex  = shift;
+    my $node    = shift;
+
+    my $key     = $vertex->{type} . "_" . $vertex->{id};
+    my $degree  = $self->get_degree($node);
+
+    $dbhref->{nodes}->{$key} = {
+        id      => $key,
+        label   => $self->get_label($node, $vertex, $degree),
+        shape   => $self->get_shape($vertex),
+        color   => $self->get_color($vertex),
+        title   => $key,
+        degree  => $degree,
+        data    => { },
+    };
+}
+
+sub get_label {
+    my $self    = shift;
+    my $node    = shift;
+    my $vertex  = shift;
+    my $degree  = shift;
+
+    my $type    = $vertex->{type};
+    my $id      = $vertex->{id};
+
+    if ( $type  eq "entity" ) {
+        return "[$id]\n".$node->value."\n{$degree}";
+    }
+    if ( grep {/$type/} (qw(entry event intel incident guide checklist)) ) {
+        return "[$id]\n".$node->subject."\n{$degree}";
+    }
+    return "$type $id";
+}
+
+sub get_color {
+    my $self    = shift;
+    my $vertex  = shift;
+    my $type    = $vertex->{type};
+    if ( $type eq "entry" ) {
+        return "#FFFF00";
+    }
+    if ( $type eq "entity" ) {
+        return "#C2FABC";
+    }
+    return "#97C2FC";
+}
+    
+sub get_shape {
+    my $self    = shift;
+    my $node    = shift;
+    if ( $node->{type} eq "entry" ) {
+        return "diamond";
+    }
+    if ( $node->{type} eq "entity" ) {
+        return "box";
+    }
+    return "box";
 }
 
 1;
