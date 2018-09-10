ada08b24d90a56e35602a96fc1154a5e246ef23c
Todd Bruner <tbruner@sandia.gov>
2018-09-10 11:24:22 -0600

    changed filename type to "file" to match old data

--- lib/Scot/Extractor/Regex.pm
+++ lib/Scot/Extractor/Regex.pm
@@ -673,7 +673,7 @@ sub _build_COMMON_FILE_EXT {
     }xims;
     return {
         regex   => $regex,
-        type    => "filename",
+        type    => "file",
         order   => 100,
         options => { multiword => "no" },
     };
