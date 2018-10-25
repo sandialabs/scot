08d4e8c4adc4828eea74e6a3624e17058585c98c
Montano <bemonta@s992509.srn.sandia.gov>
2018-10-25 14:39:44 -0600

    fix to table

--- scot-ui/src/list/tableConfig.js
+++ scot-ui/src/list/tableConfig.js
@@ -343,6 +343,14 @@ const columnDefinitions = {
         maxWidth: 150,
     },
 
+    EntityType: {
+        Header: 'Type',
+        accessor: 'type',
+        Filter: customFilters.stringFilter,
+        minWidth: 100,
+        maxWidth: 150,
+    },
+
 
     Description: {
         Header: 'Description',
@@ -440,7 +448,7 @@ const typeColumns = {
     ],
     signature: ['Id', 'Location', 'Name', 'Type', 'SigStatus', 'Group', 'Description', 'Owner', 'Tags', 'Sources', 'Updated',],
     guide: ['Id', 'Location', 'Subject', 'AppliesTo'],
-    entity: ['Id', 'Location', 'Value', 'Type', 'Entries',],
+    entity: ['Id', 'Location', 'Value', 'EntityType', 'Entries',],
     default: ['Id', 'Location', 'AlertStatus', 'Subject', 'Created', 'Sources', 'Tags', 'Views',],
 };
 
