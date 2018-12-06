8a9e2a0500b826abad0202da0c2d6261547ecce4
bemonta <bemonta@sandia.gov>
2018-12-06 10:10:42 -0700

    hmm

--- scot-ui/src/components/admin/index.js
+++ scot-ui/src/components/admin/index.js
@@ -1,20 +1,46 @@
 import React from 'react';
-import React from 'react';
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import UserGroupContainer from './usergroupcontainer'
+
+
+function TabContainer(props) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
+}
+
+const theme = createMuiTheme({
+  typography: {
+    // Tell Material-UI what the font-size on the html element is.
+    fontSize: 20,
+  },
+});
+
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
+    // backgroundColor: theme.palette.background.paper,
+  },
+  appBar: {
+    backgroundColor: '#2196f3',
+  },
+  typography: {
+    // Tell Material-UI what the font-size on the html element is.
+    fontSize: 20,
   },
 });
 
-export default class Admin extends React.Component {
+class Admin extends React.Component {
   state = {
     value: 0,
   };
@@ -28,24 +54,34 @@ export default class Admin extends React.Component {
     const { value } = this.state;
 
     return (
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Tabs value={value} onChange={this.handleChange}>
-            <Tab label="Item One" />
-            <Tab label="Item Two" />
-            <Tab label="Item Three" />
-          </Tabs>
-        </AppBar>
-        {value === 0 && <TabContainer>Item One</TabContainer>}
-        {value === 1 && <TabContainer>Item Two</TabContainer>}
-        {value === 2 && <TabContainer>Item Three</TabContainer>}
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div className={classes.root}>
+          <AppBar className={classNames(classes.appBar, classes.typography)} position="static">
+            <Tabs value={value} indicatorColor="primary" centered onChange={this.handleChange}>
+              <Tab label="Users / Groups" />
+              <Tab label="API" />
+              <Tab label="Undelete" />
+            </Tabs>
+          </AppBar>
+          {value === 0 &&
+            <TabContainer>
+              <UserGroupContainer></UserGroupContainer>
+            </TabContainer>}
+          {value === 1 &&
+            <TabContainer>
+              API stuff
+              </TabContainer>}
+          {value === 2 &&
+            <TabContainer>
+              Undelete stuffs
+            </TabContainer>}
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
-
-SimpleTabs.propTypes = {
+Admin.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(SimpleTabs);
+export default withStyles(styles)(Admin);
