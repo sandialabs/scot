f2af851ec1cd8a862d4c91a03dd820056fa7414b
bemonta <bemonta@sandia.gov>
2019-01-17 13:23:37 -0700

    almost there

--- scot-ui/scripts/watch.js
+++ scot-ui/scripts/watch.js
@@ -2,10 +2,11 @@ process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
 const fs = require('fs-extra');
 const webpack = require('webpack');
-const config = require('../config/webpack.config.prod.js');
+//const config = require('../config/webpack.config.js');
+const webpackconfig = require('../config/webpack.config.js');
+const config = webpackconfig('development');
 const paths = require('../config/paths.js');
 // removes react-dev-utils/webpackHotDevClient.js at first in the array
-config.entry.shift();
 
 console.log("📦 Starting webpack dev build with watch functionality, no minification");
 
