ff6725b2ff82fb77a2061efd814f2f361f9b3313
Montano <bemonta@s992509.srn.sandia.gov>
2019-01-23 06:31:18 -0700

    updating of cyberchef

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ebdf69cc145dd522fee8b90ed6e368ab.js"
+  "/precache-manifest.611c1030e87f048b2912b4d9f6f8e6c1.js"
 );
 
 workbox.clientsClaim();
