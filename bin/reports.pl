fe1eac239f73a0e725eb64491094aaa299a8f5f9
Todd Bruner <tbruner@.sandia.gov>
2019-02-07 11:10:08 -0700

    updating reports, incomplete fix of flair bug for cidr

--- bin/reports.pl
+++ bin/reports.pl
@@ -71,7 +71,7 @@ EOF
 
 =cut
 
-my $config_file = $ENV{'scot_reports_config_file'} // '/opt/scot/etc/scot.cfg.pl';
+my $config_file = $ENV{'scot_reports_config_file'} // '/opt/scot/etc/reports.cfg.pl';
 
 my $env  = Scot::Env->new(
     config_file => $config_file,
@@ -143,22 +143,22 @@ sub pyramid {
 
     my $query   = {
         created => {
-            '$gte'  => $start,
-            '$lte'  => $end,
+            '$gte'  => $startdt->epoch,
+            '$lte'  => $enddt->epoch,
         }
     };
     
     print "\n------- Pyramid Report ---------\n";
-    my $col = $env->mongo->collection('Alertgroup');
-    my $cursor  = $col->find($query);
-    my $alert_count = $cursor->count();
+    $env->log->debug("Pyramid Query: ",{filter=>\&Dumper, value=>$query});
 
-    $col    = $env->mongo->collection('Event');
-    $cursor     = $col->find($query);
-    my $event_count = $cursor->count();
+    my $col         = $env->mongo->collection('Alertgroup');
+    my $alert_count = $col->count($query);
 
-    $cursor     = $env->mongo->collection('Incident')->find($query);
-    my $incident_count  = $cursor->count();
+    $col            = $env->mongo->collection('Event');
+    my $event_count = $col->count($query);
+
+    $col                = $env->mongo->collection('Incident');
+    my $incident_count  = $col->count($query);
 
     printf "%12d   Alerts\n", $alert_count;
     printf "%12d   Events\n", $event_count;
@@ -193,20 +193,18 @@ sub compare_days {
     foreach my $weeks_ago (0..8) {
         my $start_epoch = $startdt->epoch - ($weeks_ago * $week);
         my $end_epoch   = $enddt->epoch - ($weeks_ago * $week);
-        my $cursor      = $mongo->read_documents({
-            collection  => "alertgroups",
-            match_ref   => {
-                '$and'  => [ 
-                    { when  => { '$gte' => $start_epoch } },
-                    { when  => { '$lte' => $end_epoch } },
-                ],
-            },
-            sort_ref    => { alertgroup_id => 1 },
-        });
+        my $col         = $mongo->collection('Alertgroup');
+        my $match       = {
+            when        => {
+                '$gte'  => $start_epoch,
+                '$lte'  => $end_epoch,
+            }
+        };
+        my $cursor      = $col->find($match);
+        $cursor->sort( { alertgroup_id => 1 });
         my $currentdt   = DateTime->from_epoch(epoch=>$start_epoch);
         my $ymd         = $currentdt->ymd('-');
-        $totals{$ymd}   = $cursor->count;
-        #print "$ymd has $totals{$ymd} alertgroups\n";
+        $totals{$ymd}   = $col->count($match);
     }
     print "\n--------------- Alerts ------------\n";
     print "Date, Count\n";
@@ -217,78 +215,32 @@ sub compare_days {
 
 
 
+
 sub alerts_by_month {
     my $counter = 0;
-    my $cursor  = $mongo->read_documents({
-        collection  => "alertgroups",
-        match_ref   => {
-            '$and'  => [
-                { when   => { '$gte' => $startdt->epoch } },
-                { when   => { '$lte' => $enddt->epoch } },
-            ],
-        },
-        sort_ref    => { alertgroup_id => 1 },
-    });
-    my $total = $cursor->count;
+    my $col     = $mongo->collection('Alert');
+    my $match   = {
+        when    => {
+            '$gte'  => $startdt->epoch,
+            '$lte'  => $enddt->epoch,
+        }
+    };
+    $env->log->debug("match is ",{filter=>\&Dumper, value=>$match});
+    my $cursor  = $col->find($match);
+    my $total   = $col->count($match);
     my %results;
     my @debug;
     my $flag    = undef;
 
-    while ( my $alert = $cursor->next_raw ) {
-        printf "%d of %d\r", $counter++, $total;
-        my $dt      = DateTime->from_epoch( epoch => $alert->{when});
-        my $month   = $dt->month;
-        my $year    = $dt->year;
+    $env->log->debug("found $total records");
 
-        #my $wdt    = DateTime->from_epoch( epoch => $alert->{when} );
-        #my $cdt    = DateTime->from_epoch( epoch => $alert->{created} );
-        #my $udt    = DateTime->from_epoch( epoch => $alert->{updated} );
-
-        #if ( $month == 9 and $year = 2013 ) {
-            #if ( $alert->{updated} < $alert->{created} or 
-            #     $alert->{when} < $alert->{created} ) {
-#
-#                if ( $alert->{updated} <= $alert->{when} ) {
-#                    # print "Alert ". $alert->{alert_id} . "updated field is earlier\n";
-#                    $year    = $udt->year;
-#                    $month   = $udt->month;
-#                    $flag++;
-#                }
-#                else {
-#                    # print "Alert ". $alert->{alert_id} . " when field is earlier\n";
-#                    $year    = $wdt->year;
-#                    $month   = $wdt->month;
-#                    $flag++;
-#                }
-#            }
-
-#            if ( $wdt->ymd ne $cdt->ymd ) {
-        #        print   "Alert ".$alert->{alert_id}." when ".$wdt->ymd.
-        #                " created ".$cdt->ymd."\n";
-#            }
-            # if ( $udt->ymd ne $cdt->ymd ) {
-        #        print   "Alert ".$alert->{alert_id}." updt ".$udt->ymd.
-        #                " created ".$cdt->ymd."\n";
-#                $year   = $wdt->year;
-#                $month  = $wdt->month;
-#                $flag++;
-#            }
-
-        #} 
-
-        #if ( $year == 2013 and $month == 9 ) {
-        #    if ( $wdt->year != 2013 or $udt->year != 2013 
-        #         or $wdt->month != 9 or $udt->month != 9 ) {
-        #        alert_debug($alert, $wdt, $cdt, $udt);
-        #    }
-        #}
-
-        $results{$year}{$month}++;
-     
+    while ( my $alert   = $cursor->next ) {
+        my $dt  = DateTime->from_epoch( epoch => $alert->when );
+        $results{$dt->year}{$dt->month}++;
     }
 
     print "\n--------------- Alerts by Month ------------\n";
-    print "Year    Month    Count\n";
+    print "Year,Month,Alerts\n";
     foreach my $y (sort {$a<=>$b} keys %results) {
         foreach my $m (sort {$a<=>$b} keys %{$results{$y}} ) {
             printf "%d/%d, %d\n", $m, $y, $results{$y}{$m};
@@ -316,43 +268,50 @@ sub alert_debug {
         $udt->ymd;
 }
 
+sub convert_epoch {
+    my $epoch   = shift;
+    my $dt      = DateTime->from_epoch(epoch=>$epoch);
+    $dt->set_time_zone('America/Denver');
+    return  $dt->year, 
+            $dt->month,
+            $dt->day_of_week,
+            $dt->hour;
+}
+
 sub response_times {
     my %results; 
     my $collection = $mongo->collection('Alertgroup');
-    my $cursor  = $collection->find({
-        '$and'  => [
-            { when   => { '$gte' => $startdt->epoch } },
-            { when   => { '$lte' => $enddt->epoch } },
-        ],
-    });
-    $cursor->sort({id => 1});
+    my $match   = {
+        when    => {
+            '$gte'  => $startdt->epoch,
+            '$lte'  => $enddt->epoch,
+        }
+    };
+    my $cursor  = $collection->find($match);
+    my $total   = $collection->count($match);
 
-    while ( my $alert = $cursor->next ) {
-        my $when    = $alert->when;
-        my $dt      = DateTime->from_epoch( epoch => $when);
-        $dt->set_time_zone('America/Denver');
-        my $month   = $dt->month;
-        my $year    = $dt->year;
-        my $hour    = $dt->hour;
-        my $day     = $dt->day_of_week;
-        printf "Alert %d : ", $alert->id;
-        if ( $day > 0 and $day < 6 ) {
+    while ( my $alertgroup = $cursor->next ) {
+        my $when    = $alertgroup->when;
+        my ($year, $month, $dow, $hour ) = convert_epoch($when);
+        # printf "Alertgroup %d : ", $alertgroup->id;
+
+        if ( $dow > 0 and $dow < 6 ) {
             if ( $hour < 18 and $hour > 7 ) {
-                print " $month $hour: ";
-                my $first_view = get_earliest($alert);
+                # print " $month $hour: ";
+                my $first_view = get_earliest($alertgroup);
                 if ( $first_view ) {
                     my $delta = $first_view - $when;
-                    printf " %d seconds\n", $delta;
+                    # printf " %d seconds\n", $delta;
                     if ( $delta > 0 ) {
                         $results{$year}{$month}{total_time} += $delta;
-                        $results{$year}{$month}{count} += $alert->alert_count;
+                        $results{$year}{$month}{count} += $alertgroup->alert_count;
                     }
                 }
             }
-            print " After Hours...\n";
+            # print " After Hours...\n";
         }
         else {
-            print " Skipping the weekend...\n";
+            # print " Skipping the weekend...\n";
         }
     }
 
@@ -386,24 +345,27 @@ sub get_earliest {
 
 sub event_report {
     my $counter = 0;
-    my $cursor  = $mongo->read_documents({
-        collection  => "events",
-        match_ref   => {
-            '$and'  => [
-                { when   => { '$gte' => $startdt->epoch } },
-                { when   => { '$lte' => $enddt->epoch } },
-            ],
+    my $match   = {
+        when   => { 
+            '$gte' => $startdt->epoch,
+            '$lte' => $enddt->epoch 
         },
-        sort_ref    => { event_id => 1 },
-    });
-    my $total = $cursor->count;
+    };
+    my $col     = $mongo->collection('Event');
+    $env->log->debug("match is :",{filter=>\&Dumper, value=>$match});
+    my $cursor  = $col->find($match);
+    $cursor->sort( { event_id => 1 });
+    my $total = $col->count($match);
+    $env->log->debug("found $total records");
 
     my %results = ();
     while ( my $event   = $cursor->next ) {
+        my $ehash = $event->as_hash;
+        # $env->log->debug("event: ",{filter=> \&Dumper, value=>$ehash});
         my $created_dt  = DateTime->from_epoch(epoch=>$event->created);
         my $entry_count = $event->entry_count;
-        my $view_count  = $event->view_count;
-        my $team_count  = scalar(keys %{$event->viewed_by});
+        my $view_count  = $event->views;
+        my $team_count  = scalar(keys %{$event->view_history});
         my $score       = $entry_count * ( $view_count/$team_count );
         $results{$created_dt->year}{$created_dt->month}{events}++; 
         $results{$created_dt->year}{$created_dt->month}{views} += $view_count; 
