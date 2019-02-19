7de06863427a28a1f77ee39d958d5b9ef971efd2
bemonta <bemonta@sandia.gov>
2019-02-19 08:45:21 -0700

    Fix to widgets

---
+++ public/precache-manifest.0b4ba51fcb6a40ca808d22f1efd696da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "981c5c79f3644d601a8bfd8c4f4d3bfd",
+    "url": "/static/media/scot_logo_473x473.981c5c79.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "90153c28b402dddf7277",
+    "url": "/static/js/main.90153c28.chunk.js"
+  },
+  {
+    "revision": "5677ac7c0ee9bd32dab7",
+    "url": "/static/js/2.5677ac7c.chunk.js"
+  },
+  {
+    "revision": "90153c28b402dddf7277",
+    "url": "/static/css/main.63eddf49.chunk.css"
+  },
+  {
+    "revision": "2bf4568d8ada95f0f875b8d2171212b3",
+    "url": "/index.html"
+  }
+];
