a8c1f0cfca02b906d67052e237e8b3f00bc40a75
Javier A. Chavez <javchav@as3023snllx.sandia.gov>
2019-09-03 10:08:12 -0600

    Bug fix encapsulated 'applies_to' in data object

---
+++ public/precache-manifest.7ee96914853d02629bc0b48bbbbc36d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "981c5c79f3644d601a8bfd8c4f4d3bfd",
+    "url": "/static/media/scot_logo_473x473.981c5c79.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "6cdec5ef9f706f89a39d",
+    "url": "/static/js/main.6cdec5ef.chunk.js"
+  },
+  {
+    "revision": "c455d814fea16bebf6c0",
+    "url": "/static/js/2.c455d814.chunk.js"
+  },
+  {
+    "revision": "6cdec5ef9f706f89a39d",
+    "url": "/static/css/main.1ead4464.chunk.css"
+  },
+  {
+    "revision": "d22bea7836de361d57554cd35f5f0cf2",
+    "url": "/index.html"
+  }
+];
