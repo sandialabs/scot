0c2303f47eea748f0d38c9554e785b8d2818c6ad
bemonta <bemonta@sandia.gov>
2019-11-13 08:38:08 -0700

    revert to smyg original bug to fix sigs. Will need to patch

---
+++ public/precache-manifest.22dc1c4eda6c2b99f0a87f980a91491d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "981c5c79f3644d601a8bfd8c4f4d3bfd",
+    "url": "/static/media/scot_logo_473x473.981c5c79.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "b7d772df214d560833b5",
+    "url": "/static/js/main.b7d772df.chunk.js"
+  },
+  {
+    "revision": "d9f59b835ecde58aa9c8",
+    "url": "/static/js/2.d9f59b83.chunk.js"
+  },
+  {
+    "revision": "b7d772df214d560833b5",
+    "url": "/static/css/main.23c18382.chunk.css"
+  },
+  {
+    "revision": "0dc9868ddb9b508f4ad9f43aba0f4028",
+    "url": "/index.html"
+  }
+];
