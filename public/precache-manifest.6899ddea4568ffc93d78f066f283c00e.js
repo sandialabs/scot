a421042dee5f7ca33006718be37fcceb9510fff2
bemonta <bemonta@sandia.gov>
2019-12-05 10:32:38 -0700

    more removal

---
+++ public/precache-manifest.6899ddea4568ffc93d78f066f283c00e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "981c5c79f3644d601a8bfd8c4f4d3bfd",
+    "url": "/static/media/scot_logo_473x473.981c5c79.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "3cc13157eeaf3fc3473b",
+    "url": "/static/js/main.3cc13157.chunk.js"
+  },
+  {
+    "revision": "74fdf2d07e58b59ce13f",
+    "url": "/static/js/2.74fdf2d0.chunk.js"
+  },
+  {
+    "revision": "3cc13157eeaf3fc3473b",
+    "url": "/static/css/main.23c18382.chunk.css"
+  },
+  {
+    "revision": "22b3def5e93bb9c6cb950eb1cb363ef3",
+    "url": "/index.html"
+  }
+];
