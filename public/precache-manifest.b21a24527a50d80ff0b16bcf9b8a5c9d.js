895b78bb8dccc9dba9cbefcf79823105eace91d9
bemonta <bemonta@sandia.gov>
2020-01-07 10:39:42 -0700

    react tags should be working much better now, less errros too

---
+++ public/precache-manifest.b21a24527a50d80ff0b16bcf9b8a5c9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06fee22c5f6803ac11184c5588d16932",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2900a359971322f8cf79",
+    "url": "/static/css/main.f9bff169.chunk.css"
+  },
+  {
+    "revision": "f4dc0b70f0d06e60d578",
+    "url": "/static/js/2.63a60a63.chunk.js"
+  },
+  {
+    "revision": "696ef60d43727c13118985e16b7b358b",
+    "url": "/static/js/2.63a60a63.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2900a359971322f8cf79",
+    "url": "/static/js/main.8ef99184.chunk.js"
+  },
+  {
+    "revision": "7a5355a5219183435ee8",
+    "url": "/static/js/runtime-main.b2b772df.js"
+  },
+  {
+    "revision": "981c5c79f3644d601a8bfd8c4f4d3bfd",
+    "url": "/static/media/scot_logo_473x473.981c5c79.png"
+  }
+]);
