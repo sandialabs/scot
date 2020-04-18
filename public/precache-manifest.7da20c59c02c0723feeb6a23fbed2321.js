35866333975dc33de683d204edafa210bbe3b1b6
Todd Bruner <tbruner@as3001snllx.sandia.gov>
2020-04-17 19:14:48 -0600

    learning this build processes

---
+++ public/precache-manifest.7da20c59c02c0723feeb6a23fbed2321.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd4ea162ac9f0b0d6e291bc53a3a1489",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df95d39e47898a028dae",
+    "url": "/static/css/main.f9bff169.chunk.css"
+  },
+  {
+    "revision": "4b3fe1d7e7684fc8f216",
+    "url": "/static/js/2.16b92249.chunk.js"
+  },
+  {
+    "revision": "900427a96803e543ecada678e9a78fb6",
+    "url": "/static/js/2.16b92249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df95d39e47898a028dae",
+    "url": "/static/js/main.4ba2cab2.chunk.js"
+  },
+  {
+    "revision": "9728aed1c948044a7930",
+    "url": "/static/js/runtime-main.fff72596.js"
+  },
+  {
+    "revision": "981c5c79f3644d601a8bfd8c4f4d3bfd",
+    "url": "/static/media/scot_logo_473x473.981c5c79.png"
+  }
+]);
