ca729aad96e9e3b7343aa5a681e0359b7b2ea75a
Todd Bruner <tbruner@as3001snllx.sandia.gov>
2020-06-30 13:19:03 -0600

    update module name of json cleanser

--- lib/Scot/App/Responder/Stretch.pm
+++ lib/Scot/App/Responder/Stretch.pm
@@ -40,7 +40,7 @@ sub process_message {
         return 1;
     }
 
-    my $cleanser    = Data::Clean::JSON->get_cleanser;
+    my $cleanser    = Data::Clean::FromJSON->get_cleanser;
     my $record      = $self->get_document($type, $id);
     $cleanser->clean_in_place($record);
 
