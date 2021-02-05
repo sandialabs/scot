249558ba60de616f60f9f121601a59862c8c089b
Bruner <tbruner@s1059902.srn.sandia.gov>
2021-02-05 08:56:45 -0700

    start roughing in dispatch-intel-product workflow

---
+++ lib/Scot/Model/Dispatch.pm
@@ -0,0 +1,89 @@
+package Scot::Model::Dispatch;
+
+use lib '../../../lib';
+use Moose;
+use namespace::autoclean;
+
+=head1 Name
+
+Scot::Model::Dispatch
+
+=head1 Description
+
+The model of an individual Dispatch item
+
+=head1 Extends
+
+Scot::Model
+
+=cut
+
+extends 'Scot::Model';
+with    qw(
+    Meerkat::Role::Document
+    Scot::Role::Entriable
+    Scot::Role::Hashable
+    Scot::Role::Historable
+    Scot::Role::Permission
+    Scot::Role::Sources
+    Scot::Role::Subject
+    Scot::Role::Tags
+    Scot::Role::Times
+    Scot::Role::TLP
+    Scot::Role::Views
+);
+
+=head1 Consumed Roles
+
+    Meerkat::Role::Document
+    Scot::Role::Entriable
+    Scot::Role::Hashable
+    Scot::Role::Historable
+    Scot::Role::Permission
+    Scot::Role::Sources
+    Scot::Role::Subject
+    Scot::Role::Tags
+    Scot::Role::Times
+    Scot::Role::TLP
+    Scot::Role::Views
+
+=head1 Attributes
+
+=over 4
+
+=item B<subject>
+
+the subject of the item
+from Scot::Role::Subject
+
+=cut
+
+=item B<source_uri>
+
+Where the dispatch came from
+
+=cut
+
+has source_uri => (
+    is          => 'ro',
+    isa         => 'Str',
+    required    => 1,
+    default     => 'unknown',
+);
+
+
+__PACKAGE__->meta->make_immutable;
+1;
+
+=back
+
+=head1 Copyright
+
+Copyright (c) 2014 Sandia National Laboratories.
+
+=head1 Author
+
+Todd Bruner.  
+
+=cut
+    
